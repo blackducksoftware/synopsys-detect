c441c184c8f087a0173741c06586522c58b7b706
Jake Mathews <mathewsj2@wit.edu>
2017-04-27 16:40:41 -0400

    Added rubygems finder

---
+++ src/main/groovy/com/blackducksoftware/integration/hub/packman/packagemanager/RubygemsPackageManager.groovy
@@ -0,0 +1,56 @@
+package com.blackducksoftware.integration.hub.packman.packagemanager
+
+import java.nio.charset.StandardCharsets
+
+import org.apache.commons.io.IOUtils
+import org.springframework.stereotype.Component
+
+import com.blackducksoftware.integration.hub.bdio.simple.model.DependencyNode
+import com.blackducksoftware.integration.hub.packman.PackageManagerType
+import com.blackducksoftware.integration.hub.packman.packagemanager.rubygems.RubygemsPackager
+
+@Component
+class RubygemsPackageManager extends PackageManager {
+    public static final String GEMFILELOCK_FILENAME = 'Gemfile.lock'
+    public static final String GEMSPEC_EXTENSION = '.gemspec'
+    public static final String GEMFILE_FILENAME = 'Gemfile'
+
+    PackageManagerType getPackageManagerType() {
+        return PackageManagerType.COCOAPODS
+    }
+
+    boolean isPackageManagerApplicable(String sourcePath) {
+        File sourceDirectory = new File(sourcePath)
+        if (sourcePath && sourceDirectory.isDirectory()) {
+            File gemlockFile = new File(sourceDirectory, GEMFILELOCK_FILENAME)
+            File gemspecFile = findGemspecFile(sourceDirectory)
+            File gemfileFile = new File(sourceDirectory, GEMFILE_FILENAME)
+            return gemlockFile.isFile() && gemspecFile.isFile() && gemfileFile.isFile()
+        }
+        false
+    }
+
+    List<DependencyNode> extractDependencyNodes(String sourcePath) {
+        File sourceDirectory = new File(sourcePath)
+        File gemlockFile = new File(sourceDirectory, GEMFILELOCK_FILENAME)
+        File gemspecFile = findGemspecFile(sourceDirectory)
+        File gemfileFile = new File(sourceDirectory, GEMFILE_FILENAME)
+
+        final InputStream gemlockStream
+        try {
+            gemlockStream = new FileInputStream(gemlockFile)
+            String potentialProjectName = sourceDirectory.getName()
+            def rubygemsPackager = new RubygemsPackager(IOUtils.toString(gemlockStream, StandardCharsets.UTF_8), gemspecFile, gemfileFile)
+            def projects = rubygemsPackager.makeDependencyNodes()
+            return projects
+        } finally {
+            IOUtils.closeQuietly(gemlockStream)
+        }
+    }
+
+    private File findGemspecFile(File sourceDirectory) {
+        sourceDirectory.listFiles().find {
+            it.name.endsWith(GEMSPEC_EXTENSION)
+        }
+    }
+}
---
+++ src/main/groovy/com/blackducksoftware/integration/hub/packman/packagemanager/rubygems/RubygemsPackager.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2017 Black Duck Software Inc.
+ * http://www.blackducksoftware.com/
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Black Duck Software ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Black Duck Software.
+ */
+package com.blackducksoftware.integration.hub.packman.packagemanager.rubygems;
+
+import java.util.List;
+
+import com.blackducksoftware.integration.hub.bdio.simple.model.DependencyNode;
+
+public class RubygemsPackager {
+
+    public List<DependencyNode> makeDependencyNodes() {
+
+        return null;
+    }
+}
