b10fd42c21e1c8260d2b710bcfd4f4eaabb443bb
Eric Kerwin <ekerwin@blackducksoftware.com>
2017-04-28 15:54:56 -0400

    adding some utility stuff for tests

--- src/test/groovy/com/blackducksoftware/integration/hub/packman/packagemanager/gradle/GradleParsingPackagerTest.groovy
+++ src/test/groovy/com/blackducksoftware/integration/hub/packman/packagemanager/gradle/GradleParsingPackagerTest.groovy
@@ -2,67 +2,81 @@ package com.blackducksoftware.integration.hub.packman.packagemanager.gradle
 
 import java.nio.charset.StandardCharsets
 
+import org.junit.Assert
 import org.junit.Test
 
 import com.blackducksoftware.integration.hub.bdio.simple.model.DependencyNode
+import com.blackducksoftware.integration.hub.bdio.simple.model.Forge
 import com.blackducksoftware.integration.hub.bdio.simple.model.externalid.ExternalId
 import com.blackducksoftware.integration.hub.bdio.simple.model.externalid.MavenExternalId
+import com.blackducksoftware.integration.hub.packman.testutils.DependencyNodeUtil
+import com.blackducksoftware.integration.hub.packman.testutils.ExternalIdTypeAdapter
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 
 class GradleParsingPackagerTest {
+    DependencyNodeUtil dependencyNodeUtil = new DependencyNodeUtil()
+
+    @Test
+    public void testParsingStandardLines() {
+        GradleParsingPackager gradleParsingPackager = new GradleParsingPackager(null, null, null)
+        String outputLine = '|    |    +--- com.google.code.gson:gson:2.7'
+        DependencyNode node = gradleParsingPackager.createDependencyNodeFromOutputLine(outputLine)
+        Assert.assertEquals('gson', node.name)
+        Assert.assertEquals('2.7', node.version)
+        Assert.assertEquals('com.google.code.gson:gson:2.7', node.externalId.createExternalId())
+        Assert.assertEquals(Forge.maven, node.externalId.forge)
+        Assert.assertTrue(node.children.empty)
+    }
+
+    @Test
+    public void testWinningVersionLines() {
+        GradleParsingPackager gradleParsingPackager = new GradleParsingPackager(null, null, null)
+        String outputLine = '|    |    |    \\--- org.slf4j:slf4j-api:1.7.21 -> 1.7.22'
+        DependencyNode node = gradleParsingPackager.createDependencyNodeFromOutputLine(outputLine)
+        Assert.assertEquals('slf4j-api', node.name)
+        Assert.assertEquals('1.7.22', node.version)
+        Assert.assertEquals('org.slf4j:slf4j-api:1.7.22', node.externalId.createExternalId())
+        Assert.assertEquals(Forge.maven, node.externalId.forge)
+        Assert.assertTrue(node.children.empty)
+    }
+
+    @Test
+    public void testParsingSeenElsewhereLines() {
+        GradleParsingPackager gradleParsingPackager = new GradleParsingPackager(null, null, null)
+        String outputLine = '|    |         \\--- com.squareup.okhttp3:okhttp:3.4.2 (*)'
+        DependencyNode standardDependencyNode = gradleParsingPackager.createDependencyNodeFromOutputLine(outputLine)
+        Assert.assertEquals('okhttp', standardDependencyNode.name)
+        Assert.assertEquals('3.4.2', standardDependencyNode.version)
+        Assert.assertEquals('com.squareup.okhttp3:okhttp:3.4.2', standardDependencyNode.externalId.createExternalId())
+        Assert.assertEquals(Forge.maven, standardDependencyNode.externalId.forge)
+        Assert.assertTrue(standardDependencyNode.children.empty)
+    }
+
     @Test
     public void testMultipleConfigurations() {
         InputStream dependenciesInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream('gradle/gradle-dependencies-hub-artifactory')
         String dependenciesContent = dependenciesInputStream.getText(StandardCharsets.UTF_8.name())
 
-        InputStream expectedInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream('gradle/gradle-dependencies-hub-artifactory-expected')
-        String expectedContent = expectedInputStream.getText(StandardCharsets.UTF_8.name())
-
         GradleParsingPackager gradleParsingPackager = new GradleParsingPackager(null, null, null)
-        def rootProjectNode = new DependencyNode('project', 'version', new MavenExternalId('group', 'project', 'version'))
-        gradleParsingPackager.createDependencyNodesFromOutputLines(rootProjectNode, dependenciesContent.split('\n'))
+        def actualDependencyNode = new DependencyNode('project', 'version', new MavenExternalId('group', 'project', 'version'))
+        gradleParsingPackager.createDependencyNodesFromOutputLines(actualDependencyNode, dependenciesContent.split('\n'))
 
-        GsonBuilder gsonBuilder = new GsonBuilder()
-        gsonBuilder.registerTypeAdapter(ExternalId.class, new ExternalIdTypeAdapter())
-        gsonBuilder.setPrettyPrinting()
-        Gson gson = gsonBuilder.create()
-        File jsonTest = new File('/Users/ekerwin/Documents/jsonTest.json')
-        jsonTest.delete()
-        jsonTest << gson.toJson(rootProjectNode)
-        String jsonContent = jsonTest.getText(StandardCharsets.UTF_8.name())
-        DependencyNode fromJson = gson.fromJson(jsonContent, DependencyNode.class)
+        InputStream expectedInputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream('gradle/gradle-dependencies-hub-artifactory-expected.json')
+        String expectedJson = expectedInputStream.getText(StandardCharsets.UTF_8.name())
 
-        StringBuilder stringBuilder = new StringBuilder()
-        buildNodeString(stringBuilder, 0, rootProjectNode)
-        println(stringBuilder.toString())
+        Gson gson = new GsonBuilder().registerTypeAdapter(ExternalId.class, new ExternalIdTypeAdapter()).create()
+        def expectedDependencyNode = gson.fromJson(expectedJson, DependencyNode.class)
 
-        stringBuilder = new StringBuilder()
-        buildNodeString(stringBuilder, 0, fromJson)
-        println(stringBuilder.toString())
-    }
+        dependencyNodeUtil.sortDependencyNode(expectedDependencyNode)
+        dependencyNodeUtil.sortDependencyNode(actualDependencyNode)
 
-    def buildNodeString(StringBuilder stringBuilder, int currentLevel, DependencyNode node) {
-        String prefix = '  '.multiply(currentLevel)
-        stringBuilder.append(prefix + node.externalId.createExternalId() + '\n')
-        node.children.each {
-            buildNodeString(stringBuilder, currentLevel + 1, it)
-        }
-    }
+        StringBuilder expectedStringBuilder = new StringBuilder()
+        dependencyNodeUtil.buildNodeString(expectedStringBuilder, 0, expectedDependencyNode)
 
-    Comparator<DependencyNode> nodeComparator() {
-        new Comparator<DependencyNode>() {
-                    public int compare(final DependencyNode lhs, final DependencyNode rhs) {
-                        if (lhs == rhs || lhs.externalId == rhs.externalId) {
-                            0
-                        } else if (lhs == null || lhs.externalId == null) {
-                            -1
-                        } else if (rhs == null || rhs.externalId == null) {
-                            1
-                        }
+        StringBuilder actualStringBuilder = new StringBuilder()
+        dependencyNodeUtil.buildNodeString(actualStringBuilder, 0, actualDependencyNode)
 
-                        lhs.externalId.createDataId().compareTo(rhs.externalId.createDataId())
-                    }
-                }
+        Assert.assertEquals(expectedStringBuilder.toString(), actualStringBuilder.toString())
     }
 }
--- src/test/groovy/com/blackducksoftware/integration/hub/packman/testutils/ExternalIdTypeAdapter.java
+++ src/test/groovy/com/blackducksoftware/integration/hub/packman/testutils/ExternalIdTypeAdapter.java
@@ -1,4 +1,4 @@
-package com.blackducksoftware.integration.hub.packman.packagemanager.gradle;
+package com.blackducksoftware.integration.hub.packman.testutils;
 
 import java.io.IOException;
 import java.util.EnumSet;
