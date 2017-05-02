04ce6dba0ed3b4e93f9e396468c5fc5888184b11
Jake Mathews <mathewsj2@wit.edu>
2017-05-02 10:07:45 -0400

    Started adding carthage support

---
+++ src/main/groovy/com/blackducksoftware/integration/hub/packman/packagemanager/carthage/parsers/CartfileParser.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2017 Black Duck Software Inc.
+ * http://www.blackducksoftware.com/
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Black Duck Software ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Black Duck Software.
+ */
+package com.blackducksoftware.integration.hub.packman.packagemanager.carthage.parsers;
+
+import java.util.List;
+
+import com.blackducksoftware.integration.hub.bdio.simple.model.DependencyNode;
+
+public class CartfileParser {
+    public List<DependencyNode> parse(final String cartfile) {
+
+        return null;
+    }
+}
