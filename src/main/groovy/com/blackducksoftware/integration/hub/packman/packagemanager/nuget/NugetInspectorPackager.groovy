819e8590b8bf87a7e63fae8da13b65f60e530d5f
Jake Mathews <mathewsj2@wit.edu>
2017-05-19 19:54:35 -0400

    Added nuget executable

--- src/main/groovy/com/blackducksoftware/integration/hub/packman/packagemanager/nuget/NugetInspectorPackager.groovy
+++ src/main/groovy/com/blackducksoftware/integration/hub/packman/packagemanager/nuget/NugetInspectorPackager.groovy
@@ -1,3 +1,14 @@
+/*
+ * Copyright (C) 2017 Black Duck Software Inc.
+ * http://www.blackducksoftware.com/
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * Black Duck Software ("Confidential Information"). You shall not
+ * disclose such Confidential Information and shall use it only in
+ * accordance with the terms of the license agreement you entered into
+ * with Black Duck Software.
+ */
 package com.blackducksoftware.integration.hub.packman.packagemanager.nuget
 
 import java.nio.charset.StandardCharsets
@@ -31,7 +42,14 @@ class NugetInspectorPackager {
     DependencyNode makeDependencyNode(String sourcePath) {
         def outputDirectory = new File(packmanProperties.outputDirectoryPath)
         def dependencyNodeFile = new File(outputDirectory, 'dependencyNodes.json')
-        def nugetInspectorExe = new File(getClass().getResource('nuget_inspector.exe').toURI())
+        def nugetInspectorExe = new File(getClass().getResource('nuget/HubNugetInspecter.exe').toURI())
+
+        File hubInspectorNupkg = fetchFromNuget(outputDirectory, 'HubNugetInspector')
+        if(hubInspectorNupkg) {
+            // TODO: We need to extract the executable from the nupkg file
+            // nugetInspectorExe = new File()
+        }
+
         String command = "${nugetInspectorExe.getAbsolutePath()} --target_path=${sourcePath} --output_directory=${outputDirectory.getAbsolutePath()}"
         command.execute()
         String dependencyNodeJson = dependencyNodeFile.getText(StandardCharsets.UTF_8.name())
@@ -46,7 +64,39 @@ class NugetInspectorPackager {
         String version = node.version
         ExternalId externalId = new NameVersionExternalId(Forge.nuget, name, version)
         DependencyNode dependencyNode = new DependencyNode(name, version, externalId)
-        node.children.each { dependencyNode.children.add(nugetNodeTransformer(it)) }
+        node.children.each {
+            dependencyNode.children.add(nugetNodeTransformer(it))
+        }
         dependencyNode
+    }
+
+    File fetchFromNuget(File outputDirectory, String nugetPackageName) {
+        def outputFile = new File(outputDirectory, "${nugetPackageName}.nupkg")
+        URL v1 = new URL("https://www.nuget.org/api/v1/package/${nugetPackageName}")
+        URL v2 = new URL("https://www.nuget.org/api/v2/package/${nugetPackageName}")
+        File downloadedFile
+        try{
+            downloadedFile = downloadFromUrl(outputFile, v2)
+        }catch (IOException e) {
+            logger.info("Failed to download package from ${v2.toString()}")
+        }
+        if(!downloadedFile) {
+            try{
+                downloadedFile = downloadFromUrl(outputFile, v1)
+            }catch (IOException e) {
+                logger.info("Failed to download package from ${v1.toString()}")
+            }
+        }
+        return downloadFromUrl
+    }
+
+
+    void downloadFromUrl(File outputFile, URL url) throws IOException {
+        logger.info("Attempting to download file from ${url.toString()}")
+        InputStream fileStream = url.openStream()
+        def outputStream = new FileOutputStream(outputFile)
+        outputStream.write(fileStream.getBytes())
+        outputStream.close()
+        logger.info("Succesfully downloaded file from ${url.toString()} to ${outputFile.getAbsolutePath()}")
     }
 }
