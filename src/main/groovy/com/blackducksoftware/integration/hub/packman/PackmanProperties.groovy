975012e8c011098ac89f9dec41332911d6ac7857
jrichard <jrichard@10.1.7.59>
2017-05-25 16:10:05 -0400

    Updating property descriptions

--- src/main/groovy/com/blackducksoftware/integration/hub/packman/PackmanProperties.groovy
+++ src/main/groovy/com/blackducksoftware/integration/hub/packman/PackmanProperties.groovy
@@ -6,11 +6,15 @@ import org.apache.commons.lang3.StringUtils
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
+import com.blackducksoftware.integration.hub.packman.help.ValueDescription
+
 @Component
 class PackmanProperties {
+    @ValueDescription(key = "packman.source.paths", description = "Source paths to inspect")
     @Value('${packman.source.paths}')
     String[] sourcePaths
 
+    @ValueDescription(key = "packman.output.path", description = "Output path")
     @Value('${packman.output.path}')
     String outputDirectoryPath
 
