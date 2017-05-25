54a1fa0bd3e50655af6774fa3a80893a4f7b6181
jrichardBD <psantos@blackducksoftware.com>
2017-05-25 16:52:05 -0400

    Fixing NugetInspectorPackager

--- src/main/groovy/com/blackducksoftware/integration/hub/packman/packagemanager/nuget/NugetInspectorPackager.groovy
+++ src/main/groovy/com/blackducksoftware/integration/hub/packman/packagemanager/nuget/NugetInspectorPackager.groovy
@@ -28,89 +28,98 @@ import com.blackducksoftware.integration.hub.packman.util.command.CommandRunner
 
 @Component
 class NugetInspectorPackager {
-    private final Logger logger = LoggerFactory.getLogger(NugetInspectorPackager.class)
+	private final Logger logger = LoggerFactory.getLogger(NugetInspectorPackager.class)
 
-    @Autowired
-    PackmanProperties packmanProperties
+	@Autowired
+	PackmanProperties packmanProperties
 
-    @Autowired
-    FileFinder fileFinder
+	@Autowired
+	FileFinder fileFinder
 
-    @Autowired
-    CommandRunner commandRunner
+	@Autowired
+	CommandRunner commandRunner
 
-    @Autowired
-    NugetNodeTransformer nugetNodeTransformer
+	@Autowired
+	NugetNodeTransformer nugetNodeTransformer
 
-    @Value('${packman.nuget.inspector.name}')
-    String inspectorPackageName
+	@Value('${packman.nuget.inspector.name}')
+	String inspectorPackageName
 
-    @Value('${packman.nuget.inspector.version}')
-    String inspectorPackageVersion
+	@Value('${packman.nuget.inspector.version}')
+	String inspectorPackageVersion
 
-    @Value('${packman.nuget.excluded.modules}')
-    String inspectorExcludedModules
+	@Value('${packman.nuget.excluded.modules}')
+	String inspectorExcludedModules
 
-    @Value('${packman.nuget.ignore.failure}')
-    boolean inspectorIgnoreFailure
+	@Value('${packman.nuget.ignore.failure}')
+	boolean inspectorIgnoreFailure
 
-    @PostConstruct
-    void init() {
-        inspectorPackageName = inspectorPackageName.trim()
-        inspectorPackageVersion = inspectorPackageVersion.trim()
-    }
+	@PostConstruct
+	void init() {
+		inspectorPackageName = inspectorPackageName.trim()
+		inspectorPackageVersion = inspectorPackageVersion.trim()
+	}
 
-    DependencyNode makeDependencyNode(String sourcePath, File nugetCommand) {
-        def outputDirectory = new File(packmanProperties.outputDirectoryPath)
-        def sourceDirectory = new File(sourcePath)
-        String inspectorExePath = getInspectorExePath(sourceDirectory, outputDirectory, nugetCommand)
+	DependencyNode makeDependencyNode(String sourcePath, File nugetCommand) {
+		def outputDirectory = new File(packmanProperties.outputDirectoryPath)
+		def sourceDirectory = new File(sourcePath)
+		String inspectorExePath = getInspectorExePath(sourceDirectory, outputDirectory, nugetCommand)
 
-        if (!inspectorExePath) {
-            return null
-        }
+		if (!inspectorExePath) {
+			return null
+		}
 
-        String[] options =  [
-            "--target_path=${sourcePath}",
-            "--output_directory=${outputDirectory.getAbsolutePath()}",
-            "--ignore_failure=${inspectorIgnoreFailure}"
-        ]
-        if(inspectorExcludedModules) {
-            options += "--excluded_modules=${inspectorExcludedModules}"
-        }
-        if(logger.traceEnabled) {
-            options += "-v"
-        }
+		def options =  [
+			"--target_path=${sourcePath}",
+			"--output_directory=${outputDirectory.getAbsolutePath()}",
+			"--ignore_failure=${inspectorIgnoreFailure}"
+		]
+		if(inspectorExcludedModules) {
+			options += "--excluded_modules=${inspectorExcludedModules}"
+		}
+		if(logger.traceEnabled) {
+			options += "-v"
+		}
 
-        def hubNugetInspectorCommand = new Command(sourceDirectory, inspectorExePath, options)
-        CommandOutput commandOutput = commandRunner.executeLoudly(hubNugetInspectorCommand)
+		def hubNugetInspectorCommand = new Command(sourceDirectory, inspectorExePath, options)
+		CommandOutput commandOutput = commandRunner.executeLoudly(hubNugetInspectorCommand)
 
-        def dependencyNodeFile = fileFinder.findFile(outputDirectory, '*_dependency_node.json')
-        DependencyNode node = nugetNodeTransformer.parse(dependencyNodeFile)
-        dependencyNodeFile.delete()
-        return node
-    }
+		def dependencyNodeFile = fileFinder.findFile(outputDirectory, '*_dependency_node.json')
+		DependencyNode node = nugetNodeTransformer.parse(dependencyNodeFile)
+		dependencyNodeFile.delete()
+		return node
+	}
 
-    private String getInspectorExePath(File sourceDirectory, File outputDirectory, File nugetCommand) {
-        File inspectorVersionDirectory = new File(outputDirectory, "${inspectorPackageName}.${inspectorPackageVersion}")
-        File toolsDirectory = new File(inspectorVersionDirectory, 'tools')
-        File inspectorExe = new File(toolsDirectory, "${inspectorPackageName}.exe")
+	private String getInspectorExePath(File sourceDirectory, File outputDirectory, File nugetCommand) {
+		File inspectorVersionDirectory = new File(outputDirectory, "${inspectorPackageName}.${inspectorPackageVersion}")
+		File toolsDirectory = new File(inspectorVersionDirectory, 'tools')
+		File inspectorExe = new File(toolsDirectory, "${inspectorPackageName}.exe")
 
-        //if we can't find the inspector where we expect to, attempt to install it from nuget.org
-        if (inspectorExe == null || !inspectorExe.exists()) {
-            installInspectorFromNugetDotOrg(sourceDirectory, outputDirectory, nugetCommand)
-            inspectorExe = new File(toolsDirectory, "${inspectorPackageName}.exe")
-        }
+		//if we can't find the inspector where we expect to, attempt to install it from nuget.org
+		if (inspectorExe == null || !inspectorExe.exists()) {
+			installInspectorFromNugetDotOrg(sourceDirectory, outputDirectory, nugetCommand)
+			inspectorExe = new File(toolsDirectory, "${inspectorPackageName}.exe")
+		}
 
-        if (inspectorExe == null || !inspectorExe.exists()) {
-            logger.error("Could not find the ${inspectorPackageName} version:${inspectorPackageVersion} even after an install attempt.")
-            return null
-        }
+		if (inspectorExe == null || !inspectorExe.exists()) {
+			logger.error("Could not find the ${inspectorPackageName} version:${inspectorPackageVersion} even after an install attempt.")
+			return null
+		}
 
-        return inspectorExe.absolutePath
-    }
+		return inspectorExe.absolutePath
+	}
 
-    private CommandOutput installInspectorFromNugetDotOrg(File sourceDirectory, File outputDirectory, File nugetCommand) {
-        Command installCommand = new Command(sourceDirectory, nugetCommand.absolutePath, 'install', inspectorPackageName, '-Version', inspectorPackageVersion, '-OutputDirectory', outputDirectory.absolutePath)
-        commandRunner.executeLoudly(installCommand)
-    }
+	private CommandOutput installInspectorFromNugetDotOrg(File sourceDirectory, File outputDirectory, File nugetCommand) {
+		def options =  [
+			'install',
+			inspectorPackageName,
+			'-Version',
+			inspectorPackageVersion,
+			'-OutputDirectory',
+			outputDirectory.absolutePath
+		]
+
+		Command installCommand = new Command(sourceDirectory, nugetCommand.absolutePath, options)
+		commandRunner.executeLoudly(installCommand)
+	}
 }
