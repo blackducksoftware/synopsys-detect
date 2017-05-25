0b984673ce3c4ebb56b13fd5a5e8c46224e15df4
jrichard <jrichard@10.1.7.59>
2017-05-25 11:28:46 -0400

    Updating the annotation

--- src/main/groovy/com/blackducksoftware/integration/hub/packman/help/ValueDescription.java
+++ src/main/groovy/com/blackducksoftware/integration/hub/packman/help/ValueDescription.java
@@ -20,7 +20,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.FIELD)
 public @interface ValueDescription {
 
-    String key() default "";
+    String key();
 
     String description() default "";
 }
