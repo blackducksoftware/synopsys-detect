3343916ade6b957ba68366b0d28760e7af6db8ce
jrichard <jrichard@10.1.7.59>
2017-05-31 08:15:09 -0400

    Fixing pip package manager

--- src/main/groovy/com/blackducksoftware/integration/hub/packman/packagemanager/PipPackageManager.groovy
+++ src/main/groovy/com/blackducksoftware/integration/hub/packman/packagemanager/PipPackageManager.groovy
@@ -135,7 +135,7 @@ class PipPackageManager extends PackageManager {
         return map['Location'].trim()
     }
 
-    private String findExecutable(String path, String executablePath, String commandType) {
+    private String findExecutable(String path, String executablePath, ExecutableType commandType) {
         if (StringUtils.isNotBlank(executablePath)) {
             executablePath
         } else {
