57da0090faae6089cc5e30a9e70a38c0e83d33f8
bamandel <bamandel@gmail.com>
2017-06-12 13:38:57 -0400

    forgot to add LoggerFactory class to a file.

--- src/main/groovy/com/blackducksoftware/integration/hub/detect/bomtool/npm/NpmCliDependencyFinder.groovy
+++ src/main/groovy/com/blackducksoftware/integration/hub/detect/bomtool/npm/NpmCliDependencyFinder.groovy
@@ -23,6 +23,7 @@
 package com.blackducksoftware.integration.hub.detect.bomtool.npm
 
 import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
