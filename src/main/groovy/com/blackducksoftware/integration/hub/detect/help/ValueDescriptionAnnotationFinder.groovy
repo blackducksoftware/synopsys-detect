7ce57a346578e0573a56b9926f3dbd674b4c980d
jrichard <jrichard@blackducksoftware.com>
2017-06-26 10:53:54 -0400

    Fixing annotation initialization

--- src/main/groovy/com/blackducksoftware/integration/hub/detect/help/ValueDescriptionAnnotationFinder.groovy
+++ src/main/groovy/com/blackducksoftware/integration/hub/detect/help/ValueDescriptionAnnotationFinder.groovy
@@ -22,8 +22,6 @@
  */
 package com.blackducksoftware.integration.hub.detect.help
 
-import javax.annotation.PostConstruct
-
 import org.apache.commons.lang3.StringUtils
 import org.apache.commons.lang3.math.NumberUtils
 import org.slf4j.Logger
@@ -48,7 +46,6 @@ public class ValueDescriptionAnnotationFinder implements ApplicationContextAware
         this.applicationContext = applicationContext
     }
 
-    @PostConstruct
     public void init() {
         Map<String, DetectOption> detectOptionsMap = [:]
         applicationContext.beanDefinitionNames.each { beanName ->
