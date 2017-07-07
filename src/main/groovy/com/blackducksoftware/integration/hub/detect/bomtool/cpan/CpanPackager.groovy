27c31ef2cafba6a6114ecb7fa4816a61233ee1d4
Jake Mathews <mathewsj2@wit.edu>
2017-07-07 15:55:20 -0400

    Formatting

--- src/main/groovy/com/blackducksoftware/integration/hub/detect/bomtool/cpan/CpanPackager.groovy
+++ src/main/groovy/com/blackducksoftware/integration/hub/detect/bomtool/cpan/CpanPackager.groovy
@@ -56,8 +56,6 @@ class CpanPackager {
                 logger.info("Could node find resolved version for module: ${moduleName}")
             }
         }
-        
-        
 
         dependencyNodes
     }
