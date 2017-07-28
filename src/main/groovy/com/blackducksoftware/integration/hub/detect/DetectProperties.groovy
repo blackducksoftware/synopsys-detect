22ca173c9857eb5a224200d95ae9ee96c3bd8e63
jrichard <jrichard@blackducksoftware.com>
2017-07-28 11:14:53 -0400

    Updating nuget inspector version

--- src/main/groovy/com/blackducksoftware/integration/hub/detect/DetectProperties.groovy
+++ src/main/groovy/com/blackducksoftware/integration/hub/detect/DetectProperties.groovy
@@ -175,7 +175,7 @@ class DetectProperties {
     @Value('${detect.nuget.inspector.name}')
     String nugetInspectorPackageName
 
-    @ValueDescription(description="Version of the Nuget Inspector", defaultValue="1.0.0", group=DetectProperties.GROUP_NUGET)
+    @ValueDescription(description="Version of the Nuget Inspector", defaultValue="1.1.0", group=DetectProperties.GROUP_NUGET)
     @Value('${detect.nuget.inspector.version}')
     String nugetInspectorPackageVersion
 
