6c61c6831ba0aa44e7e69022128cf2a392806094
BDS-AD\jpiscitelli <jpiscitelli@blackducksoftware.com>
2017-08-28 18:18:00 -0400

    Nuget package id ignores the case of the name and version when comparing.

--- src/main/groovy/com/blackducksoftware/integration/hub/detect/bomtool/nuget/model/NugetPackageId.groovy
+++ src/main/groovy/com/blackducksoftware/integration/hub/detect/bomtool/nuget/model/NugetPackageId.groovy
@@ -31,12 +31,15 @@ class NugetPackageId {
     @SerializedName('Name')
     String name
 
+    @SerializedName('Version')
+    String version
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((name == null) ? 0 : name.hashCode());
-        result = prime * result + ((version == null) ? 0 : version.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.toLowerCase().hashCode());
+        result = prime * result + ((version == null) ? 0 : version.toLowerCase().hashCode());
         return result;
     }
 
@@ -53,21 +56,19 @@ class NugetPackageId {
             if (other.name != null) {
                 return false;
             }
-        } else if (!name.equals(other.name)) {
+        } else if (!name.equalsIgnoreCase(other.name)) {
             return false;
         }
         if (version == null) {
             if (other.version != null) {
                 return false;
             }
-        } else if (!version.equals(other.version)) {
+        } else if (!version.equalsIgnoreCase(other.version)) {
             return false;
         }
         return true;
     }
 
-    @SerializedName('Version')
-    String version
 
     @Override
     String toString() {
