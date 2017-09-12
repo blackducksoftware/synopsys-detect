8ead6f5dedcc141d80a6ec450c979684f35ce775
jrichard <jrichard@blackducksoftware.com>
2017-09-12 13:41:46 -0400

    Fixing Nuget issues

--- src/main/groovy/com/blackducksoftware/integration/hub/detect/DetectProperties.groovy
+++ src/main/groovy/com/blackducksoftware/integration/hub/detect/DetectProperties.groovy
@@ -190,7 +190,7 @@ class DetectProperties {
     @Value('${detect.nuget.inspector.name}')
     String nugetInspectorPackageName
 
-    @ValueDescription(description="Version of the Nuget Inspector", defaultValue="2.1.0", group=DetectProperties.GROUP_NUGET)
+    @ValueDescription(description="Version of the Nuget Inspector", defaultValue="2.1.1", group=DetectProperties.GROUP_NUGET)
     @Value('${detect.nuget.inspector.version}')
     String nugetInspectorPackageVersion
 
