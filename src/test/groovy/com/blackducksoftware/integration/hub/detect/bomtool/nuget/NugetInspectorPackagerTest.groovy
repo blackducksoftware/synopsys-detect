5754b0483ca990bd70a1eca25fe2f07998f099a2
BDS-AD\jpiscitelli <jpiscitelli@blackducksoftware.com>
2017-10-03 12:19:25 -0400

    Fixing nuget tests.

--- src/test/groovy/com/blackducksoftware/integration/hub/detect/bomtool/nuget/NugetInspectorPackagerTest.groovy
+++ src/test/groovy/com/blackducksoftware/integration/hub/detect/bomtool/nuget/NugetInspectorPackagerTest.groovy
@@ -3,7 +3,6 @@ package com.blackducksoftware.integration.hub.detect.bomtool.nuget
 import static org.junit.Assert.*
 
 import org.junit.Test
-import org.skyscreamer.jsonassert.JSONAssert
 
 import com.blackducksoftware.integration.hub.bdio.BdioNodeFactory
 import com.blackducksoftware.integration.hub.bdio.BdioPropertyHelper
@@ -11,9 +10,11 @@ import com.blackducksoftware.integration.hub.bdio.graph.DependencyGraphTransform
 import com.blackducksoftware.integration.hub.bdio.model.BdioComponent
 import com.blackducksoftware.integration.hub.bdio.model.BdioProject
 import com.blackducksoftware.integration.hub.bdio.model.Forge
+import com.blackducksoftware.integration.hub.bdio.model.externalid.ExternalId
 import com.blackducksoftware.integration.hub.bdio.model.externalid.ExternalIdFactory
 import com.blackducksoftware.integration.hub.detect.model.DetectCodeLocation
 import com.blackducksoftware.integration.hub.detect.nameversion.NameVersionNodeTransformer
+import com.blackducksoftware.integration.hub.detect.testutils.DependencyGraphTestUtil
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 
@@ -23,22 +24,34 @@ public class NugetInspectorPackagerTest {
     @Test
     public void createCodeLocationLDServiceDashboard() throws IOException {
         def dependencyNodeFile = new File(getClass().getResource("/nuget/LDService.Dashboard_inspection.json").getFile())
-        def expectedOutputFile = new File(getClass().getResource("/nuget/LDService.Dashboard_Output.json").getFile())
-        createCodeLocation(dependencyNodeFile, expectedOutputFile)
+        def expectedOutputFiles = new ArrayList<String>();
+        expectedOutputFiles.add("/nuget/LDService.Dashboard_Output_0_graph.json");
+        createCodeLocation(dependencyNodeFile, expectedOutputFiles)
     }
 
     @Test
     public void createCodeLocationLDService() throws IOException {
         def dependencyNodeFile = new File(getClass().getResource("/nuget/LDService_inspection.json").getFile())
-        def expectedOutputFile = new File(getClass().getResource("/nuget/LDService_Output.json").getFile())
-        createCodeLocation(dependencyNodeFile, expectedOutputFile)
+        def expectedOutputFiles = new ArrayList<String>();
+        expectedOutputFiles.add("/nuget/LDService_Output_0_graph.json");
+        expectedOutputFiles.add("/nuget/LDService_Output_1_graph.json");
+        expectedOutputFiles.add("/nuget/LDService_Output_2_graph.json");
+        expectedOutputFiles.add("/nuget/LDService_Output_3_graph.json");
+        expectedOutputFiles.add("/nuget/LDService_Output_4_graph.json");
+        expectedOutputFiles.add("/nuget/LDService_Output_5_graph.json");
+        expectedOutputFiles.add("/nuget/LDService_Output_6_graph.json");
+        expectedOutputFiles.add("/nuget/LDService_Output_7_graph.json");
+        expectedOutputFiles.add("/nuget/LDService_Output_8_graph.json");
+        expectedOutputFiles.add("/nuget/LDService_Output_9_graph.json");
+        expectedOutputFiles.add("/nuget/LDService_Output_10_graph.json");
+        expectedOutputFiles.add("/nuget/LDService_Output_11_graph.json");
+        expectedOutputFiles.add("/nuget/LDService_Output_12_graph.json");
+        createCodeLocation(dependencyNodeFile, expectedOutputFiles)
     }
 
     @Test(timeout=5000l)
     public void createCodeLocationDWService() throws IOException {
         def dependencyNodeFile = new File(getClass().getResource("/nuget/dwCheckApi_inspection_martin.json").getFile())
-        def expectedOutputFile = new File(getClass().getResource("/nuget/LDService_Output.json").getFile())
-        //createCodeLocation(dependencyNodeFile, expectedOutputFile)
         NameVersionNodeTransformer nameVersionNodeTransformer = new NameVersionNodeTransformer()
         nameVersionNodeTransformer.externalIdFactory = new ExternalIdFactory()
         def packager = new NugetInspectorPackager()
@@ -53,15 +66,19 @@ public class NugetInspectorPackagerTest {
 
             DependencyGraphTransformer dependencyNodeTransformer = new DependencyGraphTransformer(bdioNodeFactory, bdioPropertyHelper)
 
-            final BdioProject project = bdioNodeFactory.createProject(codeLocation.bomToolProjectName, codeLocation.bomToolProjectVersionName, Forge.NUGET.toString(), codeLocation.bomToolProjectExternalId)
+            def projectId = bdioPropertyHelper.createExternalIdentifier(codeLocation.bomToolProjectExternalId);
+            final BdioProject project = bdioNodeFactory.createProject(codeLocation.bomToolProjectName, codeLocation.bomToolProjectVersionName, Forge.NUGET.toString(), projectId)
 
-            final List<BdioComponent> bdioComponents = dependencyNodeTransformer.addComponentsGraph(project, codeLocation.dependencyGraph)
+            Map<ExternalId, BdioComponent> components = new HashMap<>();
+            components.put(codeLocation.bomToolProjectExternalId, project);
+
+            final List<BdioComponent> bdioComponents = dependencyNodeTransformer.transformDependencyGraph(codeLocation.dependencyGraph, project, codeLocation.dependencyGraph.getRootDependencies(), components)
 
             assertEquals(bdioComponents.size(), bdioComponents.size())
         }
     }
 
-    private void createCodeLocation(File dependencyNodeFile, File expectedOutputFile) throws IOException {
+    private void createCodeLocation(File dependencyNodeFile, List<String> expectedOutputFiles) throws IOException {
         NameVersionNodeTransformer nameVersionNodeTransformer = new NameVersionNodeTransformer()
         nameVersionNodeTransformer.externalIdFactory = new ExternalIdFactory()
         def packager = new NugetInspectorPackager()
@@ -70,10 +87,14 @@ public class NugetInspectorPackagerTest {
         packager.nameVersionNodeTransformer = nameVersionNodeTransformer
         packager.externalIdFactory = nameVersionNodeTransformer.externalIdFactory
 
-        List<DetectCodeLocation> codeLocation = packager.createDetectCodeLocation(dependencyNodeFile)
-        String actual = gson.toJson(codeLocation)
-        String expected = expectedOutputFile.text
+        List<DetectCodeLocation> codeLocations = packager.createDetectCodeLocation(dependencyNodeFile)
 
-        JSONAssert.assertEquals(expected, actual, false)
+        for (def i = 0; i < expectedOutputFiles.size(); i++){
+            def codeLocation = codeLocations[i];
+            def expectedOutputFile = expectedOutputFiles[i];
+
+
+            DependencyGraphTestUtil.assertGraph(expectedOutputFile, codeLocation.dependencyGraph);
+        }
     }
 }
