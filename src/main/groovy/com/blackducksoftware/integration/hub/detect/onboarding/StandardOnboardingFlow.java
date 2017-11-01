6bd7f3fc38d5f85b3f1e9820be2e2dec1c71fd62
BDS-AD\jpiscitelli <jpiscitelli@blackducksoftware.com>
2017-11-01 13:20:27 -0400

    Removing possibly extraneous logs from onboarding.

--- src/main/groovy/com/blackducksoftware/integration/hub/detect/onboarding/StandardOnboardingFlow.java
+++ src/main/groovy/com/blackducksoftware/integration/hub/detect/onboarding/StandardOnboardingFlow.java
@@ -23,12 +23,11 @@ public class StandardOnboardingFlow {
 
         onboarder.printReady();
 
-        final Boolean hubConnect = onboarder.askYesOrNo("Would you like to connect to a Hub Instance?");
-        if (hubConnect == true) {
+        final Boolean connectToHub = onboarder.askYesOrNo("Would you like to connect to a Hub Instance?");
+        if (connectToHub == true) {
             onboarder.askFieldQuestion("hubUrl", "What is the hub instance url?");
             onboarder.askFieldQuestion("hubUsername", "What is the hub username?");
             onboarder.askFieldQuestion("hubPassword", "What is the hub password?");
-            onboarder.println("Hub information updated.");
 
             final Boolean useProxy = onboarder.askYesOrNo("Would you like to configure a proxy for the hub?");
             if (useProxy) {
@@ -36,44 +35,38 @@ public class StandardOnboardingFlow {
                 onboarder.askFieldQuestion("hubProxyPort", "What is the hub proxy port?");
                 onboarder.askFieldQuestion("hubProxyUsername", "What is the hub proxy username?");
                 onboarder.askFieldQuestion("hubProxyPassword", "What is the hub proxy password?");
-                onboarder.println("Hub proxy updated.");
             }
 
             final Boolean trustCert = onboarder.askYesOrNo("Would you like to automatically trust the hub certificate?");
             if (trustCert) {
                 onboarder.setField("hubTrustCertificate", "true");
-                onboarder.println("Hub certificate updated.");
             }
 
-            final Boolean hubName = onboarder.askYesOrNo("Would you like to provide a project name and version to use on the hub?");
-            if (hubName) {
+            final Boolean customDetails = onboarder.askYesOrNo("Would you like to provide a project name and version to use on the hub?");
+            if (customDetails) {
                 onboarder.askFieldQuestion("projectName", "What is the hub project name?");
                 onboarder.askFieldQuestion("projectVersionName", "What is the hub project version?");
-                onboarder.println("Project information updated.");
             }
 
         } else {
-            onboarder.println("Setting detect to offline mode.");
             onboarder.setField("hubOfflineMode", "true");
         }
 
         final Boolean scan = onboarder.askYesOrNo("Would you like run a CLI scan?");
         if (!scan) {
-            onboarder.println("Disabling signature scanner.");
             onboarder.setField("hubSignatureScannerDisabled", "true");
-        } else if (scan && hubConnect) {
+        } else if (scan && connectToHub) {
             final Boolean upload = onboarder.askYesOrNo("Would you like to upload CLI scan results to the hub?");
             if (!upload) {
-                onboarder.println("Setting signature scanner to dry run.");
                 onboarder.setField("hubSignatureScannerDryRun", "true");
             }
         }
 
         if (scan) {
-            final Boolean custom = onboarder.askYesOrNo("Would you like to provide a custom scanner?");
-            if (custom) {
-                final Boolean download = onboarder.askYesOrNo("Would you like to download the custom scanner?");
-                if (download) {
+            final Boolean customScanner = onboarder.askYesOrNo("Would you like to provide a custom scanner?");
+            if (customScanner) {
+                final Boolean downloadCustomScanner = onboarder.askYesOrNo("Would you like to download the custom scanner?");
+                if (downloadCustomScanner) {
                     onboarder.askFieldQuestion("hubSignatureScannerHostUrl", "What is the scanner host url?");
                 } else {
                     onboarder.askFieldQuestion("hubSignatureScannerOfflineLocalPath", "What is the location of your offline scanner?");
