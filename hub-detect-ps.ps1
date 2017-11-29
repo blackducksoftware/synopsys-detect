36b54ae4f726aafe5d7d3e9d0fd3fd9e561aa57a
BDS-AD\jpiscitelli <jpiscitelli@blackducksoftware.com>
2017-11-28 19:31:45 -0500

    Powershell changes.

--- hub-detect-ps.ps1
+++ hub-detect-ps.ps1
@@ -1,67 +1,64 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
+$Version = "1.0.0"
 
 function Detect {
-    $SCRIPT_ARGS = $args;
-    $DETECT = Get-Detect;
-    Invoke-Detect -DetectJar $DETECT -ScriptArgs $SCRIPT_ARGS
+    Write-Host "Detect Powershell Script $Version"
+    $DetectArgs = $args;
+    $DetectJar = Get-Detect;
+    $DetectExitCode = Invoke-Detect -DetectJar $DetectJar -DetectArgs $DetectArgs
+    exit $DetectExitCode
 }
 
-function Invoke-Detect ($DetectJar, $ScriptArgs) {
-    $JAVA_ARGUMENTS = @("-jar", $DetectJar)
-    $DETECT_ARGUMENTS =  $JAVA_ARGUMENTS + $ScriptArgs
-    Write-Host "Executing detect with parameters $DetectJar"
-    Write-Host "Executing detect with parameters $JAVA_ARGUMENTS"
-    Write-Host "Executing detect with parameters $DETECT_ARGUMENTS"
-    $DetectProcess = Start-Process java -ArgumentList $DETECT_ARGUMENTS -NoNewWindow -Wait
-    $ExitCode = $DetectProcess.ExitCode;
-    Write-Host "Result code of $ExitCode, exiting"
-    exit $ExitCode
+function Invoke-Detect ($DetectJar, $DetectArgs) {
+    $JavaArgs = @("-jar", $DetectJar)
+    $AllArgs =  $JavaArgs + $DetectArgs
+    Write-Host "Running detect: $AllArgs"
+    $DetectProcess = Start-Process java -ArgumentList $AllArgs -NoNewWindow -Wait
+    $DetectExitCode = $DetectProcess.ExitCode;
+    Write-Host "Result code of $DetectExitCode, exiting"
+    return $DetectExitCode    
 }
 
 function Get-Detect () {
-    Write-Host "Downloading Detect"
-
-    $DETECT_RELEASE_VERSION = Get-EnvironmentVariable -Name "DETECT_RELEASE_VERSION" -DefaultValue "";
-    $DETECT_JAR_PATH = Get-EnvironmentVariable -Name "DETECT_JAR_PATH" -DefaultValue "$HOME\tmp";
-    $DETECT_JAVA_OPTS = Get-EnvironmentVariable -Name "DETECT_JAVA_OPTS" -DefaultValue "";
+    $DetectVersion = Get-EnvironmentVariable -Name "DetectVersion" -DefaultValue "";
+    $DetectJarFolder = Get-EnvironmentVariable -Name "DetectJarFolder" -DefaultValue "$HOME\tmp";
+    #$DETECT_JAVA_OPTS = Get-EnvironmentVariable -Name "DETECT_JAVA_OPTS" -DefaultValue "";
     #$DETECT_GET_OPTS = Get-EnvironmentVariable -Name "DETECT_GET_OPTS" -DefaultValue "";
 
-    Write-Host "Resolved Variables"
-    Write-Host "Detect Jar Path: $DETECT_JAR_PATH"
+    Write-Host "Checking for detect jar folder: $DetectJarFolder"
 
-    If(!(Test-Path $DETECT_JAR_PATH)) {
-        Write-Host "Creating detect jar directory $DETECT_JAR_PATH"
-        New-Item -ItemType Directory -Force -Path $DETECT_JAR_PATH
+    If(!(Test-Path $DetectJarFolder)) {
+        Write-Host "Creating detect jar folder: $DetectJarFolder"
+        New-Item -ItemType Directory -Force -Path $DetectJarFolder
     }
 
-    if ($DETECT_RELEASE_VERSION -eq "") {
-        Write-Host "Getting latest version."
-        $DETECT_RELEASE_VERSION = Invoke-WebRequest "https://test-repo.blackducksoftware.com/artifactory/api/search/latestVersion?g=com.blackducksoftware.integration&a=hub-detect&repos=bds-integrations-release"
-        Write-Host "Resolved version $DETECT_RELEASE_VERSION"
+    if ($DetectVersion -eq "") {
+        Write-Host "Finding latest detect version."
+        $DetectVersion = Invoke-WebRequest "https://test-repo.blackducksoftware.com/artifactory/api/search/latestVersion?g=com.blackducksoftware.integration&a=hub-detect&repos=bds-integrations-release"
+        Write-Host "Resolved version $DetectVersion"
     }
 
-    $DETECT_SOURCE="https://test-repo.blackducksoftware.com/artifactory/bds-integrations-release/com/blackducksoftware/integration/hub-detect/${DETECT_RELEASE_VERSION}/hub-detect-${DETECT_RELEASE_VERSION}.jar"
-    $DETECT_DESTINATION="$DETECT_JAR_PATH\hub-detect-$DETECT_RELEASE_VERSION.jar"
+    $DetectUrl="https://test-repo.blackducksoftware.com/artifactory/bds-integrations-release/com/blackducksoftware/integration/hub-detect/${DetectVersion}/hub-detect-${DetectVersion}.jar"
+    $DetectJarFile="$DetectJarFolder\hub-detect-$DetectVersion.jar"
 
-    $DETECT_EXISTS = Test-Path $DETECT_DESTINATION
+    Write-Host "Checking for existing detect jar file: $DetectJarFile"
+    $DetectJarExists = Test-Path $DetectJarFile
 
-    if (!$DETECT_EXISTS){
+    if (!$DetectJarExists){
         Write-Host "You don't have detect. Downloading now."
-        Write-Host "Downloading to $DETECT_DESTINATION"
-        Invoke-WebRequest $DETECT_SOURCE -OutFile $DETECT_DESTINATION
+        Invoke-WebRequest $DetectUrl -OutFile $DetectJarFile
+    }else{
+        Write-Host "Using existing jar file."
     }
 
-    return $DETECT_DESTINATION
+    return $DetectJarFile
 }
 
 
 function Get-EnvironmentVariable($Name, $DefaultValue) {
     if (-not (Test-Path Env:$Name)) {
-        Write-Host "Default"
         return $DefaultValue;
     }else{
-        Write-Host "Value"
         return Get-ChildItem Env:$Name;
     }
 }
