6d22313d2487cfdab969b72414a11fdeb2affecd
Builder Service <serv-builder@int-ubuntu-jenkins.dc1.lan>
2018-03-13 09:14:43 -0400

    Committing the latest update-site contents to gh-pages branch.

--- hub-detect.sh
+++ hub-detect.sh
@@ -38,6 +38,8 @@ for i in $*; do
     LOGGABLE_SCRIPT_ARGS="$LOGGABLE_SCRIPT_ARGS --blackduck.hub.password=<redacted>"
   elif [[ $i == --blackduck.hub.proxy.password=* ]]; then
     LOGGABLE_SCRIPT_ARGS="$LOGGABLE_SCRIPT_ARGS --blackduck.hub.proxy.password=<redacted>"
+  elif [[ $i == --blackduck.hub.api.token=* ]]; then
+    LOGGABLE_SCRIPT_ARGS="$LOGGABLE_SCRIPT_ARGS --blackduck.hub.api.token=<redacted>"
   else
     LOGGABLE_SCRIPT_ARGS="$LOGGABLE_SCRIPT_ARGS $i"
   fi
