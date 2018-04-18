71c3e1641cda902392bd08cb149a8a0f3c2c9932
Jordan Piscitelli <jordanp@synopsys.com>
2018-04-18 14:09:45 -0400

    Finishing go vndr search support.

--- src/main/groovy/com/blackducksoftware/integration/hub/detect/bomtool/go/vndr/GoVndrApplicableResult.java
+++ src/main/groovy/com/blackducksoftware/integration/hub/detect/bomtool/go/vndr/GoVndrApplicableResult.java
@@ -1,5 +1,21 @@
 package com.blackducksoftware.integration.hub.detect.bomtool.go.vndr;
 
-public class GoVndrApplicableResult {
+import java.io.File;
+
+import com.blackducksoftware.integration.hub.detect.bomtool.BomToolApplicableResult;
+import com.blackducksoftware.integration.hub.detect.model.BomToolType;
+
+public class GoVndrApplicableResult extends BomToolApplicableResult {
+    private final File vendorConf;
+
+    public GoVndrApplicableResult(final File searchedDirectory, final File vendorConf) {
+        super(searchedDirectory, BomToolType.GO_VNDR);
+        this.vendorConf = vendorConf;
+    }
+
+    public File getVenderConf() {
+        return vendorConf;
+    }
+
 
 }
--- src/main/groovy/com/blackducksoftware/integration/hub/detect/bomtool/go/vndr/GoVndrBomTool.groovy
+++ src/main/groovy/com/blackducksoftware/integration/hub/detect/bomtool/go/vndr/GoVndrBomTool.groovy
@@ -23,20 +23,18 @@
  */
 package com.blackducksoftware.integration.hub.detect.bomtool.go.vndr
 
-import com.blackducksoftware.integration.hub.detect.bomtool.BomTool
-
 import java.nio.charset.StandardCharsets
 import java.nio.file.Files
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
 import com.blackducksoftware.integration.hub.bdio.graph.DependencyGraph
 import com.blackducksoftware.integration.hub.bdio.model.Forge
 import com.blackducksoftware.integration.hub.bdio.model.externalid.ExternalId
-import com.blackducksoftware.integration.hub.bdio.model.externalid.ExternalIdFactory
+import com.blackducksoftware.integration.hub.detect.bomtool.BomTool
+import com.blackducksoftware.integration.hub.detect.bomtool.BomToolExtractionResult
 import com.blackducksoftware.integration.hub.detect.model.BomToolType
 import com.blackducksoftware.integration.hub.detect.model.DetectCodeLocation
 
@@ -44,13 +42,10 @@ import groovy.transform.TypeChecked
 
 @Component
 @TypeChecked
-class GoVndrBomTool extends BomTool {
+class GoVndrBomTool extends BomTool<GoVndrApplicableResult> {
     private final Logger logger = LoggerFactory.getLogger(GoVndrBomTool.class)
 
     public static final String VNDR_CONF_FILENAME= 'vendor.conf'
-
-    @Autowired
-    ExternalIdFactory externalIdFactory
 
     @Override
     public BomToolType getBomToolType() {
@@ -58,20 +53,25 @@ class GoVndrBomTool extends BomTool {
     }
 
     @Override
-    public boolean isBomToolApplicable() {
-        detectFileManager.containsAllFiles(sourcePath, VNDR_CONF_FILENAME)
+    public GoVndrApplicableResult isBomToolApplicable(File directory) {
+        def vendorConf = new File(directory, VNDR_CONF_FILENAME);
+        if (vendorConf.exists()) {
+            return new GoVndrApplicableResult(directory, vendorConf);
+        }
+        return null;
     }
 
-    List<DetectCodeLocation> extractDetectCodeLocations() {
+    BomToolExtractionResult extractDetectCodeLocations(GoVndrApplicableResult applicableResult) {
         File sourceDirectory = detectConfiguration.sourceDirectory
 
         VndrParser vndrParser = new VndrParser(externalIdFactory)
-        def vendorConf = new File(sourcePath, VNDR_CONF_FILENAME)
-        List<String> venderConfContents = Files.readAllLines(vendorConf.toPath(), StandardCharsets.UTF_8)
-        DependencyGraph dependencyGraph = vndrParser.parseVendorConf(venderConfContents)
-        ExternalId externalId = externalIdFactory.createPathExternalId(Forge.GOLANG, sourcePath)
 
-        def codeLocation = new DetectCodeLocation.Builder(getBomToolType(), sourcePath, externalId, dependencyGraph).build()
-        [codeLocation]
+        List<String> venderConfContents = Files.readAllLines(applicableResult.vendorConf.toPath(), StandardCharsets.UTF_8)
+        DependencyGraph dependencyGraph = vndrParser.parseVendorConf(venderConfContents)
+        ExternalId externalId = externalIdFactory.createPathExternalId(Forge.GOLANG, applicableResult.directoryString)
+
+        def codeLocation = new DetectCodeLocation.Builder(getBomToolType(), applicableResult.directoryString, externalId, dependencyGraph).build()
+
+        bomToolExtractionResultsFactory.fromCodeLocations([codeLocation], getBomToolType(), applicableResult.directory);
     }
 }
