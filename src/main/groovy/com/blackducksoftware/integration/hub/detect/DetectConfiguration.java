7cab60b4633dcaeda2f52d3f9f578cdf28b9c7c9
jrichard <jrichard@synopsys.com>
2018-04-27 15:10:46 -0400

    Removing wildcard import

--- src/main/groovy/com/blackducksoftware/integration/hub/detect/DetectConfiguration.java
+++ src/main/groovy/com/blackducksoftware/integration/hub/detect/DetectConfiguration.java
@@ -53,7 +53,11 @@ import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
