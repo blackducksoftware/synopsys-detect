3ad088e61abd725e578b88297918bd4a4b081155
Jordan Piscitelli <jordanp@synopsys.com>
2018-05-11 13:59:22 -0400

    Conda update.

--- src/main/groovy/com/blackducksoftware/integration/hub/detect/extraction/bomtool/conda/CondaCliLdStrategy.java
+++ src/main/groovy/com/blackducksoftware/integration/hub/detect/extraction/bomtool/conda/CondaCliLdStrategy.java
@@ -1,27 +1,11 @@
 package com.blackducksoftware.integration.hub.detect.extraction.bomtool.conda;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import com.blackducksoftware.integration.hub.detect.DetectConfiguration;
-import com.blackducksoftware.integration.hub.detect.extraction.StandardExecutableFinder;
 import com.blackducksoftware.integration.hub.detect.extraction.StandardExecutableFinder.StandardExecutableType;
 import com.blackducksoftware.integration.hub.detect.extraction.strategy.Strategy;
 import com.blackducksoftware.integration.hub.detect.model.BomToolType;
-import com.blackducksoftware.integration.hub.detect.util.DetectFileFinder;
 
-@Component
 public class CondaCliLdStrategy extends Strategy<CondaCliContext, CondaCliExtractor> {
     public static final String ENVIRONEMNT_YML = "environment.yml";
-
-    @Autowired
-    public DetectFileFinder fileFinder;
-
-    @Autowired
-    public StandardExecutableFinder standardExecutableFinder;
-
-    @Autowired
-    public DetectConfiguration detectConfiguration;
 
     public CondaCliLdStrategy() {
         super("Conda Cli", BomToolType.COCOAPODS, CondaCliContext.class, CondaCliExtractor.class);
