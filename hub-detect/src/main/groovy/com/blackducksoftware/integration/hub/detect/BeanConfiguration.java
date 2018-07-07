9f17c88b6e00bb06a38df8c035d7beb3f1ac3fa5
Eric Kerwin <ekerwin@kingofnerds.com>
2018-07-07 12:43:35 -0400

    using latest hub-common

--- hub-detect/src/main/groovy/com/blackducksoftware/integration/hub/detect/BeanConfiguration.java
+++ hub-detect/src/main/groovy/com/blackducksoftware/integration/hub/detect/BeanConfiguration.java
@@ -134,9 +134,10 @@ import com.blackducksoftware.integration.hub.detect.workflow.search.SearchSummar
 import com.blackducksoftware.integration.hub.detect.workflow.search.rules.BomToolSearchProvider;
 import com.blackducksoftware.integration.hub.detect.workflow.summary.DetectSummaryManager;
 import com.blackducksoftware.integration.hub.detect.workflow.summary.StatusSummaryProvider;
+import com.blackducksoftware.integration.hub.service.HubServicesFactory;
 import com.blackducksoftware.integration.util.IntegrationEscapeUtil;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+import com.google.gson.JsonParser;
 
 import freemarker.template.Configuration;
 
@@ -151,7 +152,12 @@ public class BeanConfiguration {
 
     @Bean
     public Gson gson() {
-        return new GsonBuilder().setPrettyPrinting().create();
+        return HubServicesFactory.createDefaultGsonBuilder().setPrettyPrinting().create();
+    }
+
+    @Bean
+    public JsonParser jsonParser() {
+        return new JsonParser();
     }
 
     @Bean
@@ -250,7 +256,7 @@ public class BeanConfiguration {
 
     @Bean
     public HubServiceWrapper hubServiceWrapper() {
-        return new HubServiceWrapper(detectConfigWrapper(), additionalPropertyConfig());
+        return new HubServiceWrapper(detectConfigWrapper(), additionalPropertyConfig(), gson(), jsonParser());
     }
 
     @Bean
