3ea6657cf56136588830c230669c65ccd076089f
Jordan Piscitelli <jordanp@synopsys.com>
2018-07-25 14:13:32 -0400

    Moving relative code to common location and improving aggregation.

---
+++ hub-detect/src/main/groovy/com/blackducksoftware/integration/hub/detect/workflow/codelocation/FileNameUtils.java
@@ -0,0 +1,35 @@
+package com.blackducksoftware.integration.hub.detect.workflow.codelocation;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FileNameUtils {
+    private static final Logger logger = LoggerFactory.getLogger(FileNameUtils.class);
+
+    public static String relativize(final String from, final String to) {
+        String relative = to;
+        try {
+            final Path toPath = new File(to).toPath();
+            final Path fromPath = new File(from).toPath();
+            final Path fromParentPath = fromPath.getParent();
+            final Path relativePath = fromParentPath.relativize(toPath);
+            final List<String> relativePieces = new ArrayList<>();
+            for (int i = 0; i < relativePath.getNameCount(); i++) {
+                relativePieces.add(relativePath.getName(i).toFile().getName());
+            }
+            relative = StringUtils.join(relativePieces, "/");
+        } catch (final Exception e) {
+            logger.info(String.format("Unable to relativize path, full source path will be used: %s", to));
+            logger.debug("The reason relativize failed: ", e);
+        }
+
+        return relative;
+    }
+
+}
