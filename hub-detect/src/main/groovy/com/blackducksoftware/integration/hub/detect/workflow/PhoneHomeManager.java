0578757755931bec15938049243a3530b112b164
Jordan Piscitelli <jordanp@synopsys.com>
2018-09-07 16:52:15 -0400

    Update PhoneHomeManager.java
    Fixing phone home meta data.

--- hub-detect/src/main/groovy/com/blackducksoftware/integration/hub/detect/workflow/PhoneHomeManager.java
+++ hub-detect/src/main/groovy/com/blackducksoftware/integration/hub/detect/workflow/PhoneHomeManager.java
@@ -128,7 +128,7 @@ public class PhoneHomeManager {
         if (null != phoneHomeService) {
             try {
                 final PhoneHomeCallable callable;
-                final Set<Entry<String, String>> additionalMetadata = detectConfiguration.getBlackduckProperties().entrySet();
+                final Set<Entry<String, String>> additionalMetadata = detectConfiguration.getPhoneHomeProperties().entrySet();
                 additionalMetadata.forEach(it -> metadata.put(it.getKey(), it.getValue()));
                 if (isBlackDuckOffline) {
                     callable = createOfflineCallable(metadata);
