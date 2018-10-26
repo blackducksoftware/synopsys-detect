6ce3a5cd9622db0e884eea9c87b5711c4ade40a6
Builder Service <serv-builder@int-ubuntu-jenkins02.dc1.lan>
2018-10-26 12:04:45 -0400

    Committing the latest update-site contents to gh-pages branch.

--- hub-detect.ps1
+++ hub-detect.ps1
@@ -1,50 +1,35 @@
 #Detect Powershell Script
 #Recommended Invocation: powershell "irm https://blackducksoftware.github.io/hub-detect/hub-detect.ps1?$(Get-Random) | iex; detect"
 
-function Get-EnvironmentVariable($Key, $DefaultValue) { if (-not (Test-Path Env:$Key)) { return $DefaultValue; }else { return (Get-ChildItem Env:$Key).Value; } }
+function Get-EnvironmentVariable($Key, $DefaultValue) { if (-not (Test-Path Env:$Key)) { return $DefaultValue; }else{ return (Get-ChildItem Env:$Key).Value; } }
 
 # DETECT_LATEST_RELEASE_VERSION should be set in your
 # environment if you wish to use a version different
 # from LATEST.
 $EnvDetectDesiredVersion = Get-EnvironmentVariable -Key "DETECT_LATEST_RELEASE_VERSION" -DefaultValue "";
 
-# To override the default version key, specify a
-# different DETECT_VERSION_KEY in your environment and
-# *that* key will be used to get the download url from
-# artifactory. These DETECT_VERSION_KEY values are
-# properties in Artifactory that resolve to download
-# urls for the detect jar file.As of 2018-10-10, the
-# available DETECT_VERSION_KEY values are:
-# (DETECT_LATEST, DETECT_LATEST_4, DETECT_LATEST_3)
-# and we plan to soon add a DETECT_LATEST_5. Every new
-# major version of detect will have its own
-# DETECT_LATEST_X key.
-$EnvDetectVersionKey = Get-EnvironmentVariable -Key "DETECT_VERSION_KEY" -DefaultValue "DETECT_LATEST";
+# If you would like to enable the shell script to use
+# the latest snapshot instead of the latest release,
+# specify DETECT_USE_SNAPSHOT=1 in your environment.
+# The default is to NOT use snapshots. If you enable
+# snapshots, the jar file will be downloaded whenever
+# a new commit is added to the master branch.
+$EnvDetectUseSnapshot = Get-EnvironmentVariable -Key "DETECT_USE_SNAPSHOT" -DefaultValue "0";
 
 # If you want to skip the test for java
 # DETECT_SKIP_JAVA_TEST=1
 $EnvDetectSkipJavaTest = Get-EnvironmentVariable -Key "DETECT_SKIP_JAVA_TEST" -DefaultValue "";
 
-# You can specify your own download url from
-# artifactory which can bypass using the property keys
-# (this is mainly for QA purposes only)
-$EnvDetectSource = Get-EnvironmentVariable -Key "DETECT_SOURCE" -DefaultValue "";
+# If you do not want to exit with the detect exit code,
+# set DETECT_EXIT_CODE_PASSTHRU to 1 and this script won't exit, but simply return it (pass it thru).
+$EnvDetectExitCodePassthru = Get-EnvironmentVariable -Key "DETECT_EXIT_CODE_PASSTHRU" -DefaultValue "";
 
-# To override the default location of /tmp, specify
-# your own DETECT_JAR_PATH in your environment and
-# *that* location will be used.
-# *NOTE* We currently do not support spaces in the
-# DETECT_JAR_PATH.
+# To override the default location specify your own DETECT_JAR_PATH
 # Otherwise, if the environment temp folder is set, it will be used.
 # Otherwise, a temporary folder will be created in your home directory
 $EnvDetectFolder = Get-EnvironmentVariable -Key "DETECT_JAR_PATH" -DefaultValue "";
 $EnvTempFolder = Get-EnvironmentVariable -Key "TMP" -DefaultValue "";
 $EnvHomeTempFolder = "$HOME/tmp"
-
-
-# If you do not want to exit with the detect exit code,
-# set DETECT_EXIT_CODE_PASSTHRU to 1 and this script won't exit, but simply return it (pass it thru).
-$EnvDetectExitCodePassthru = Get-EnvironmentVariable -Key "DETECT_EXIT_CODE_PASSTHRU" -DefaultValue "1";
 
 # If you want to pass proxy information, use detect environment variables such as 'blackduck.proxy.host'
 # If you do pass the proxy information in this way, you do not need to supply it to detect as arguments. 
@@ -59,27 +44,29 @@ $EnvDetectExitCodePassthru = Get-EnvironmentVariable -Key "DETECT_EXIT_CODE_PASS
 # heap size, you would set DETECT_JAVA_OPTS=-Xmx6G.
 #$DetectJavaOpts = Get-EnvironmentVariable -Key "DETECT_JAVA_OPTS" -DefaultValue "";
 
-$Version = "1.0.0"
+$Version = "0.7.0"
+
+$DetectReleaseBaseUrl = "https://test-repo.blackducksoftware.com/artifactory/bds-integrations-release/com/blackducksoftware/integration/hub-detect"
+$DetectSnapshotBaseUrl = "https://test-repo.blackducksoftware.com/artifactory/bds-integrations-snapshot/com/blackducksoftware/integration/hub-detect"
+$DetectCommitUrl = "https://blackducksoftware.github.io/hub-detect/latest-commit-id.txt"
+$DetectVersionUrl = "https://test-repo.blackducksoftware.com/artifactory/api/search/latestVersion?g=com.blackducksoftware.integration&a=hub-detect&repos=bds-integrations-release"
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 #Enable TLS2
 
 function Detect {
     Write-Host "Detect Powershell Script $Version"
     
-    if ($EnvDetectSkipJavaTest -ne "1") {
-        if (Test-JavaNotAvailable) {
-            #If java is not available, we abort early.
-            $JavaExitCode = 127 #Command not found http://tldp.org/LDP/abs/html/exitcodes.html 
-            if ($EnvDetectExitCodePassthru -eq "1") {
-                return $JavaExitCode
-            }
-            else {
-                exit $JavaExitCode
-            }
-        }
-    }
-    else {
-        Write-Host "Skipping java test."
+    if ($EnvDetectSkipJavaTest -ne "1"){
+    	if (Test-JavaNotAvailable){ #If java is not available, we abort early.
+			$JavaExitCode = 127 #Command not found http://tldp.org/LDP/abs/html/exitcodes.html 
+    		if ($EnvDetectExitCodePassthru -eq "1"){
+		        return $JavaExitCode
+		    }else{
+		    	exit $JavaExitCode
+		    }
+    	}
+    }else{
+   		Write-Host "Skipping java test."
     }
 
     Write-Host "Initializing detect folder."
@@ -89,53 +76,54 @@ function Detect {
     $ProxyInfo = Get-ProxyInfo
 
     Write-Host "Getting detect."
-    $DetectJarFile = Get-DetectJar -DetectFolder $DetectFolder -DetectSource $EnvDetectSource -DetectVersionKey $EnvDetectVersionKey -DetectVersion $EnvDetectDesiredVersion -ProxyInfo $ProxyInfo
+    if ($EnvDetectUseSnapshot -eq "1"){
+        $DetectJarFile = Get-DetectSnapshotJar -DetectFolder $DetectFolder -DetectVersion $EnvDetectDesiredVersion -ProxyInfo $ProxyInfo
+    }else{
+        $DetectJarFile = Get-DetectJar -DetectFolder $DetectFolder -DetectVersion $EnvDetectDesiredVersion -ProxyInfo $ProxyInfo
+    }
 
     Write-Host "Executing detect."
     $DetectArgs = $args;
     $DetectExitCode = Invoke-Detect -DetectJar $DetectJarFile -DetectArgs $DetectArgs
     
-    if ($EnvDetectExitCodePassthru -eq "1") {
+    if ($EnvDetectExitCodePassthru -eq "1"){
         return $DetectExitCode
-    }
-    else {
-        exit $DetectExitCode
+    }else{
+    	exit $DetectExitCode
     }
 }
 
 function Get-FirstFromEnv($Names) {
-    foreach ($Name in $Names) {
-        $Value = Get-EnvironmentVariable -Key $Name -Default $null;
-        if (-Not [string]::IsNullOrEmpty($Value)) {
-            return $Value;
-        }
-    }
-    return $null;
+	foreach ($Name in $Names){
+		$Value = Get-EnvironmentVariable -Key $Name -Default $null;
+		if (-Not [string]::IsNullOrEmpty($Value)){
+			return $Value;
+		}
+	}
+	return $null;
 }
 
 function Get-ProxyInfo () {
     $ProxyInfoProperties = @{
-        'Uri'         = $null
-        'Credentials' = $null
+        'Uri'=$null
+        'Credentials'=$null
     }
 
     try {
 
         $ProxyHost = Get-FirstFromEnv @("blackduck.proxy.host", "BLACKDUCK_PROXY_HOST", "blackduck.hub.proxy.host", "BLACKDUCK_HUB_PROXY_HOST");
         
-        if ([string]::IsNullOrEmpty($ProxyHost)) {
+        if ([string]::IsNullOrEmpty($ProxyHost)){
             Write-Host "Skipping proxy, no host found."
-        }
-        else {
+        }else{
             Write-Host "Found proxy host."
             $ProxyUrlBuilder = New-Object System.UriBuilder -ArgumentList $ProxyHost
 
             $ProxyPort = Get-FirstFromEnv @("blackduck.proxy.port", "BLACKDUCK_PROXY_PORT", "blackduck.hub.proxy.port", "BLACKDUCK_HUB_PROXY_PORT"); 
 
-            if ([string]::IsNullOrEmpty($ProxyPort)) {
+            if ([string]::IsNullOrEmpty($ProxyPort)){
                 Write-Host "No proxy port found."
-            }
-            else {
+            }else{
                 Write-Host "Found proxy port."
                 $ProxyUrlBuilder.Port = $ProxyPort
             }
@@ -146,10 +134,9 @@ function Get-ProxyInfo () {
             $ProxyUsername = Get-FirstFromEnv @("blackduck.proxy.username", "BLACKDUCK_PROXY_USERNAME", "blackduck.hub.proxy.username", "BLACKDUCK_HUB_PROXY_USERNAME"); 
             $ProxyPassword = Get-FirstFromEnv @("blackduck.proxy.password", "BLACKDUCK_PROXY_PASSWORD", "blackduck.hub.proxy.password", "BLACKDUCK_HUB_PROXY_PASSWORD"); 
 
-            if ([string]::IsNullOrEmpty($ProxyPassword) -or [string]::IsNullOrEmpty($ProxyUsername)) {
+            if ([string]::IsNullOrEmpty($ProxyPassword) -or [string]::IsNullOrEmpty($ProxyUsername)){
                 Write-Host "No proxy credentials found."
-            }
-            else {
+            }else{
                 Write-Host "Found proxy credentials."
                 $ProxySecurePassword = ConvertTo-SecureString $ProxyPassword -AsPlainText -Force
                 $ProxyCredentials = New-Object System.Management.Automation.PSCredential ($ProxyUsername, $ProxySecurePassword)
@@ -160,8 +147,7 @@ function Get-ProxyInfo () {
             Write-Host "Successfully setup proxy."
         }
 
-    }
-    catch [Exception] {
+    } catch [Exception] {
         Write-Host ("An exception occurred setting up the proxy, will continue but will not use a proxy.")
         Write-Host ("  Reason: {0}" -f $_.Exception.GetType().FullName); 
         Write-Host ("  Reason: {0}" -f $_.Exception.Message); 
@@ -176,19 +162,18 @@ function Get-ProxyInfo () {
 function Invoke-WebRequestWrapper($Url, $ProxyInfo, $DownloadLocation = $null) {
     $parameters = @{}
     try {
-        if ($DownloadLocation -ne $null) {
+        if ($DownloadLocation -ne $null){
             $parameters.Add("OutFile", $DownloadLocation);
         }
-        if ($ProxyInfo -ne $null) {
-            if ($ProxyInfo.Uri -ne $null) {
+        if ($ProxyInfo -ne $null){
+            if ($ProxyInfo.Uri -ne $null){
                 $parameters.Add("Proxy", $ProxyInfo.Uri);
             }
-            if ($ProxyInfo.Credentials -ne $null) {
-                $parameters.Add("ProxyCredential", $ProxyInfo.Credentials);
+            if ($ProxyInfo.Credentials -ne $null){
+                $parameters.Add("ProxyCredential",$ProxyInfo.Credentials);
             }
         }
-    }
-    catch [Exception] {
+    }catch [Exception] {
         Write-Host ("An exception occurred setting additional properties on web request.")
         Write-Host ("  Reason: {0}" -f $_.Exception.GetType().FullName); 
         Write-Host ("  Reason: {0}" -f $_.Exception.Message); 
@@ -198,51 +183,69 @@ function Invoke-WebRequestWrapper($Url, $ProxyInfo, $DownloadLocation = $null) {
     return Invoke-WebRequest $Url -UseBasicParsing @parameters
 }
 
-function Get-DetectJar ($DetectFolder, $DetectSource, $DetectVersionKey, $DetectVersion, $ProxyInfo) {
-    if ($DetectSource -eq "") {
-        if ($DetectVersion -eq "") {
-            $DetectVersionUrl = "https://test-repo.blackducksoftware.com/artifactory/api/storage/bds-integrations-release/com/blackducksoftware/integration/hub-detect?properties=" + $DetectVersionKey
-            $DetectSource = Receive-DetectSource -ProxyInfo $ProxyInfo -DetectVersionUrl $DetectVersionUrl -DetectVersionKey $DetectVersionKey
-        }
-        else {
-            $DetectSource = "https://test-repo.blackducksoftware.com/artifactory/bds-integrations-release/com/blackducksoftware/integration/hub-detect/" + $DetectVersion + "/hub-detect-" + $DetectVersion + ".jar"
+function Get-DetectSnapshotJar ($DetectFolder, $DetectVersion, $ProxyInfo) {
+    if ($DetectVersion -eq ""){
+        $DetectVersion = "latest-SNAPSHOT"
+    }
+    
+    Write-Host "Using detect version $DetectVersion"
+
+    $DetectJarFile = "$DetectFolder/hub-detect-$DetectVersion.jar"
+    $DetectCurrentCommitFile = "$DetectFolder/hub-detect-latest-commit.txt"
+
+    $DetectJarExists = Test-Path $DetectJarFile
+    $DetectCurrentCommitFileExists = Test-Path $DetectCurrentCommitFile
+    $DetectLatestCommit = Invoke-WebRequestWrapper -Url $DetectCommitUrl -ProxyInfo $ProxyInfo
+    $DetectLatestCommit = $DetectLatestCommit.ToString().Trim()
+    Write-Host "Detect jar exists '$DetectJarExists', commit file exists '$DetectCurrentCommitFileExists', latest commit '$DetectLatestCommit'"
+
+    $Download = $TRUE;
+    if ($DetectJarExists -and $DetectCurrentCommitFileExists){
+        $DetectCurrentCommit = Get-Content $DetectCurrentCommitFile
+        $DetectCurrentCommit = $DetectCurrentCommit.Trim()
+        Write-Host "Current commit '$DetectCurrentCommit'"
+        if ($DetectCurrentCommit -eq $DetectLatestCommit){
+            $Download = $FALSE;
         }
     }
 
-    Write-Host "Using detect source $DetectSource"
+    if ($Download){
+        $DetectUrl = Join-DetectUrl -DetectBaseUrl $DetectSnapshotBaseUrl -DetectVersion $DetectVersion
+        Receive-DetectJar -DetectUrl $DetectUrl -DetectJarFile $DetectJarFile -ProxyInfo $ProxyInfo
+        Set-Content $DetectCurrentCommitFile $DetectLatestCommit
+    }else{
+        Write-Host "You have already downloaded the latest file, so the local file will be used."
+    }
+    
+    return $DetectJarFile    
+}
 
-    $DetectVersion = Parse-Version -DetectSource $DetectSource
+function Get-DetectJar ($DetectFolder, $DetectVersion, $ProxyInfo) {
+    if ($DetectVersion -eq ""){
+        $DetectVersion = Receive-DetectLatestVersion -ProxyInfo $ProxyInfo
+    }
 
-    Write-Host "Using detect version $DetectVersion"
+	Write-Host "Using detect version $DetectVersion"
 
     $DetectJarFile = "$DetectFolder/hub-detect-$DetectVersion.jar"
 
     $DetectJarExists = Test-Path $DetectJarFile
     Write-Host "Detect jar exists '$DetectJarExists'"
 
-    if (!$DetectJarExists) {
-        Receive-DetectJar -DetectUrl $DetectSource -DetectJarFile $DetectJarFile -ProxyInfo $ProxyInfo
-    }
-    else {
+    if (!$DetectJarExists){
+        $DetectUrl = Join-DetectUrl -DetectBaseUrl $DetectReleaseBaseUrl -DetectVersion $DetectVersion
+        Receive-DetectJar -DetectUrl $DetectUrl -DetectJarFile $DetectJarFile -ProxyInfo $ProxyInfo
+    }else{
         Write-Host "You have already downloaded the latest file, so the local file will be used."
     }
 
     return $DetectJarFile    
 }
 
-function Parse-Version($DetectSource) {
-    $SlashParts = $DetectSource.Split("/")
-    $LastPart = $SlashParts[$SlashParts.Length - 1]
-    $DashParts = $LastPart.Split("-")
-    $VersionJar = $DashParts[$DashParts.Length - 1]
-    $Version = $VersionJar.TrimEnd(".jar")
-    return $Version
-}
-
 function Invoke-Detect ($DetectJarFile, $DetectArgs) {
-    ${Env:detect.phone.home.passthrough.powershell.version} = $Version
+	${Env:detect.phone.home.passthrough.powershell.version} = $Version
     $JavaArgs = @("-jar", $DetectJarFile)
-    $AllArgs = $JavaArgs + $DetectArgs
+    $AllArgs =  $JavaArgs + $DetectArgs
     Set-ToEscaped($AllArgs)
     Write-Host "Running detect: $AllArgs"
     $DetectProcess = Start-Process java -ArgumentList $AllArgs -NoNewWindow -PassThru
@@ -252,13 +255,17 @@ function Invoke-Detect ($DetectJarFile, $DetectArgs) {
     return $DetectExitCode
 }
 
+function Join-DetectUrl ($DetectBaseUrl, $DetectVersion) {
+    return "$DetectBaseUrl/${DetectVersion}/hub-detect-${DetectVersion}.jar"
+}
+
 function Initialize-DetectFolder ($DetectFolder, $TempFolder, $HomeTempFolder) {
-    if ($DetectFolder -ne "") {
+    if ($DetectFolder -ne ""){
         Write-Host "Using supplied detect folder: $DetectFolder"
         return Initialize-Folder -Folder $DetectFolder
     }
 
-    if ($TempFolder -ne "") {
+    if ($TempFolder -ne ""){
         Write-Host "Using system temp folder: $TempFolder"
         return Initialize-Folder -Folder $TempFolder
     }
@@ -267,22 +274,18 @@ function Initialize-DetectFolder ($DetectFolder, $TempFolder, $HomeTempFolder) {
 }
 
 function Initialize-Folder ($Folder) {
-    If (!(Test-Path $Folder)) {
+    If(!(Test-Path $Folder)) {
         Write-Host "Created folder: $Folder"
         New-Item -ItemType Directory -Force -Path $Folder | Out-Null #Pipe to Out-Null to prevent dirtying to the function output
     }
     return $Folder
 }
 
-function Receive-DetectSource ($ProxyInfo, $DetectVersionUrl, $DetectVersionKey) {
+function Receive-DetectLatestVersion ($ProxyInfo) {
     Write-Host "Finding latest detect version."
-    $DetectVersionData = Invoke-WebRequestWrapper -Url $DetectVersionUrl -ProxyInfo $ProxyInfo
-    $DetectVersionJson = ConvertFrom-Json -InputObject $DetectVersionData
-    
-    $Properties = $DetectVersionJson | select -ExpandProperty "properties"
-    $DetectVersionUrl = $Properties | select -ExpandProperty $DetectVersionKey
-    Write-Host "Resolved detect source $DetectVersionUrl"
-    return $DetectVersionUrl
+    $DetectVersion = Invoke-WebRequestWrapper -Url $DetectVersionUrl -ProxyInfo $ProxyInfo
+    Write-Host "Resolved version $DetectVersion"
+    return $DetectVersion
 }
 
 function Receive-DetectJar ($DetectUrl, $DetectJarFile, $ProxyInfo) {
@@ -293,36 +296,35 @@ function Receive-DetectJar ($DetectUrl, $DetectJarFile, $ProxyInfo) {
     Write-Host "Downloaded detect jar successfully '$DetectJarExists'"
 }
 
-function Set-ToEscaped ($ArgArray) {
-    for ($i = 0; $i -lt $ArgArray.Count ; $i++) {
-        $Value = $ArgArray[$i]
+function Set-ToEscaped ($ArgArray){
+	for ($i = 0; $i -lt $ArgArray.Count ; $i++) {
+		$Value = $ArgArray[$i]
         $ArgArray[$i] = """$Value"""
     }
 }
 
 function Test-JavaNotAvailable() {
-    Write-Host "Checking if Java is installed by asking for version."
-    try {
-        $ProcessStartInfo = New-object System.Diagnostics.ProcessStartInfo 
-        $ProcessStartInfo.CreateNoWindow = $true 
-        $ProcessStartInfo.UseShellExecute = $false 
-        $ProcessStartInfo.RedirectStandardOutput = $true 
-        $ProcessStartInfo.RedirectStandardError = $true 
-        $ProcessStartInfo.FileName = 'java' 
-        $ProcessStartInfo.Arguments = @("-version") 
-        $Process = New-Object System.Diagnostics.Process 
-        $Process.StartInfo = $ProcessStartInfo
-        [void]$Process.Start()
-        $StdOutput = $process.StandardOutput.ReadToEnd()
-        $StdError = $process.StandardError.ReadToEnd() 
-        $Process.WaitForExit()
-        Write-Host "Java Standard Output: $StdOutput"
-        Write-Host "Java Error Output: $StdError"
-        Write-Host "Successfully able to start java and get version."
-        return $FALSE;
-    }
-    catch { 
-        Write-Host "An error occurred checking the Java version. Please ensure Java is installed."
-        return $TRUE;
-    }
+	Write-Host "Checking if Java is installed by asking for version."
+	try {
+		$ProcessStartInfo = New-object System.Diagnostics.ProcessStartInfo 
+		$ProcessStartInfo.CreateNoWindow = $true 
+		$ProcessStartInfo.UseShellExecute = $false 
+		$ProcessStartInfo.RedirectStandardOutput = $true 
+		$ProcessStartInfo.RedirectStandardError = $true 
+		$ProcessStartInfo.FileName = 'java' 
+		$ProcessStartInfo.Arguments = @("-version") 
+		$Process = New-Object System.Diagnostics.Process 
+		$Process.StartInfo = $ProcessStartInfo
+		[void]$Process.Start()
+		$StdOutput = $process.StandardOutput.ReadToEnd()
+		$StdError = $process.StandardError.ReadToEnd() 
+		$Process.WaitForExit()
+		Write-Host "Java Standard Output: $StdOutput"
+		Write-Host "Java Error Output: $StdError"
+		Write-Host "Successfully able to start java and get version."
+		return $FALSE;
+	}catch { 
+		Write-Host "An error occurred checking the Java version. Please ensure Java is installed."
+		return $TRUE;
+	}
 }
