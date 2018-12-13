fe99731f5a3ffbb1eb88b8fb502e2ec384fb1a88
Jordan Piscitelli <jordanp@synopsys.com>
2018-12-13 14:46:15 -0500

    Adding a property name to the json.

--- detect-configuration/src/main/java/com/blackducksoftware/integration/hub/detect/help/json/HelpJsonOption.java
+++ detect-configuration/src/main/java/com/blackducksoftware/integration/hub/detect/help/json/HelpJsonOption.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HelpJsonOption {
+    public String propertyName = "";
     public String propertyKey = "";
     public String propertyType = "";
     public String defaultValue = "";
