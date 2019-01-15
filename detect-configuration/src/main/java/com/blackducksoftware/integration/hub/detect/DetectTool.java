fda5ea080adda551bb03d289170499b99213c8bb
Jordan Piscitelli <jordanp@synopsys.com>
2019-01-15 15:10:38 -0500

    Adding code to better show how the default for project order is used - if a project order is not supplied and a decision is needed, detect will error out.

--- detect-configuration/src/main/java/com/blackducksoftware/integration/hub/detect/DetectTool.java
+++ detect-configuration/src/main/java/com/blackducksoftware/integration/hub/detect/DetectTool.java
@@ -23,16 +23,11 @@
  */
 package com.blackducksoftware.integration.hub.detect;
 
-import java.util.Arrays;
-import java.util.List;
-
 public enum DetectTool {
     DETECTOR,
     SIGNATURE_SCAN,
     BINARY_SCAN,
     POLARIS,
     DOCKER;
-
-    public static List<DetectTool> DEFAULT_PROJECT_ORDER = Arrays.asList(DOCKER, DETECTOR);
 
 }
