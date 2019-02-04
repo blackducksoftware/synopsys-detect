2fff899873ab3ecfc8db07181dc8d4e81ad65107
Jake Mathews <jake.mathews.email@gmail.com>
2019-02-04 18:06:39 -0500

    Added CPAN as a Detectable

--- detectable/src/test/java/com/synopsys/integration/detectable/util/FunctionalTestFiles.java
+++ detectable/src/test/java/com/synopsys/integration/detectable/util/FunctionalTestFiles.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.junit.Assert;
@@ -15,14 +16,22 @@ import com.synopsys.integration.util.ResourceUtil;
 public class FunctionalTestFiles {
     private static String resourcePrefix = "/detectables/functional";
 
-    public static String asString(String relativeResourcePath) {
-        final String data;
+    public static String asString(final String relativeResourcePath) {
+        String data;
         try {
-            data = ResourceUtil.getResourceAsString(FunctionalTestFiles.class, resourcePrefix + relativeResourcePath, StandardCharsets.UTF_8.toString());
+            data = ResourceUtil.getResourceAsString(FunctionalTestFiles.class, resourcePrefix + relativeResourcePath, StandardCharsets.UTF_8);
         } catch (final IOException e) {
             throw new RuntimeException(e);
         }
-        return Arrays.asList(data.split("\r?\n")).stream().collect(Collectors.joining(System.lineSeparator()));
+
+        return Arrays.stream(data.split("\r?\n"))
+                   .collect(Collectors.joining(System.lineSeparator()));
+    }
+
+    public static List<String> asListOfStrings(final String relativeResourcePath) {
+        final String data = asString(relativeResourcePath);
+
+        return Arrays.asList(data.split(System.lineSeparator()));
     }
 
     public static InputStream asInputStream(final String relativeResourcePath) {
@@ -37,6 +46,7 @@ public class FunctionalTestFiles {
         URL resource = FunctionalTestFiles.class.getResource(resourcePrefix + relativeResourcePath);
         File file = new File(resource.getFile());
         Assert.assertTrue(file.exists());
+
         return file;
     }
 }
