179718ed4ea5f203fb889d827f4b72a98b1d53f3
Jake Mathews <jake.mathews.email@gmail.com>
2019-03-11 18:25:37 -0400

    Re-added sourcePath to codelocations produced by Bitbake

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/bitbake/BitbakeExtractor.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/bitbake/BitbakeExtractor.java
@@ -98,7 +98,7 @@ public class BitbakeExtractor {
                 final DependencyGraph dependencyGraph = bitbakeGraphTransformer.transform(bitbakeGraph, targetArchitecture);
 
                 final ExternalId externalId = new ExternalId(Forge.YOCTO);
-                final CodeLocation codeLocation = new CodeLocation(dependencyGraph, externalId);//TODO: Source path: sourcePath.getCanonicalPath()
+                final CodeLocation codeLocation = new CodeLocation(dependencyGraph, externalId, sourcePath);
 
                 codeLocations.add(codeLocation);
             } catch (final IOException | IntegrationException e) {
