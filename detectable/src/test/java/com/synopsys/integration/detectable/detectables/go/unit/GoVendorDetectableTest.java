6188762490d84adc80f9040f93fa823b7acf6d1c
Steve Billings <sbillings@blackducksoftware.com>
2019-04-16 17:58:45 -0400

    test(govendor): applicable test

---
+++ detectable/src/test/java/com/synopsys/integration/detectable/detectables/go/unit/GoVendorDetectableTest.java
@@ -0,0 +1,40 @@
+package com.synopsys.integration.detectable.detectables.go.unit;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.synopsys.integration.detectable.DetectableEnvironment;
+import com.synopsys.integration.detectable.detectable.file.FileFinder;
+import com.synopsys.integration.detectable.detectables.go.vendor.GoVendorDetectable;
+import com.synopsys.integration.detectable.detectables.go.vendor.GoVendorExtractor;
+
+public class GoVendorDetectableTest {
+
+    public static final String VENDOR_DIRNAME = "vendor";
+    public static final String VENDOR_JSON_FILENAME = "vendor.json";
+
+    @Test
+    public void testApplicableVendor() {
+
+        final File currentDir = new File(".");
+        final File vendorDir = new File(VENDOR_DIRNAME);
+        final File vendorFile = new File(vendorDir, VENDOR_JSON_FILENAME);
+
+        final DetectableEnvironment environment = Mockito.mock(DetectableEnvironment.class);
+        Mockito.when(environment.getDirectory()).thenReturn(currentDir);
+        final GoVendorExtractor goVendorExtractor = null;
+
+        final FileFinder fileFinder = Mockito.mock(FileFinder.class);
+        Mockito.when(fileFinder.findFile(currentDir, VENDOR_DIRNAME)).thenReturn(vendorDir);
+        Mockito.when(fileFinder.findFile(vendorDir, VENDOR_JSON_FILENAME)).thenReturn(vendorFile);
+
+        final GoVendorDetectable detectable = new GoVendorDetectable(environment, fileFinder, goVendorExtractor);
+
+        assertTrue(detectable.applicable().getPassed());
+    }
+}
