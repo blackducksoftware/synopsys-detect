3d14f973f09bb4e92d8431153d2ad2c302e760f7
Steve Billings <sbillings@blackducksoftware.com>
2019-04-16 16:40:21 -0400

    test(conda): applicable test

--- detectable/src/test/java/com/synopsys/integration/detectable/detectables/conda/unit/CondaCliDetectableTest.java
+++ detectable/src/test/java/com/synopsys/integration/detectable/detectables/conda/unit/CondaCliDetectableTest.java
@@ -1,4 +1,4 @@
-package com.synopsys.integration.detectable.detectables.cocoapods;
+package com.synopsys.integration.detectable.detectables.conda.unit;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -8,21 +8,25 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import com.synopsys.integration.detectable.DetectableEnvironment;
+import com.synopsys.integration.detectable.detectable.executable.resolver.CondaResolver;
 import com.synopsys.integration.detectable.detectable.file.FileFinder;
+import com.synopsys.integration.detectable.detectables.conda.CondaCliDetectable;
+import com.synopsys.integration.detectable.detectables.conda.CondaCliExtractor;
 
-public class PodlockDetectableTest {
+public class CondaCliDetectableTest {
 
     @Test
     public void testApplicable() {
 
         final DetectableEnvironment environment = Mockito.mock(DetectableEnvironment.class);
         final FileFinder fileFinder = Mockito.mock(FileFinder.class);
-        final PodlockExtractor podlockExtractor = null;
 
         Mockito.when(environment.getDirectory()).thenReturn(new File("."));
         Mockito.when(fileFinder.findFile(Mockito.any(File.class), Mockito.anyString())).thenReturn(new File("."));
 
-        final PodlockDetectable detectable = new PodlockDetectable(environment, fileFinder, podlockExtractor);
+        final CondaResolver condaResolver = null;
+        final CondaCliExtractor condaExtractor = null;
+        final CondaCliDetectable detectable = new CondaCliDetectable(environment, fileFinder, condaResolver, condaExtractor);
 
         assertTrue(detectable.applicable().getPassed());
     }
