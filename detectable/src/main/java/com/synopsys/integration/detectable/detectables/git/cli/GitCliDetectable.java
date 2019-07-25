10b57f2ceaa31c1289ecb9d1c24b11c0b632e335
Jake Mathews <jake.mathews.email@gmail.com>
2019-07-25 16:32:36 -0400

    fix(git) Git was failing to find the executable. Since git is a "nice to have", the check will now happen in isApplicable to avoid errors for the user. (Fixes IDETECT-1576)

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/git/cli/GitCliDetectable.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/git/cli/GitCliDetectable.java
@@ -59,17 +59,20 @@ public class GitCliDetectable extends Detectable {
             return new FileNotFoundDetectableResult(GIT_DIRECTORY_NAME);
         }
 
+        try {
+            gitExecutable = gitResolver.resolveGit();
+            if (gitExecutable == null) {
+                throw new DetectableException();
+            }
+        } catch (final DetectableException exception) {
+            return new ExecutableNotFoundDetectableResult("git");
+        }
+
         return new PassedDetectableResult();
     }
 
     @Override
-    public DetectableResult extractable() throws DetectableException {
-        gitExecutable = gitResolver.resolveGit();
-
-        if (gitExecutable == null) {
-            return new ExecutableNotFoundDetectableResult("git");
-        }
-
+    public DetectableResult extractable() {
         return new PassedDetectableResult();
     }
 
