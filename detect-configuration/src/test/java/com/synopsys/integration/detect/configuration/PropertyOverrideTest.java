3aa2fc66356b117476d141afe6dd3b9cc307df9f
Jordan Piscitelli <jordanp@synopsys.com>
2019-10-03 15:14:55 -0400

    test(configuration): Added a test to verify you haven't made an invalid override map.

---
+++ detect-configuration/src/test/java/com/synopsys/integration/detect/configuration/PropertyOverrideTest.java
@@ -0,0 +1,18 @@
+package com.synopsys.integration.detect.configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PropertyOverrideTest {
+    @Test
+    public void CheckOverrideContainsNoDuplicateValues() {
+        final List<DetectProperty> found = new ArrayList<>();
+        DetectPropertyDeprecations.PROPERTY_OVERRIDES.forEach((key, value) -> {
+            Assertions.assertFalse(found.contains(value), "Each detect property can only exist in the property override value side exactly once. This was found twice: " + value.getPropertyKey());
+            found.add(value);
+        });
+    }
+}
