f88a7d299886c94f7df5b0c636571846b60e707d
Jordan Piscitelli <jordanp@synopsys.com>
2019-10-04 17:49:58 -0400

    test(battery): Cocoapods uses git shortcut.

--- src/test/java/com/synopsys/integration/detect/battery/tests/CocoapodsBattery.java
+++ src/test/java/com/synopsys/integration/detect/battery/tests/CocoapodsBattery.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import com.synopsys.integration.detect.battery.BatteryTest;
-import com.synopsys.integration.detect.configuration.DetectProperty;
 
 @Tag("battery")
 public class CocoapodsBattery {
@@ -13,8 +12,7 @@ public class CocoapodsBattery {
         final BatteryTest test = new BatteryTest("cocoapods-podlock");
         test.sourceDirectoryNamed("linux-cocoapods");
         test.sourceFileFromResource("Podfile.lock");
-        test.sourceFileNamed(".git");
-        test.executable(DetectProperty.DETECT_GIT_PATH, "https://github.com/BlackDuckCoPilot/example-cocoapods-travis.git", "HEAD");
+        test.git("https://github.com/BlackDuckCoPilot/example-cocoapods-travis.git", "HEAD");
         test.expectBdioResources();
         test.run();
     }
