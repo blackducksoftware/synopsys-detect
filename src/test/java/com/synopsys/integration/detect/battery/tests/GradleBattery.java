cb08f6041c07a6454ecb448c0f59177ce5242658
Jordan Piscitelli <jordanp@synopsys.com>
2019-10-09 18:22:07 -0400

    feat(battery): Added gradle battery test.

--- src/test/java/com/synopsys/integration/detect/battery/tests/GradleBattery.java
+++ src/test/java/com/synopsys/integration/detect/battery/tests/GradleBattery.java
@@ -4,14 +4,17 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import com.synopsys.integration.detect.battery.BatteryTest;
+import com.synopsys.integration.detect.configuration.DetectProperty;
 
 @Tag("battery")
 public class GradleBattery {
     @Test
-    void lock() {
+    void inspector() {
+        //Note about this test: The paths have been removed from the inspector meta data.
         final BatteryTest test = new BatteryTest("gradle-inspector");
         test.sourceDirectoryNamed("linux-gradle");
         test.sourceFileNamed("build.gradle");
+        test.executableThatCopiesFiles(DetectProperty.DETECT_GRADLE_PATH, 5, "", "GRADLE-0");
         test.git("https://github.com/BlackDuckCoPilot/example-gradle-travis", "HEAD");
         test.expectBdioResources();
         test.run();
