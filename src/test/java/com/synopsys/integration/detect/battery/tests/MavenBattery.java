1d51d56cb0ce3e7eb12a14bd4fac0309fa8da799
Jordan Piscitelli <jordanp@synopsys.com>
2019-10-11 09:44:49 -0400

    test(battery): Added maven battery test.

---
+++ src/test/java/com/synopsys/integration/detect/battery/tests/MavenBattery.java
@@ -0,0 +1,23 @@
+package com.synopsys.integration.detect.battery.tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.synopsys.integration.detect.battery.BatteryTest;
+import com.synopsys.integration.detect.configuration.DetectProperty;
+
+@Tag("battery")
+public class MavenBattery {
+    @Test
+    void inspector() {
+        //Note about this test: The paths have been removed from the inspector meta data.
+        final BatteryTest test = new BatteryTest("maven-cli");
+        test.sourceDirectoryNamed("linux-maven");
+        test.sourceFileNamed("pom.xml");
+        test.executableFromResourceFiles(DetectProperty.DETECT_MAVEN_PATH, "maven-dependencytree.xout");
+        test.git("https://github.com/BlackDuckCoPilot/example-maven-travis", "HEAD");
+        test.expectBdioResources();
+        test.run();
+    }
+}
+
