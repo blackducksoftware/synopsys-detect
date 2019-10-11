f936e521b510c2e74b5c3b365e75ccd9d92049c4
Jordan Piscitelli <jordanp@synopsys.com>
2019-10-11 11:48:30 -0400

    test(battery): Added packrat battery test.

---
+++ src/test/java/com/synopsys/integration/detect/battery/tests/PackratBattery.java
@@ -0,0 +1,20 @@
+package com.synopsys.integration.detect.battery.tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.synopsys.integration.detect.battery.BatteryTest;
+
+@Tag("battery")
+public class PackratBattery {
+    @Test
+    void lock() {
+        final BatteryTest test = new BatteryTest("packrat-lock");
+        test.sourceDirectoryNamed("packrat-lock");
+        test.sourceFileFromResource("packrat.lock");
+        test.git("https://github.com/pingles/redshift-r.git", "HEAD");
+        test.expectBdioResources();
+        test.run();
+    }
+}
+
