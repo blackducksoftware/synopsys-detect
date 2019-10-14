724da66ad4af96c52c76124f4e9485e4bb2b2604
Jordan Piscitelli <jordanp@synopsys.com>
2019-10-14 14:15:27 -0400

    test(battery): Added sbt battery test.

---
+++ src/test/java/com/synopsys/integration/detect/battery/tests/SbtBattery.java
@@ -0,0 +1,21 @@
+package com.synopsys.integration.detect.battery.tests;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import com.synopsys.integration.detect.battery.BatteryTest;
+
+@Tag("battery")
+public class SbtBattery {
+    @Test
+    void resolutioncache() {
+        final BatteryTest test = new BatteryTest("sbt-resolutioncache");
+        test.sourceDirectoryNamed("linux-sbt");
+        test.sourceFileNamed("build.sbt");
+        test.sourceFolderFromExpandedResource("target");
+        test.git("https://github.com/sbt/sbt-bintray.git", "HEAD");
+        test.expectBdioResources();
+        test.run();
+    }
+}
+
