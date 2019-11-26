ce293073fca8faaebda50f605657578f4e044acc
Jordan Piscitelli <jordanp@synopsys.com>
2019-11-26 17:16:17 -0500

    fix(signature scan): Fixed an issue with file target not matching path.

--- src/main/java/com/synopsys/integration/detect/tool/signaturescanner/SignatureScanPath.java
+++ src/main/java/com/synopsys/integration/detect/tool/signaturescanner/SignatureScanPath.java
@@ -26,8 +26,11 @@ import java.io.File;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.synopsys.integration.detect.workflow.file.DetectFileUtils;
+
 public class SignatureScanPath {
     private File targetPath;
+    private String targetCanonicalPath;
     private final Set<String> exclusions = new HashSet<>();
 
     public File getTargetPath() {
@@ -36,9 +39,14 @@ public class SignatureScanPath {
 
     public void setTargetPath(final File targetPath) {
         this.targetPath = targetPath;
+        this.targetCanonicalPath  = DetectFileUtils.tryGetCanonicalPath(targetPath);
     }
 
     public Set<String> getExclusions() {
         return exclusions;
+    }
+
+    public String getTargetCanonicalPath() {
+        return targetCanonicalPath;
     }
 }
