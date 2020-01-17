e7f76091505802398701a520752371506bc8f78e
Jordan Piscitelli <jordanp@synopsys.com>
2020-01-17 16:07:45 -0500

    feat(configuration): Added ability to have a deprecation aware value retrieval.

--- configuration/src/main/java/com/synopsys/integration/configuration/property/base/TypedProperty.kt
+++ configuration/src/main/java/com/synopsys/integration/configuration/property/base/TypedProperty.kt
@@ -3,4 +3,4 @@ package com.synopsys.integration.configuration.property.base
 import com.synopsys.integration.configuration.parse.ValueParser
 import com.synopsys.integration.configuration.property.Property
 
-abstract class TypedProperty<T>(key: String, val parser: ValueParser<T>) : Property(key)
+abstract class TypedProperty<T>(key: String, val parser: ValueParser<T>) : Property(key) {}
--- configuration/src/main/java/com/synopsys/integration/configuration/property/base/ValuedProperty.kt
+++ configuration/src/main/java/com/synopsys/integration/configuration/property/base/ValuedProperty.kt
@@ -4,4 +4,10 @@ import com.synopsys.integration.configuration.parse.ValueParser
 
 abstract class ValuedProperty<T>(key: String, parser: ValueParser<T>, val default: T) : TypedProperty<T>(key, parser) {
     // This is a property with a key and with a default value, it will always have a value.
+
+    var replacesDeprecatedProperty: ValuedProperty<T>? = null
+    fun replacesDeprecatedProperty(oldProperty: ValuedProperty<T>): ValuedProperty<T> {
+        this.replacesDeprecatedProperty = oldProperty
+        return this
+    }
 }
