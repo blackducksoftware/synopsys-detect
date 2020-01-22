83831e7c398f0945c3a15008528be5d054712718
Jake Mathews <jake.mathews.email@gmail.com>
2020-01-22 14:19:24 -0500

    refactor(phone-home): Converted PhoneHomeOptions to a data class.

--- src/main/java/com/synopsys/integration/detect/workflow/phonehome/PhoneHomeOptions.kt
+++ src/main/java/com/synopsys/integration/detect/workflow/phonehome/PhoneHomeOptions.kt
@@ -22,5 +22,4 @@
  */
 package com.synopsys.integration.detect.workflow.phonehome
 
-class PhoneHomeOptions(val passthrough: Map<String, String>) {
-}
+data class PhoneHomeOptions(val passthrough: Map<String, String>) {}
