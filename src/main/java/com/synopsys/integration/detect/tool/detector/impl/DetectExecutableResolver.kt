40f789e172aa17ded5d038b05a8cff518c8fad0b
Jake Mathews <jake.mathews.email@gmail.com>
2020-01-23 17:07:54 -0500

    fix(executable): Executable resolvers can resolve to null.

--- src/main/java/com/synopsys/integration/detect/tool/detector/impl/DetectExecutableResolver.kt
+++ src/main/java/com/synopsys/integration/detect/tool/detector/impl/DetectExecutableResolver.kt
@@ -27,6 +27,7 @@ import com.synopsys.integration.detectable.detectable.exception.DetectableExcept
 import com.synopsys.integration.detectable.detectable.executable.impl.SimpleExecutableResolver
 import com.synopsys.integration.detectable.detectable.executable.resolver.*
 import com.synopsys.integration.detectable.detectable.inspector.go.GoResolver
+import org.antlr.v4.runtime.misc.Nullable
 import org.apache.commons.lang3.StringUtils
 import java.io.File
 import java.nio.file.Path
@@ -37,7 +38,7 @@ class DetectExecutableResolver(private val simpleExecutableResolver: SimpleExecu
     private val cachedExecutables: MutableMap<String, File> = mutableMapOf()
 
     @Throws(DetectableException::class)
-    private fun resolveExecutable(cacheKey: String?, resolveExecutable: Supplier<File>, executableOverride: Path?): File {
+    private fun resolveExecutable(cacheKey: String?, resolveExecutable: Supplier<File?>, executableOverride: Path?): File? {
         if (executableOverride != null) {
             val exe = executableOverride.toFile()
             return if (!exe.exists()) {
@@ -52,113 +53,114 @@ class DetectExecutableResolver(private val simpleExecutableResolver: SimpleExecu
         }
         val hasCacheKey = StringUtils.isNotBlank(cacheKey)
         if (hasCacheKey && cachedExecutables.containsKey(cacheKey)) {
-            var cached = cachedExecutables.get(cacheKey);
-            if (cached != null) return cached;
+            val cached = cachedExecutables[cacheKey]
+            if (cached != null) return cached
         }
         val resolved = resolveExecutable.get()
-        if (hasCacheKey && cacheKey != null) {
+        if (hasCacheKey && cacheKey != null && resolved != null) {
             cachedExecutables[cacheKey] = resolved
         }
         return resolved
     }
 
     @Throws(DetectableException::class)
-    private fun resolveExecutableLocally(resolveExecutable: Function<DetectableEnvironment, File>, environment: DetectableEnvironment, executableOverride: Path?): File {
+    private fun resolveExecutableLocally(resolveExecutable: Function<DetectableEnvironment, File>, environment: DetectableEnvironment, executableOverride: Path?): File? {
         return resolveExecutable(null, Supplier { resolveExecutable.apply(environment) }, executableOverride)
     }
 
     @Throws(DetectableException::class)
-    override fun resolveBash(): File {
+    override fun resolveBash(): File? {
         return resolveExecutable("bash", Supplier { simpleExecutableResolver.resolveBash() }, detectExecutableOptions.bashUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolveBazel(): File {
+    override fun resolveBazel(): File? {
         return resolveExecutable("bazel", Supplier { simpleExecutableResolver.resolveBazel() }, detectExecutableOptions.bazelUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolveConda(): File {
+    override fun resolveConda(): File? {
         return resolveExecutable("conda", Supplier { simpleExecutableResolver.resolveConda() }, detectExecutableOptions.condaUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolveCpan(): File {
+    override fun resolveCpan(): File? {
         return resolveExecutable("cpan", Supplier { simpleExecutableResolver.resolveCpan() }, detectExecutableOptions.cpanUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolveCpanm(): File {
+    override fun resolveCpanm(): File? {
         return resolveExecutable("cpanm", Supplier { simpleExecutableResolver.resolveCpanm() }, detectExecutableOptions.cpanmUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolveGradle(environment: DetectableEnvironment): File {
+    override fun resolveGradle(environment: DetectableEnvironment): File? {
         return resolveExecutableLocally(Function { simpleExecutableResolver.resolveGradle(it) }, environment, detectExecutableOptions.gradleUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolveMaven(environment: DetectableEnvironment): File {
+    override fun resolveMaven(environment: DetectableEnvironment): File? {
         return resolveExecutableLocally(Function { simpleExecutableResolver.resolveMaven(it) }, environment, detectExecutableOptions.mavenUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolveNpm(environment: DetectableEnvironment): File {
+    override fun resolveNpm(environment: DetectableEnvironment): File? {
         return resolveExecutableLocally(Function { simpleExecutableResolver.resolveNpm(it) }, environment, detectExecutableOptions.npmUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolvePear(): File {
+    override fun resolvePear(): File? {
         return resolveExecutable("pear", Supplier { simpleExecutableResolver.resolvePear() }, detectExecutableOptions.pearUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolvePip(): File {
+    override fun resolvePip(): File? {
         return resolveExecutable("pip", Supplier { simpleExecutableResolver.resolvePip() }, null)
     }
 
     @Throws(DetectableException::class)
-    override fun resolvePipenv(): File {
+    override fun resolvePipenv(): File? {
         return resolveExecutable("pipenv", Supplier { simpleExecutableResolver.resolvePipenv() }, detectExecutableOptions.pipenvUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolvePython(): File {
+    override fun resolvePython(): File? {
         return resolveExecutable("python", Supplier { simpleExecutableResolver.resolvePython() }, detectExecutableOptions.pythonUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolveRebar3(): File {
+    override fun resolveRebar3(): File? {
         return resolveExecutable("rebar3", Supplier { simpleExecutableResolver.resolveRebar3() }, detectExecutableOptions.rebarUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolveJava(): File {
+    override fun resolveJava(): File? {
         return resolveExecutable("java", Supplier { simpleExecutableResolver.resolveJava() }, detectExecutableOptions.javaUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolveDocker(): File {
+    override fun resolveDocker(): File? {
         return resolveExecutable("docker", Supplier { simpleExecutableResolver.resolveDocker() }, detectExecutableOptions.dockerUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolveDotNet(): File {
+    @Nullable
+    override fun resolveDotNet(): File? {
         return resolveExecutable("dotnet", Supplier { simpleExecutableResolver.resolveDotNet() }, detectExecutableOptions.dotnetUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolveGit(): File {
+    override fun resolveGit(): File? {
         return resolveExecutable("git", Supplier { simpleExecutableResolver.resolveGit() }, detectExecutableOptions.gitUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolveSwift(): File {
+    override fun resolveSwift(): File? {
         return resolveExecutable("swift", Supplier { simpleExecutableResolver.resolveSwift() }, detectExecutableOptions.swiftUserPath)
     }
 
     @Throws(DetectableException::class)
-    override fun resolveGo(): File {
+    override fun resolveGo(): File? {
         return resolveExecutable("go", Supplier { simpleExecutableResolver.resolveGo() }, detectExecutableOptions.goUserPath)
     }
 }
