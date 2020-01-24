0994788fa95f14d27277957b97f2c8e0eaad9dd5
Jake Mathews <jake.mathews.email@gmail.com>
2020-01-24 11:41:20 -0500

    test(integer-property): Added tests for integer properties.

---
+++ configuration/src/test/java/com/synopsys/integration/configuration/config/integer/IntegerPropertyTests.kt
@@ -0,0 +1,56 @@
+package com.synopsys.integration.configuration.config.bool
+
+import com.synopsys.integration.configuration.config.InvalidPropertyException
+import com.synopsys.integration.configuration.config.PropertyTest
+import com.synopsys.integration.configuration.property.types.integer.IntegerProperty
+import com.synopsys.integration.configuration.property.types.integer.NullableIntegerProperty
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class IntegerPropertyTests : PropertyTest() {
+    private val exampleKey = "example.key"
+    private val defaultPos1 = IntegerProperty(exampleKey, 1)
+    private val defaultNeg1 = IntegerProperty(exampleKey, -1)
+    private val nullable = NullableIntegerProperty(exampleKey)
+
+    @Test
+    fun emptyDefaultPos1() {
+        Assertions.assertEquals(1, emptyConfig().getValue(defaultPos1))
+    }
+
+    @Test
+    fun emptyDefaultNeg1() {
+        Assertions.assertEquals(-1, emptyConfig().getValue(defaultNeg1))
+    }
+
+    @Test
+    fun emptyDefaultsNull() {
+        Assertions.assertNull(emptyConfig().getValue(nullable))
+    }
+
+    @Test
+    fun providedOverridesDefault() {
+        val config = configOf(exampleKey to "-5")
+        Assertions.assertEquals(-5, config.getValue(defaultPos1))
+    }
+
+    @Test
+    fun providesValue() {
+        val config = configOf(exampleKey to "5")
+        val value = config.getValue(nullable)
+        Assertions.assertNotNull(value, "Config should not have returned null.")
+        Assertions.assertEquals(5, value)
+    }
+
+    @Test()
+    fun unknownValueThrows() {
+        Assertions.assertThrows(InvalidPropertyException::class.java) {
+            configOf("example.key" to "unknown").getValue(defaultPos1)
+        }
+    }
+
+    @Test()
+    fun unknownValueGivesDefault() {
+        Assertions.assertEquals(1, configOf("example.key" to "unknown").getValueOrDefault(defaultPos1))
+    }
+}
---
+++ configuration/src/test/java/com/synopsys/integration/configuration/config/integer/IntegerValueParserTests.kt
@@ -0,0 +1,30 @@
+package com.synopsys.integration.configuration.config.integer
+
+import com.synopsys.integration.configuration.parse.ValueParseException
+import com.synopsys.integration.configuration.property.types.integer.IntegerValueParser
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+class IntegerValueParserTests() {
+    @ParameterizedTest()
+    @ValueSource(strings = ["unknown", "Nan", "", " 1"])
+    fun parseUnknownThrows(value: String) {
+        Assertions.assertThrows(ValueParseException::class.java) {
+            IntegerValueParser().parse(value)
+        }
+    }
+
+    @Test
+    fun parseInt() {
+        fun assert(expected: Int, value: String) {
+            Assertions.assertEquals(expected, IntegerValueParser().parse(value))
+        }
+
+        assert(-1, "-1")
+        assert(1, "1")
+        assert(Integer.MAX_VALUE, "2147483647")
+        assert(Integer.MIN_VALUE, "-2147483648")
+    }
+}
