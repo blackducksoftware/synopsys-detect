757bbd4a2ca795cc52d9a8acdaf0f70dc5fada63
Jordan Piscitelli <jordanp@synopsys.com>
2020-01-24 13:20:33 -0500

    refactor: Removed no longer needed deprecation class.

--- configuration/src/main/java/com/synopsys/integration/configuration/property/base/NullableProperty.kt
+++ configuration/src/main/java/com/synopsys/integration/configuration/property/base/NullableProperty.kt
@@ -26,10 +26,4 @@ import com.synopsys.integration.configuration.parse.ValueParser
 
 abstract class NullableProperty<T>(key: String, parser: ValueParser<T>) : TypedProperty<T>(key, parser) {
     // This is a property with a key and a value, but the value is optional (nullable).
-
-    var replacesDeprecatedProperty: NullableProperty<T>? = null
-    fun replacesDeprecatedProperty(oldProperty: NullableProperty<T>): NullableProperty<T> {
-        this.replacesDeprecatedProperty = oldProperty
-        return this
-    }
 }
