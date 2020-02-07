705565421913f533e3a518ff5d2b09caf0a5987b
Jake Mathews <jake.mathews.email@gmail.com>
2020-02-07 09:43:23 -0500

    refactor(interactive): PrintStream in InteractiveManager is now wrapped by UncloseablePrintStream to protect against System.out being closed.

---
+++ src/main/java/com/synopsys/integration/detect/interactive/UncloseablePrintStream.java
@@ -0,0 +1,18 @@
+package com.synopsys.integration.detect.interactive;
+
+import java.io.PrintStream;
+
+/**
+ * A wrapped PrintStream that does nothing when closed.
+ * Useful for wrapping System.out without the underlying stream being closed.
+ */
+public class UncloseablePrintStream extends PrintStream {
+    public UncloseablePrintStream(final PrintStream printStream) {
+        super(printStream);
+    }
+
+    @Override
+    public void close() {
+        // Do nothing
+    }
+}
