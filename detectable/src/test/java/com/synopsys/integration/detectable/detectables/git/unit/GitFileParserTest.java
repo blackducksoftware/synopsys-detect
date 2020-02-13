710974a70b2e747d46ae41f8b787032d8679a0a0
Jake Mathews <jake.mathews.email@gmail.com>
2020-02-13 15:40:27 -0500

    test-refactor(git): Converted GitFileParserTest from Kotlin to Java.

---
+++ detectable/src/test/java/com/synopsys/integration/detectable/detectables/git/unit/GitFileParserTest.java
@@ -0,0 +1,105 @@
+/**
+ * detectable
+ *
+ * Copyright (c) 2020 Synopsys, Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package com.synopsys.integration.detectable.detectables.git.unit;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.input.ReaderInputStream;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import com.synopsys.integration.detectable.detectables.git.parsing.model.GitConfigElement;
+import com.synopsys.integration.detectable.detectables.git.parsing.parse.GitFileParser;
+
+class GitFileParserTest {
+    @Test
+    public void parseHeadFile() throws IOException {
+        final GitFileParser gitFileParser = new GitFileParser();
+        final String gitHeadContent = "ref: refs/heads/master\n";
+        final InputStream inputStream = IOUtils.toInputStream(gitHeadContent, StandardCharsets.UTF_8);
+        final String head = gitFileParser.parseGitHead(inputStream);
+        Assertions.assertEquals("refs/heads/master", head);
+    }
+
+    @Test
+    public void parseGitConfig() throws IOException {
+        final GitFileParser gitFileParser = new GitFileParser();
+        final String output = String.join("\n",
+            "[core]",
+            "	repositoryformatversion = 0",
+            "	filemode = true",
+            "	bare = false",
+            "	logallrefupdates = true",
+            "	ignorecase = true",
+            "	precomposeunicode = true",
+            "[remote \"origin\"]",
+            "	url = https://github.com/blackducksoftware/synopsys-detect.git",
+            "	fetch = +refs/heads/*:refs/remotes/origin/*",
+            "[branch \"master\"]",
+            "	remote = origin",
+            "	merge = refs/heads/master",
+            "[branch \"test\"]",
+            "	remote = origin",
+            "	merge = refs/heads/test"
+        );
+        final InputStream inputStream = new ReaderInputStream(new StringReader(output), StandardCharsets.UTF_8);
+
+        final List<GitConfigElement> gitConfigElements = gitFileParser.parseGitConfig(inputStream);
+        Assertions.assertEquals(4, gitConfigElements.size());
+
+        final List<GitConfigElement> gitConfigCores = getElements(gitConfigElements, "core");
+        Assertions.assertEquals(1, gitConfigCores.size());
+
+        final List<GitConfigElement> gitConfigRemotes = getElements(gitConfigElements, "remote");
+        Assertions.assertEquals(1, gitConfigRemotes.size());
+
+        final List<GitConfigElement> gitConfigBranches = getElements(gitConfigElements, "branch");
+        Assertions.assertEquals(2, gitConfigBranches.size());
+
+        final Optional<GitConfigElement> remoteOrigin = gitConfigRemotes.stream()
+                                                            .filter(it -> it.getName().isPresent())
+                                                            .filter(it -> it.getName().get().equals("origin"))
+                                                            .findAny();
+
+        Assertions.assertTrue(remoteOrigin.isPresent());
+        Assertions.assertTrue(remoteOrigin.get().containsKey("fetch"));
+
+        final String fetch = remoteOrigin.get().getProperty("fetch");
+        Assertions.assertEquals("+refs/heads/*:refs/remotes/origin/*", fetch);
+    }
+
+    @NotNull
+    private List<GitConfigElement> getElements(@NotNull final List<GitConfigElement> gitConfigElements, @NotNull final String tag) {
+        return gitConfigElements.stream()
+                   .filter(gitConfigElement -> tag.equals(gitConfigElement.getElementType()))
+                   .collect(Collectors.toList());
+    }
+}
