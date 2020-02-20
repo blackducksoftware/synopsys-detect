06fcb1cd2dcf8e31c6ea8b3adf689804478c0dfa
Jake Mathews <jake.mathews.email@gmail.com>
2020-02-20 15:16:03 -0500

    test-refactor(configuration): Converted Boolean property tests from Kotlin to Java.

--- configuration/src/test/java/com/synopsys/integration/configuration/property/types/bool/BooleanValueParserTests.java
+++ configuration/src/test/java/com/synopsys/integration/configuration/property/types/bool/BooleanValueParserTests.java
@@ -20,31 +20,30 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package com.synopsys.integration.configuration.property.types.bool
+package com.synopsys.integration.configuration.property.types.bool;
 
-import com.synopsys.integration.configuration.parse.ValueParseException
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.ValueSource
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import com.synopsys.integration.configuration.parse.ValueParseException;
 
 class BooleanValueParserTests {
     @ParameterizedTest
-    @ValueSource(strings = ["unknown", "we ird tef ", "243354323", "@Q@ASD"])
-    fun parseUnknownThrows(value: String) {
-        Assertions.assertThrows(ValueParseException::class.java) {
-            BooleanValueParser().parse(value)
-        }
+    @ValueSource(strings = { "unknown", "we ird tef ", "243354323", "@Q@ASD" })
+    public void parseUnknownThrows(final String value) {
+        Assertions.assertThrows(ValueParseException.class, () -> new BooleanValueParser().parse(value));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["tRuE", "true ", " true", "    ", "", "t"])
-    fun booleanParsesTrue(value: String) {
-        Assertions.assertTrue(BooleanValueParser().parse(value))
+    @ValueSource(strings = { "tRuE", "true ", " true", "    ", "", "t" })
+    public void booleanParsesTrue(final String value) throws ValueParseException {
+        Assertions.assertTrue(new BooleanValueParser().parse(value));
     }
 
     @ParameterizedTest
-    @ValueSource(strings = ["false", "f"])
-    fun booleanParsesFalse(value: String) {
-        Assertions.assertFalse(BooleanValueParser().parse(value))
+    @ValueSource(strings = { "false", "f" })
+    public void booleanParsesFalse(final String value) throws ValueParseException {
+        Assertions.assertFalse(new BooleanValueParser().parse(value));
     }
 }
