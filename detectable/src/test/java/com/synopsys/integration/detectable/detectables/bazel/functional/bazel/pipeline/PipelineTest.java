7b77e8f41573c354e6504a7e7ce2cbe310c196f0
Steve Billings <sbillings@blackducksoftware.com>
2020-03-10 16:06:33 +0000

    fix(bazel): maven_install: loosen up the regex trimming tag text before maven_coordinates to correctly omit other types of tags in the tag array. Fixes IDETECT-1925.

--- detectable/src/test/java/com/synopsys/integration/detectable/detectables/bazel/functional/bazel/pipeline/PipelineTest.java
+++ detectable/src/test/java/com/synopsys/integration/detectable/detectables/bazel/functional/bazel/pipeline/PipelineTest.java
@@ -40,7 +40,7 @@ import com.synopsys.integration.detectable.detectables.bazel.pipeline.stepexecut
 import com.synopsys.integration.exception.IntegrationException;
 
 public class PipelineTest {
-
+    private static final List<String> STANDARD_BAZEL_COMMAND_ARGS = Arrays.asList("cquery", "--noimplicit_deps", "kind(j.*import, deps(/:testTarget))", "--output", "build");
     private static final String cqueryOutput = "# /root/.cache/bazel/_bazel_root/896e039de1e50d7e2de0b14a9acf4028/external/exclusion_testing/BUILD:41:1\n"
                                                    + "jvm_import(\n"
                                                    + "  name = \"com_google_guava_guava\",\n"
@@ -104,12 +104,73 @@ public class PipelineTest {
                                                    + "  deps = [],\n"
                                                    + ")\n";
 
+    private static final String cqueryOutputMixed = "java_import(\n"
+                                                        + "  name = \"sso-adminsdk\",\n"
+                                                        + "  tags = [\"__SOME_OTHER_TAG__\"],\n"
+                                                        + "  generator_name = \"sso-adminsdk\",\n"
+                                                        + "  generator_function = \"java_import\",\n"
+                                                        + "  generator_location = \"/home/dail/test/b-cls-bazel/2e7559aa3afc160ade8ae4cb99d56da9/external/rd_platform_services/BUILD.bazel:238\",\n"
+                                                        + "  jars = [\"@rd_platform_services//:lin64/jars/sso-adminsdk.jar\"],\n"
+                                                        + ")\n"
+                                                        + "\n"
+                                                        + "# /home/dail/test/b-cls-bazel/2e7559aa3afc160ade8ae4cb99d56da9/external/thing_cis/BUILD.bazel:20:5\n"
+                                                        + "java_import(\n"
+                                                        + "  name = \"thing-common-client\",\n"
+                                                        + "  tags = [\"__SOME_OTHER_TAG__\", \"maven_coordinates=com.company.thing:thing-common-client:2.100.0\"],\n"
+                                                        + "  generator_name = \"thing-common-client\",\n"
+                                                        + "  generator_function = \"java_import\",\n"
+                                                        + "  generator_location = \"/home/dail/test/b-cls-bazel/2e7559aa3afc160ade8ae4cb99d56da9/external/thing_cis/BUILD.bazel:20\",\n"
+                                                        + "  jars = [\"@thing_cis//:java-toolkit/runtime/thing-common-client-2.100.0.jar\"],\n"
+                                                        + "  srcjar = \"@thing_cis//:java-toolkit/runtime/thing-common-client-2.100.0-sources.jar\",\n"
+                                                        + ")\n"
+                                                        + "\n"
+                                                        + "# /home/dail/test/b-cls-bazel/2e7559aa3afc160ade8ae4cb99d56da9/external/cls_maven_stubs/BUILD:41:1\n"
+                                                        + "jvm_import(\n"
+                                                        + "  name = \"javax_servlet_javax_servlet_api\",\n"
+                                                        + "  tags = [\"maven_coordinates=javax.servlet:javax.servlet-api:3.0.1\"],\n"
+                                                        + "  jars = [\"@cls_maven_stubs//:v1/https/build-artifactory.eng.company.com/artifactory/maven/javax/servlet/javax.servlet-api/3.0.1/javax.servlet-api-3.0.1.jar\"],\n"
+                                                        + "  srcjar = \"@cls_maven_stubs//:v1/https/build-artifactory.eng.company.com/artifactory/maven/javax/servlet/javax.servlet-api/3.0.1/javax.servlet-api-3.0.1-sources.jar\",\n"
+                                                        + "  deps = [],\n"
+                                                        + "  neverlink = True,\n"
+                                                        + ")";
+
+    private static final String cqueryOutputMixedReversedOrder = "java_import(\n"
+                                                                     + "  name = \"thing-common-client\",\n"
+                                                                     + "  tags = [\"maven_coordinates=com.company.thing:thing-common-client:2.100.0\", \"__SOME_OTHER_TAG__\"],\n"
+                                                                     + "  generator_name = \"thing-common-client\",\n"
+                                                                     + "  generator_function = \"java_import\",\n"
+                                                                     + "  generator_location = \"/home/dail/test/b-cls-bazel/2e7559aa3afc160ade8ae4cb99d56da9/external/thing_cis/BUILD.bazel:20\",\n"
+                                                                     + "  jars = [\"@thing_cis//:java-toolkit/runtime/thing-common-client-2.100.0.jar\"],\n"
+                                                                     + "  srcjar = \"@thing_cis//:java-toolkit/runtime/thing-common-client-2.100.0-sources.jar\",\n"
+                                                                     + ")";
+
     @Test
     public void testMavenInstall() throws IntegrationException {
         final List<String> userProvidedCqueryAdditionalOptions = null;
-        final List<String> expectedBazelCommandArgs = Arrays.asList("cquery", "--noimplicit_deps", "kind(j.*import, deps(/:testTarget))", "--output", "build");
 
-        doTest(expectedBazelCommandArgs, userProvidedCqueryAdditionalOptions);
+        final List<String> results = doTest(STANDARD_BAZEL_COMMAND_ARGS, userProvidedCqueryAdditionalOptions, cqueryOutput);
+        assertEquals(8, results.size());
+        assertEquals("com.google.guava:guava:27.0-jre", results.get(0));
+        assertEquals("com.google.code.findbugs:jsr305:3.0.2", results.get(7));
+    }
+
+    @Test
+    public void testMavenInstallMixedTags() throws IntegrationException {
+        final List<String> userProvidedCqueryAdditionalOptions = null;
+
+        final List<String> results = doTest(STANDARD_BAZEL_COMMAND_ARGS, userProvidedCqueryAdditionalOptions, cqueryOutputMixed);
+        assertEquals(2, results.size());
+        assertEquals("com.company.thing:thing-common-client:2.100.0", results.get(0));
+        assertEquals("javax.servlet:javax.servlet-api:3.0.1", results.get(1));
+    }
+
+    @Test
+    public void testMavenInstallMixedTagsReversedOrder() throws IntegrationException {
+        final List<String> userProvidedCqueryAdditionalOptions = null;
+
+        final List<String> results = doTest(STANDARD_BAZEL_COMMAND_ARGS, userProvidedCqueryAdditionalOptions, cqueryOutputMixedReversedOrder);
+        assertEquals(1, results.size());
+        assertEquals("com.company.thing:thing-common-client:2.100.0", results.get(0));
     }
 
     @Test
@@ -117,22 +178,23 @@ public class PipelineTest {
         final List<String> userProvidedCqueryAdditionalOptions = Arrays.asList("--option1=a", "--option2=b");
         final List<String> expectedBazelCommandArgs = Arrays.asList("cquery", "--noimplicit_deps", "--option1=a", "--option2=b", "kind(j.*import, deps(/:testTarget))", "--output", "build");
 
-        doTest(expectedBazelCommandArgs, userProvidedCqueryAdditionalOptions);
+        List<String> results = doTest(expectedBazelCommandArgs, userProvidedCqueryAdditionalOptions, cqueryOutput);
+        assertEquals(8, results.size());
+        assertEquals("com.google.guava:guava:27.0-jre", results.get(0));
+        assertEquals("com.google.code.findbugs:jsr305:3.0.2", results.get(7));
     }
 
-    private void doTest(final List<String> expectedBazelCommandArgs, final List<String> userProvidedCqueryAdditionalOptions) throws IntegrationException {
+    private List<String> doTest(final List<String> expectedBazelCommandArgs, final List<String> userProvidedCqueryAdditionalOptions, final String input) throws IntegrationException {
         final BazelCommandExecutor bazelCommandExecutor = Mockito.mock(BazelCommandExecutor.class);
-        Mockito.when(bazelCommandExecutor.executeToString(expectedBazelCommandArgs)).thenReturn(Optional.of(cqueryOutput));
+        Mockito.when(bazelCommandExecutor.executeToString(expectedBazelCommandArgs)).thenReturn(Optional.of(input));
         final BazelVariableSubstitutor bazelVariableSubstitutor = new BazelVariableSubstitutor("/:testTarget", userProvidedCqueryAdditionalOptions);
 
         final Pipelines pipelines = new Pipelines(bazelCommandExecutor, bazelVariableSubstitutor);
         System.out.printf("# maven_install steps: %d\n", pipelines.get(WorkspaceRule.MAVEN_INSTALL).size());
-        List<String> input = new ArrayList<>();
+        List<String> intermediateResult = new ArrayList<>();
         for (final StepExecutor stepExecutor : pipelines.get(WorkspaceRule.MAVEN_INSTALL)) {
-            input = stepExecutor.process(input);
+            intermediateResult = stepExecutor.process(intermediateResult);
         }
-        assertEquals(8, input.size());
-        assertEquals("com.google.guava:guava:27.0-jre", input.get(0));
-        assertEquals("com.google.code.findbugs:jsr305:3.0.2", input.get(7));
+        return intermediateResult;
     }
 }
