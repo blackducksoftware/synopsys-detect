53ec1d19bedca42e01baddef989f511306fbe99d
crowleySynopsys <alex.crowley@synopsys.com>
2020-03-10 16:14:29 -0400

    feat(test): Edited CondaCliDetectableTest to extend DetectableFunctionalTest

--- detectable/src/test/java/com/synopsys/integration/detectable/detectables/conda/unit/CondaCliDetectableTest.java
+++ detectable/src/test/java/com/synopsys/integration/detectable/detectables/conda/unit/CondaCliDetectableTest.java
@@ -24,18 +24,35 @@ package com.synopsys.integration.detectable.detectables.conda.unit;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.synopsys.integration.bdio.model.Forge;
+import com.synopsys.integration.detectable.Detectable;
 import com.synopsys.integration.detectable.DetectableEnvironment;
+import com.synopsys.integration.detectable.Extraction;
+import com.synopsys.integration.detectable.detectable.exception.DetectableException;
+import com.synopsys.integration.detectable.detectable.executable.ExecutableOutput;
 import com.synopsys.integration.detectable.detectable.executable.resolver.CondaResolver;
 import com.synopsys.integration.detectable.detectable.file.FileFinder;
 import com.synopsys.integration.detectable.detectables.conda.CondaCliDetectable;
 import com.synopsys.integration.detectable.detectables.conda.CondaCliDetectableOptions;
 import com.synopsys.integration.detectable.detectables.conda.CondaCliExtractor;
+import com.synopsys.integration.detectable.functional.DetectableFunctionalTest;
 import com.synopsys.integration.detectable.util.MockDetectableEnvironment;
 import com.synopsys.integration.detectable.util.MockFileFinder;
+import com.synopsys.integration.detectable.util.graph.NameVersionGraphAssert;
 
-public class CondaCliDetectableTest {
+public class CondaCliDetectableTest extends DetectableFunctionalTest {
+
+    public CondaCliDetectableTest() throws IOException {
+        super("conda");
+    }
+
     @Test
     public void testApplicable() {
 
@@ -47,5 +64,76 @@ public class CondaCliDetectableTest {
         final CondaCliDetectable detectable = new CondaCliDetectable(environment, fileFinder, condaResolver, condaExtractor, new CondaCliDetectableOptions("example"));
 
         assertTrue(detectable.applicable().getPassed());
+    }
+
+    @Override
+    protected void setup() throws IOException {
+        addFile("environment.yml");
+
+        ExecutableOutput condaListOutput = createStandardOutput(
+            "[",
+            "   {",
+            "       \"base_url\": null,",
+            "       \"build_number\": 0,",
+            "       \"build_string\": \"0\",",
+            "       \"channel\": \"defaults\",",
+            "       \"dist_name\": \"mkl-2017.0.3-0\",",
+            "       \"name\": \"mkl\",",
+            "       \"platform\": null,",
+            "       \"version\": \"2017.0.3\",",
+            "       \"with_features_depends\": null",
+            "   },",
+            "   {",
+            "       \"base_url\": null,",
+            "       \"build_number\": 0,",
+            "       \"build_string\": \"py36_0\",",
+            "       \"channel\": \"defaults\",",
+            "       \"dist_name\": \"numpy-1.13.1-py36_0\",",
+            "       \"name\": \"numpy\",",
+            "       \"platform\": null,",
+            "       \"version\": \"1.13.1\",",
+            "       \"with_features_depends\": null",
+            "   }",
+            "]"
+        );
+        addExecutableOutput(condaListOutput, "conda", "list", "-n", "conda-env", "--json");
+
+        ExecutableOutput condaInfoOutput = createStandardOutput(
+            "{",
+            "   \"conda_build_version\": \"not installed\",",
+            "   \"conda_env_version\": \"4.3.22\",",
+            "   \"conda_location\": \"/usr/local/miniconda3/lib/python3.6/site-packages/conda\",",
+            "   \"conda_prefix\": \"/usr/local/miniconda3\",",
+            "   \"conda_private\": false,",
+            "   \"conda_version\": \"4.3.22\",",
+            "   \"default_prefix\": \"/usr/local/miniconda3\",",
+            "   \"platform\": \"osx-64\"",
+            "}"
+        );
+        addExecutableOutput(getOutputDirectory(), condaInfoOutput, "conda", "info", "--json");
+    }
+
+    @NotNull
+    @Override
+    public Detectable create(@NotNull final DetectableEnvironment detectableEnvironment) {
+        class CondaResolverTest implements CondaResolver {
+
+            @Override
+            public File resolveConda() throws DetectableException {
+                return new File("conda");
+            }
+        }
+        return detectableFactory.createCondaCliDetectable(detectableEnvironment, new CondaResolverTest(), new CondaCliDetectableOptions("conda-env"));
+    }
+
+    @Override
+    public void assertExtraction(@NotNull final Extraction extraction) {
+        Assertions.assertNotEquals(0, extraction.getCodeLocations().size());
+
+        NameVersionGraphAssert graphAssert = new NameVersionGraphAssert(Forge.ANACONDA, extraction.getCodeLocations().get(0).getDependencyGraph());
+        graphAssert.hasRootSize(2);
+        graphAssert.hasRootDependency("mkl", "2017.0.3-0-osx-64");
+        graphAssert.hasRootDependency("numpy", "1.13.1-py36_0-osx-64");
+
     }
 }
