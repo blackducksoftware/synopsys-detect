6500da2cbf0039ed64b30bc629f9f7dc1e7c4bce
crowleySynopsys <alex.crowley@synopsys.com>
2020-04-20 12:31:57 -0400

    refactor(): Edits to resolve critical code smells

--- src/main/java/com/synopsys/integration/detect/workflow/diagnostic/DiagnosticLogUtil.java
+++ src/main/java/com/synopsys/integration/detect/workflow/diagnostic/DiagnosticLogUtil.java
@@ -24,8 +24,6 @@ package com.synopsys.integration.detect.workflow.diagnostic;
 
 import org.slf4j.LoggerFactory;
 
-import ch.qos.logback.classic.Level;
-
 public class DiagnosticLogUtil {
     public static ch.qos.logback.classic.Logger getLogger(String named) {
         final ch.qos.logback.classic.Logger logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(named);
@@ -37,6 +35,6 @@ public class DiagnosticLogUtil {
     }
 
     public static ch.qos.logback.classic.Logger getRootLogger() {
-        return getLogger(ch.qos.logback.classic.Logger.ROOT_LOGGER_NAME);
+        return getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
     }
 }
