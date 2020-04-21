517a5c1056b7cfebcd804e28643f04f6c251b9f9
crowleySynopsys <alex.crowley@synopsys.com>
2020-04-21 11:57:46 -0400

    feat(test): Created new detectable tests for Gradle

--- detectable/src/test/java/com/synopsys/integration/detectable/detectables/gradle/unit/GradleInspectorDetectableTest.java
+++ detectable/src/test/java/com/synopsys/integration/detectable/detectables/gradle/unit/GradleInspectorDetectableTest.java
@@ -22,34 +22,115 @@
  */
 package com.synopsys.integration.detectable.detectables.gradle.unit;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
-import org.junit.jupiter.api.Test;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.Assertions;
 
+import com.synopsys.integration.bdio.model.Forge;
+import com.synopsys.integration.bdio.model.externalid.ExternalId;
+import com.synopsys.integration.bdio.model.externalid.ExternalIdFactory;
+import com.synopsys.integration.detectable.Detectable;
 import com.synopsys.integration.detectable.DetectableEnvironment;
-import com.synopsys.integration.detectable.detectable.executable.resolver.GradleResolver;
-import com.synopsys.integration.detectable.detectable.file.FileFinder;
-import com.synopsys.integration.detectable.detectable.inspector.GradleInspectorResolver;
-import com.synopsys.integration.detectable.detectables.gradle.inspection.GradleDetectable;
-import com.synopsys.integration.detectable.detectables.gradle.inspection.GradleInspectorExtractor;
+import com.synopsys.integration.detectable.Extraction;
+import com.synopsys.integration.detectable.detectable.executable.ExecutableOutput;
 import com.synopsys.integration.detectable.detectables.gradle.inspection.GradleInspectorOptions;
-import com.synopsys.integration.detectable.util.MockDetectableEnvironment;
-import com.synopsys.integration.detectable.util.MockFileFinder;
+import com.synopsys.integration.detectable.detectables.gradle.inspection.inspector.GradleInspectorScriptOptions;
+import com.synopsys.integration.detectable.functional.DetectableFunctionalTest;
+import com.synopsys.integration.detectable.util.graph.NameVersionGraphAssert;
+import com.synopsys.integration.rest.proxy.ProxyInfo;
 
-public class GradleInspectorDetectableTest {
-    @Test
-    public void testApplicable() {
+public class GradleInspectorDetectableTest extends DetectableFunctionalTest {
 
-        final GradleResolver gradleResolver = null;
-        final GradleInspectorResolver gradleInspectorResolver = null;
-        final GradleInspectorExtractor gradleInspectorExtractor = null;
-        final GradleInspectorOptions gradleInspectorOptions = null;
+    public GradleInspectorDetectableTest() throws IOException {
+        super("gradle-inspector");
+    }
 
-        final DetectableEnvironment environment = MockDetectableEnvironment.empty();
-        final FileFinder fileFinder = MockFileFinder.withFileNamed("build.gradle");
+    @Override
+    protected void setup() throws IOException {
+        addFile(Paths.get("build.gradle"),
+            "buildscript {",
+            "    repositories {",
+            "        jcenter()",
+            "        mavenCentral()",
+            "        maven { url 'https://plugins.gradle.org/m2/' }",
+            "    }",
+            "    dependencies { classpath 'com.blackducksoftware.integration:common-gradle-plugin:0.0.+' }",
+            "}",
+            "",
+            "version = '13.1.1-SNAPSHOT'",
+            "",
+            "apply plugin: 'com.blackducksoftware.integration.library'",
+            "",
+            "dependencies {",
+            "    compile 'org.apache.httpcomponents:httpmime:4.5.6'",
+            "    compile group: 'commons-collections', name: 'commons-collections', version: '3.2.2'",
+            "    compile 'org.apache.commons:commons-lang3:3.7'",
+            "}"
+        );
 
-        final GradleDetectable detectable = new GradleDetectable(environment, fileFinder, gradleResolver, gradleInspectorResolver, gradleInspectorExtractor, gradleInspectorOptions);
+        ExecutableOutput gradleDependenciesOutput = createStandardOutput("");
+        addExecutableOutput(gradleDependenciesOutput, "gradle", "dependencies", "--init-script=gradle-inspector", "-DGRADLEEXTRACTIONDIR=/private" + getOutputDirectory(), "--info");
 
-        assertTrue(detectable.applicable().getPassed());
+        addOutputFile(Paths.get("rootProjectMetadata.txt"), Arrays.asList(
+            "DETECT META DATA START",
+            "rootProjectPath:/Users/ekerwin/Documents/source/integration/hub-detect",
+            "rootProjectGroup:com.blackducksoftware.integration",
+            "rootProjectName:hub-detect",
+            "rootProjectVersion:2.0.0-SNAPSHOT",
+            "DETECT META DATA END"
+        ));
+
+        addOutputFile(Paths.get("_dependencyGraph.txt"), Arrays.asList(
+            "------------------------------------------------------------",
+            "Root project",
+            "------------------------------------------------------------",
+            "",
+            "compile - Dependencies for source set 'main'.",
+            "+--- com.blackducksoftware.integration:hub-common:11.0.0-SNAPSHOT",
+            "|    +--- com.blackducksoftware.integration:hub-common-rest:1.0.0",
+            "|    |    +--- com.blackducksoftware.integration:integration-common:5.2.1",
+            "|    |    |    +--- org.apache.commons:commons-lang3:3.5",
+            "|    |    |    +--- commons-io:commons-io:2.5",
+            "|    |    |    +--- commons-codec:commons-codec:1.10",
+            "|    |    |    \\--- org.slf4j:slf4j-api:1.7.21 -> 1.7.22"
+        ));
+    }
+
+    @NotNull
+    @Override
+    public Detectable create(@NotNull final DetectableEnvironment detectableEnvironment) {
+
+        final GradleInspectorOptions gradleInspectorOptions = new GradleInspectorOptions("", new GradleInspectorScriptOptions("", "", "", "", "", ""), ProxyInfo.NO_PROXY_INFO);
+        return detectableFactory.createGradleDetectable(detectableEnvironment, gradleInspectorOptions, () -> new File("gradle-inspector"), (environment) -> new File("gradle"));
+    }
+
+    @Override
+    public void assertExtraction(@NotNull final Extraction extraction) {
+        Assertions.assertEquals(1, extraction.getCodeLocations().size());
+
+        NameVersionGraphAssert graphAssert = new NameVersionGraphAssert(Forge.MAVEN, extraction.getCodeLocations().get(0).getDependencyGraph());
+        graphAssert.hasRootSize(1);
+
+        ExternalIdFactory externalIdFactory = new ExternalIdFactory();
+
+        ExternalId hubCommonExternalId = externalIdFactory.createMavenExternalId("com.blackducksoftware.integration", "hub-common", "11.0.0-SNAPSHOT");
+        ExternalId hubCommonRestExternalId = externalIdFactory.createMavenExternalId("com.blackducksoftware.integration", "hub-common-rest", "1.0.0");
+        ExternalId integrationCommonExternalId = externalIdFactory.createMavenExternalId("com.blackducksoftware.integration", "integration-common", "5.2.1");
+        ExternalId slf4jExternalId = externalIdFactory.createMavenExternalId("org.slf4j", "slf4j-api", "1.7.22");
+        ExternalId commonsIoExternalId = externalIdFactory.createMavenExternalId("commons-io", "commons-io", "2.5");
+        ExternalId commonsCodecExternalId = externalIdFactory.createMavenExternalId("commons-codec", "commons-codec", "1.10");
+        ExternalId commonsLangExternalId = externalIdFactory.createMavenExternalId("org.apache.commons", "commons-lang3", "3.5");
+
+        graphAssert.hasParentChildRelationship(hubCommonExternalId, hubCommonRestExternalId);
+        graphAssert.hasParentChildRelationship(hubCommonRestExternalId, integrationCommonExternalId);
+        graphAssert.hasParentChildRelationship(integrationCommonExternalId, slf4jExternalId);
+        graphAssert.hasParentChildRelationship(integrationCommonExternalId, commonsIoExternalId);
+        graphAssert.hasParentChildRelationship(integrationCommonExternalId, commonsCodecExternalId);
+        graphAssert.hasParentChildRelationship(integrationCommonExternalId, commonsLangExternalId);
+
     }
 }
