6e06305c125a554ff644e18664b24d39a80e84bd
crowleySynopsys <alex.crowley@synopsys.com>
2020-04-24 13:19:52 -0400

    fix(test): Deleted testSpringFrameworkAop

--- detectable/src/test/java/com/synopsys/integration/detectable/detectables/gradle/functional/GradleReportParserFunctionalTest.java
+++ detectable/src/test/java/com/synopsys/integration/detectable/detectables/gradle/functional/GradleReportParserFunctionalTest.java
@@ -108,13 +108,6 @@ public class GradleReportParserFunctionalTest {
     }
 
     @Test
-    void testSpringFrameworkAop() {
-        final Optional<CodeLocation> codeLocation = buildCodeLocation("/gradle/spring_aop_dependencyGraph.txt");
-        Assertions.assertTrue(codeLocation.isPresent());
-        System.out.println(new GsonBuilder().setPrettyPrinting().create().toJson(codeLocation.get()));
-    }
-
-    @Test
     void testImplementationsGraph() {
         final Optional<CodeLocation> codeLocation = buildCodeLocation("/gradle/gradle_implementations_dependencyGraph.txt");
         Assertions.assertTrue(codeLocation.isPresent());
