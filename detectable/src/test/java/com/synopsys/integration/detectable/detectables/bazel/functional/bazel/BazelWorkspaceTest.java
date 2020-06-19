f8f2cd86ac17a392d38e4627cb5c8cf8b2e1413d
Steve Billings <sbillings@blackducksoftware.com>
2020-06-19 17:49:18 -0400

    test(bazel): added test for detecting multiple supported rules in a workspace file

--- detectable/src/test/java/com/synopsys/integration/detectable/detectables/bazel/functional/bazel/BazelWorkspaceTest.java
+++ detectable/src/test/java/com/synopsys/integration/detectable/detectables/bazel/functional/bazel/BazelWorkspaceTest.java
@@ -25,6 +25,7 @@ package com.synopsys.integration.detectable.detectables.bazel.functional.bazel;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
 
@@ -35,11 +36,19 @@ import com.synopsys.integration.detectable.detectables.bazel.WorkspaceRule;
 public class BazelWorkspaceTest {
 
     @Test
-    public void test() {
+    public void testSingleRule() {
         File workspaceFile = new File("src/test/resources/detectables/functional/bazel/WORKSPACE");
         BazelWorkspace bazelWorkspace = new BazelWorkspace(workspaceFile);
 
         assertEquals(Sets.newHashSet(WorkspaceRule.MAVEN_INSTALL), bazelWorkspace.getDependencyRules());
     }
-    // TODO test multiple rules
+
+    @Test
+    public void testMultipleRules() {
+        File workspaceFile = new File("src/test/resources/detectables/functional/bazel/WORKSPACE_multipleRules");
+        BazelWorkspace bazelWorkspace = new BazelWorkspace(workspaceFile);
+
+        Set<WorkspaceRule> rulesFound = bazelWorkspace.getDependencyRules();
+        assertEquals(Sets.newHashSet(WorkspaceRule.MAVEN_INSTALL, WorkspaceRule.HASKELL_CABAL_LIBRARY), rulesFound);
+    }
 }
