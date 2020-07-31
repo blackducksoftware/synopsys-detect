1e0993c75b83cf8de4eb27527ecbf2c13bf6f0fa
Steve Billings <sbillings@blackducksoftware.com>
2020-07-31 17:34:14 -0400

    fix(bazel): maven_jar: expect/handle (null) after dependency in the output of the first query; IDETECT-2152

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/bazel/pipeline/Pipelines.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/bazel/pipeline/Pipelines.java
@@ -52,7 +52,7 @@ public class Pipelines {
         Pipeline mavenJarPipeline = (new PipelineBuilder())
                                         .addIntermediateStep(new IntermediateStepExecuteBazelOnEach(bazelCommandExecutor, bazelVariableSubstitutor,
                                             Arrays.asList(CQUERY_COMMAND, CQUERY_OPTIONS_PLACEHOLDER, "filter('@.*:jar', deps(${detect.bazel.target}))"), false))
-                                        .addIntermediateStep(new IntermediateStepReplaceInEach(" \\([0-9a-f]+\\)", ""))
+                                        .addIntermediateStep(new IntermediateStepReplaceInEach(" \\([0-9a-z]+\\)", ""))
                                         .addIntermediateStep(new IntermediateStepSplitEach("\\s+"))
                                         .addIntermediateStep(new IntermediateStepReplaceInEach("^@", ""))
                                         .addIntermediateStep(new IntermediateStepReplaceInEach("//.*", ""))
