ee64ab01e4e9ac4cc0eaa41ccf759b8579f19a56
Eric Kerwin <ekerwin@synopsys.com>
2020-09-10 12:46:54 -0400

    adding an empty test

---
+++ src/test/java/com/synopsys/integration/detect/SampleTest.java
@@ -0,0 +1,13 @@
+package com.synopsys.integration.detect;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@Tag("sample")
+public class SampleTest {
+    @Test
+    public void testNothing() {
+
+    }
+
+}
