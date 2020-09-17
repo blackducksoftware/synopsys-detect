0708b95eb835697a206e18a19db0049fcd0cd61f
Steve Billings <sbillings@blackducksoftware.com>
2020-09-17 15:48:39 -0400

    refactor(clang): refactoring, mostly for better naming

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/clang/packagemanager/resolver/DpkgPkgDetailsResolver.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/clang/packagemanager/resolver/DpkgPkgDetailsResolver.java
@@ -40,39 +40,41 @@ import com.synopsys.integration.detectable.detectables.clang.packagemanager.Pack
 
 public class DpkgPkgDetailsResolver {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private static final int PKG_INFO_LINE_LABEL_POSITION = 0;
+    private static final int PKG_INFO_LINE_VALUE_POSITION = 1;
 
-    public Optional<PackageDetails> resolvePackageDetails(ClangPackageManagerInfo currentPackageManager, ExecutableRunner executableRunner, File workingDirectory, PackageDetails pkg) {
+    public Optional<PackageDetails> resolvePackageDetails(ClangPackageManagerInfo currentPackageManager, ExecutableRunner executableRunner, File workingDirectory, PackageDetails pkgPartial) {
         try {
             List<String> args = new ArrayList<>(currentPackageManager.getPkgInfoArgs().get());
-            args.add(pkg.getPackageName());
-            ExecutableOutput packageStatusOutput = executableRunner.execute(workingDirectory, currentPackageManager.getPkgMgrCmdString(), args);
-            logger.debug(String.format("packageStatusOutput: %s", packageStatusOutput));
-            return parsePackageDetailsFromStatusOutput(pkg, packageStatusOutput.getStandardOutput());
+            args.add(pkgPartial.getPackageName());
+            ExecutableOutput packageInfoOutput = executableRunner.execute(workingDirectory, currentPackageManager.getPkgMgrCmdString(), args);
+            logger.debug(String.format("packageInfoOutput: %s", packageInfoOutput));
+            return parsePackageDetailsFromInfoOutput(pkgPartial, packageInfoOutput.getStandardOutput());
         } catch (ExecutableRunnerException e) {
             logger.error(String.format("Error executing %s to get package info: %s", currentPackageManager.getPkgMgrName(), e.getMessage()));
         }
         return Optional.empty();
     }
 
-    private Optional<PackageDetails> parsePackageDetailsFromStatusOutput(PackageDetails pkg, String packageStatusOutput) {
-        String[] packageStatusOutputLines = packageStatusOutput.split("\\n");
-        for (String packageStatusOutputLine : packageStatusOutputLines) {
-            if (foundUninstalledStatus(pkg, packageStatusOutputLine)) {
+    private Optional<PackageDetails> parsePackageDetailsFromInfoOutput(PackageDetails pkg, String packageInfoOutput) {
+        String[] packageInfoOutputLines = packageInfoOutput.split("\\n");
+        for (String packageInfoOutputLine : packageInfoOutputLines) {
+            if (foundUninstalledStatus(pkg, packageInfoOutputLine)) {
                 return Optional.empty();
             }
-            consumeValueFromLineIfPresent(pkg, packageStatusOutputLine, "Architecture", () -> pkg.getPackageArch(), (String a) -> pkg.setPackageArch(a));
-            consumeValueFromLineIfPresent(pkg, packageStatusOutputLine, "Version", () -> pkg.getPackageVersion(), (String v) -> pkg.setPackageVersion(v));
+            consumeValueFromLineIfPresent(pkg, packageInfoOutputLine, "Architecture", () -> pkg.getPackageArch(), (String a) -> pkg.setPackageArch(a));
+            consumeValueFromLineIfPresent(pkg, packageInfoOutputLine, "Version", () -> pkg.getPackageVersion(), (String v) -> pkg.setPackageVersion(v));
         }
-        logger.trace(String.format("Parsed package from status output: %s", pkg));
+        logger.trace(String.format("Parsed package from pkg info output: %s", pkg));
         return Optional.of(pkg);
     }
 
-    private boolean foundUninstalledStatus(PackageDetails pkg, String packageStatusOutputLine) {
-        String[] packageStatusOutputLineNameValue = packageStatusOutputLine.split(":\\s+");
-        String label = packageStatusOutputLineNameValue[0];
+    private boolean foundUninstalledStatus(PackageDetails pkg, String packageInfoOutputLine) {
+        String[] packageInfoOutputLineParts = packageInfoOutputLine.split(":\\s+");
+        String label = packageInfoOutputLineParts[PKG_INFO_LINE_LABEL_POSITION];
         if ("Status".equals(label.trim())) {
-            if (packageStatusOutputLineNameValue.length > 1) {
-                String value = packageStatusOutputLineNameValue[1];
+            if (packageInfoOutputLineParts.length > PKG_INFO_LINE_VALUE_POSITION) {
+                String value = packageInfoOutputLineParts[PKG_INFO_LINE_VALUE_POSITION];
                 if ((value != null) && !value.contains("installed")) {
                     logger.debug(String.format("%s is not installed; Status is: %s", pkg.getPackageName(), value));
                     return true;
@@ -84,18 +86,18 @@ public class DpkgPkgDetailsResolver {
         return false;
     }
 
-    private boolean consumeValueFromLineIfPresent(PackageDetails pkg, String packageStatusOutputLine, String targetLabel, Supplier<String> oldValueGetter, Consumer<String> newValueConsumer) {
-        String[] packageStatusOutputLineNameValue = packageStatusOutputLine.split(":\\s+");
-        String parsedLabel = packageStatusOutputLineNameValue[0].trim();
+    private boolean consumeValueFromLineIfPresent(PackageDetails pkg, String packageInfoOutputLine, String targetLabel, Supplier<String> oldValueGetter, Consumer<String> newValueSetter) {
+        String[] packageInfoOutputLineParts = packageInfoOutputLine.split(":\\s+");
+        String parsedLabel = packageInfoOutputLineParts[PKG_INFO_LINE_LABEL_POSITION].trim();
         if (targetLabel.equals(parsedLabel)) {
-            if (packageStatusOutputLineNameValue.length > 1) {
-                String parsedValue = packageStatusOutputLineNameValue[1];
+            if (packageInfoOutputLineParts.length > PKG_INFO_LINE_VALUE_POSITION) {
+                String parsedValue = packageInfoOutputLineParts[PKG_INFO_LINE_VALUE_POSITION];
                 String oldValue = oldValueGetter.get();
                 if ((oldValue != null) && !oldValue.equals(parsedValue)) {
                     logger.warn("Package %s %s value changed from %s to %s during details resolution",
                         pkg.getPackageName(), targetLabel, oldValue, parsedValue);
                 }
-                newValueConsumer.accept(parsedValue);
+                newValueSetter.accept(parsedValue);
                 return true;
             } else {
                 logger.warn(String.format("Missing value for %s field for package %s", targetLabel, pkg.getPackageName()));
