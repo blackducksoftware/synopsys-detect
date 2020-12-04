6aa19954c2700d1f93ac5a6c797298cc6735389d
psantos1113 <psantos@synopsys.com>
2020-12-04 16:11:12 -0500

    refactor: Create a method to reduce complexity.

--- src/main/java/com/synopsys/integration/detect/workflow/status/DetectStatusLogger.java
+++ src/main/java/com/synopsys/integration/detect/workflow/status/DetectStatusLogger.java
@@ -23,6 +23,7 @@
 package com.synopsys.integration.detect.workflow.status;
 
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import com.synopsys.integration.detect.configuration.enumeration.ExitCodeType;
@@ -93,5 +94,77 @@ public class DetectStatusLogger {
         logger.info("===============================");
         logger.info("");
 
+    }
+
+    public void logDetectStatus2(IntLogger logger, List<Status> statusSummaries, List<DetectResult> detectResults, List<DetectIssue> detectIssues, ExitCodeType exitCodeType) {
+        logger.info("");
+        logger.info("");
+
+        logDetectIssues(logger, detectIssues);
+        logDetectResults(logger, detectResults);
+        logDetectStatus(logger, statusSummaries);
+
+        logger.info(String.format("Overall Status: %s - %s", exitCodeType.toString(), exitCodeType.getDescription()));
+        logger.info("");
+        logger.info("===============================");
+        logger.info("");
+
+    }
+
+    private void logDetectIssues(IntLogger logger, List<DetectIssue> detectIssues) {
+        if (!detectIssues.isEmpty()) {
+            logger.info("======== Detect Issues ========");
+            logger.info("");
+
+            Predicate<DetectIssue> detectorsFilter = issue -> issue.getType() == DetectIssueType.DETECTOR;
+            Predicate<DetectIssue> exceptionsFilter = issue -> issue.getType() == DetectIssueType.EXCEPTION;
+            Predicate<DetectIssue> deprecationsFilter = issue -> issue.getType() == DetectIssueType.DEPRECATION;
+            logIssuesInGroup(logger, "DETECTORS:", detectorsFilter, detectIssues);
+            logIssuesInGroup(logger, "EXCEPTIONS:", exceptionsFilter, detectIssues);
+            logIssuesInGroup(logger, "DEPRECATIONS:", deprecationsFilter, detectIssues);
+        }
+    }
+
+    private void logIssuesInGroup(IntLogger logger, String groupHeading, Predicate<DetectIssue> issueFilter, List<DetectIssue> detectIssues) {
+        List<DetectIssue> detectors = detectIssues.stream().filter(issueFilter).collect(Collectors.toList());
+        if (!detectors.isEmpty()) {
+            logger.info(groupHeading);
+            detectors.stream().flatMap(issue -> issue.getMessages().stream()).forEach(line -> logger.info("\t" + line));
+            logger.info("");
+        }
+    }
+
+    private void logDetectResults(IntLogger logger, List<DetectResult> detectResults) {
+        if (!detectResults.isEmpty()) {
+            logger.info("======== Detect Result ========");
+            logger.info("");
+            for (DetectResult detectResult : detectResults) {
+                logger.info(detectResult.getResultMessage());
+            }
+            logger.info("");
+        }
+    }
+
+    private void logDetectStatus(IntLogger logger, List<Status> statusSummaries) {
+        // sort by type, and within type, sort by description
+        statusSummaries.sort((left, right) -> {
+            if (left.getClass() == right.getClass()) {
+                return left.getDescriptionKey().compareTo(right.getDescriptionKey());
+            } else {
+                return left.getClass().getName().compareTo(right.getClass().getName());
+            }
+        });
+        logger.info("======== Detect Status ========");
+        logger.info("");
+        Class<? extends Status> previousSummaryClass = null;
+
+        for (Status status : statusSummaries) {
+            if (previousSummaryClass != null && !previousSummaryClass.equals(status.getClass())) {
+                logger.info("");
+            }
+            logger.info(String.format("%s: %s", status.getDescriptionKey(), status.getStatusType().toString()));
+
+            previousSummaryClass = status.getClass();
+        }
     }
 }
---
+++ src/test/java/com/synopsys/integration/detect/workflow/status/DetectStatusLoggerTest.java
@@ -0,0 +1,2 @@
+package com.synopsys.integration.detect.workflow.status;public class DetectStatusLoggerTest {
+}
