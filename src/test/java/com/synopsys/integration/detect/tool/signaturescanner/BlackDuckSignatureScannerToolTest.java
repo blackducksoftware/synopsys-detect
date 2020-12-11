2e49527946c6a69cb3768b56fff254a14bb910fc
crowleySynopsys <alex.crowley@synopsys.com>
2020-12-11 15:48:46 -0500

    Mocked more objects.

--- src/test/java/com/synopsys/integration/detect/tool/signaturescanner/BlackDuckSignatureScannerToolTest.java
+++ src/test/java/com/synopsys/integration/detect/tool/signaturescanner/BlackDuckSignatureScannerToolTest.java
@@ -4,10 +4,16 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import com.synopsys.integration.detect.configuration.DetectConfigurationFactory;
+import com.synopsys.integration.detect.configuration.DetectUserFriendlyException;
+import com.synopsys.integration.detect.configuration.connection.ConnectionFactory;
 import com.synopsys.integration.detect.lifecycle.DetectContext;
 import com.synopsys.integration.detect.tool.detector.file.DetectDetectorFileFilter;
 import com.synopsys.integration.detect.workflow.DetectRun;
@@ -16,19 +22,43 @@ import com.synopsys.integration.detect.workflow.codelocation.CodeLocationNameMan
 import com.synopsys.integration.detect.workflow.file.DirectoryManager;
 import com.synopsys.integration.detect.workflow.file.DirectoryOptions;
 import com.synopsys.integration.detectable.detectable.file.WildcardFileFinder;
+import com.synopsys.integration.log.SilentIntLogger;
+import com.synopsys.integration.log.Slf4jIntLogger;
+import com.synopsys.integration.rest.client.IntHttpClient;
+import com.synopsys.integration.rest.proxy.ProxyInfo;
 
 public class BlackDuckSignatureScannerToolTest {
 
     @Test
-    public void testRunScanTool() throws IOException, URISyntaxException {
+    public void testRunScanToolOnline() throws URISyntaxException, DetectUserFriendlyException {
+        BlackDuckSignatureScannerOptions blackDuckSignatureScannerOptions = Mockito.mock(BlackDuckSignatureScannerOptions.class);
+        Mockito.when(blackDuckSignatureScannerOptions.getParallelProcessors()).thenReturn(1);
+        Mockito.when(blackDuckSignatureScannerOptions.getOfflineLocalScannerInstallPath()).thenReturn(null);
+        Mockito.when(blackDuckSignatureScannerOptions.getOnlineLocalScannerInstallPath()).thenReturn(null);
+        String userProvidedScannerInstallUrl = "https://int-hub02.dc1.lan/";
+        Mockito.when(blackDuckSignatureScannerOptions.getUserProvidedScannerInstallUrl()).thenReturn(Optional.of(userProvidedScannerInstallUrl));
+
+        DetectContext detectContext = Mockito.mock(DetectContext.class);
+
+        DetectConfigurationFactory detectConfigurationFactory = Mockito.mock(DetectConfigurationFactory.class);
+        Mockito.when(detectConfigurationFactory.createBlackDuckSignatureScannerOptions()).thenReturn(blackDuckSignatureScannerOptions);
+        Mockito.when(detectContext.getBean(DetectConfigurationFactory.class)).thenReturn(detectConfigurationFactory);
+
         DirectoryManager directoryManager = Mockito.mock(DirectoryManager.class);
         File signatureScannerInstallationDirectory = new File(BlackDuckSignatureScannerToolTest.class.getClassLoader().getResource("tool/signaturescanner/tools").toURI());
         Mockito.when(directoryManager.getPermanentDirectory()).thenReturn(signatureScannerInstallationDirectory);
-        BlackDuckSignatureScanner blackDuckSignatureScanner = Mockito.mock(BlackDuckSignatureScanner.class);
-
-        BlackDuckSignatureScannerOptions blackDuckSignatureScannerOptions = Mockito.mock(BlackDuckSignatureScannerOptions.class);
-        DetectContext detectContext = Mockito.mock(DetectContext.class);
         Mockito.when(detectContext.getBean(DirectoryManager.class)).thenReturn(directoryManager);
-        Mockito.when(detectContext.getBean(BlackDuckSignatureScanner.class))
+
+        ConnectionFactory connectionFactory = Mockito.mock(ConnectionFactory.class);
+        Logger logger = LoggerFactory.getLogger(BlackDuckSignatureScannerToolTest.class);
+        IntHttpClient intHttpClient = new IntHttpClient(new Slf4jIntLogger(logger), 0, true, ProxyInfo.NO_PROXY_INFO);
+        Mockito.when(connectionFactory.createConnection(userProvidedScannerInstallUrl, new SilentIntLogger())).thenReturn(intHttpClient);
+        Mockito.when(detectContext.getBean(ConnectionFactory.class)).thenReturn(connectionFactory);
+
+        CodeLocationNameGenerator codeLocationNameGenerator = Mockito.mock(CodeLocationNameGenerator.class);
+        CodeLocationNameManager codeLocationNameManager = Mockito.mock(CodeLocationNameManager.class);
+        Mockito.when(detectContext.getBean(CodeLocationNameManager.class, codeLocationNameGenerator)).thenReturn(codeLocationNameManager);
+
+        Mockito.when(detectContext.getBean(BlackDuckSignatureScanner.class, ));
     }
 }
