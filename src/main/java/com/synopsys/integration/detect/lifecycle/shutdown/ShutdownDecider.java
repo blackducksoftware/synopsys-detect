80f0ea38bc4b86cf9ddb1186bc5a499239a2d582
Jordan Piscitelli <jordanp@synopsys.com>
2020-12-14 16:24:53 -0500

    refactor: Split shutdown into decision and execution to better describe and test.

---
+++ src/main/java/com/synopsys/integration/detect/lifecycle/shutdown/ShutdownDecider.java
@@ -0,0 +1,45 @@
+package com.synopsys.integration.detect.lifecycle.shutdown;
+
+import java.io.File;
+import java.util.Optional;
+
+import org.jetbrains.annotations.Nullable;
+
+import com.synopsys.integration.configuration.config.PropertyConfiguration;
+import com.synopsys.integration.detect.configuration.DetectProperties;
+import com.synopsys.integration.detect.lifecycle.boot.DetectBootResult;
+import com.synopsys.integration.detect.lifecycle.run.data.ProductRunData;
+import com.synopsys.integration.detect.workflow.diagnostic.DiagnosticSystem;
+import com.synopsys.integration.detect.workflow.phonehome.PhoneHomeManager;
+
+public class ShutdownDecider {
+    public ShutdownDecision decideShutdown(DetectBootResult detectBootResult) {
+        Optional<ProductRunData> productRunData = detectBootResult.getProductRunData();
+        DiagnosticSystem diagnosticSystem = detectBootResult.getDiagnosticSystem().orElse(null);
+
+        PhoneHomeManager phoneHomeManager = null;
+        if (productRunData.isPresent() && productRunData.get().shouldUseBlackDuckProduct()) {
+            phoneHomeManager = productRunData.get().getBlackDuckRunData().getPhoneHomeManager().orElse(null);
+        }
+
+        CleanupDecision cleanupDecision = decideCleanup(detectBootResult.getDetectConfiguration().orElse(null), detectBootResult.getProductRunData().orElse(null), detectBootResult.getAirGapZip().orElse(null));
+        return new ShutdownDecision(phoneHomeManager, diagnosticSystem, cleanupDecision);
+    }
+
+    public CleanupDecision decideCleanup(@Nullable PropertyConfiguration detectConfiguration, @Nullable ProductRunData productRunData, @Nullable File airGapZip) {
+        if (detectConfiguration == null || !detectConfiguration.getValue(DetectProperties.DETECT_CLEANUP.getProperty())) {
+            return new CleanupDecision(false, false, false, false, null);
+        }
+        boolean dryRun = detectConfiguration.getValue(DetectProperties.DETECT_BLACKDUCK_SIGNATURE_SCANNER_DRY_RUN.getProperty());
+
+        boolean offline = false;
+        if (productRunData != null && productRunData.shouldUseBlackDuckProduct()) {
+            offline = !productRunData.getBlackDuckRunData().isOnline();
+        }
+
+        boolean preserveScan = dryRun || offline;
+        boolean preserveBdio = offline;
+        boolean preserveAirGap = airGapZip != null;
+        return new CleanupDecision(true, preserveScan, preserveBdio, preserveAirGap, airGapZip);
+    }
+}
---
+++ src/test/java/com/synopsys/integration/detect/lifecycle/shutdown/ShutdownDeciderTest.java
@@ -0,0 +1,52 @@
+package com.synopsys.integration.detect.lifecycle.shutdown;
+
+import java.io.File;
+
+import org.apache.commons.lang3.tuple.Pair;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.synopsys.integration.configuration.config.PropertyConfiguration;
+import com.synopsys.integration.configuration.util.ConfigTestUtils;
+import com.synopsys.integration.detect.configuration.DetectProperties;
+import com.synopsys.integration.detect.lifecycle.run.data.BlackDuckRunData;
+import com.synopsys.integration.detect.lifecycle.run.data.ProductRunData;
+
+public class ShutdownDeciderTest {
+    @Test
+    public void defaultDoesNotPreserve() {
+        CleanupDecision decision = new ShutdownDecider().decideCleanup(ConfigTestUtils.emptyConfig(), null, null);
+        Assert.assertTrue(decision.shouldCleanup());
+        Assert.assertFalse(decision.shouldPreserveAirGap());
+        Assert.assertFalse(decision.shouldPreserveBdio());
+        Assert.assertFalse(decision.shouldPreserveScan());
+    }
+
+    @Test
+    public void whenAirGapProvidedItIsPreserved() {
+        CleanupDecision decision = new ShutdownDecider().decideCleanup(ConfigTestUtils.emptyConfig(), null, new File("."));
+        Assert.assertTrue(decision.shouldPreserveAirGap());
+    }
+
+    @Test
+    public void shouldPreserveBdioAndScanIfOffline() {
+        ProductRunData productRunData = new ProductRunData(null, BlackDuckRunData.offline());
+        CleanupDecision decision = new ShutdownDecider().decideCleanup(ConfigTestUtils.emptyConfig(), productRunData, null);
+        Assert.assertTrue(decision.shouldPreserveBdio());
+        Assert.assertTrue(decision.shouldPreserveScan());
+    }
+
+    @Test
+    public void shouldPreserveScanIfDryRun() {
+        PropertyConfiguration configuration = ConfigTestUtils.configOf(Pair.of(DetectProperties.DETECT_BLACKDUCK_SIGNATURE_SCANNER_DRY_RUN.getProperty().getKey(), "true"));
+        CleanupDecision decision = new ShutdownDecider().decideCleanup(configuration, null, null);
+        Assert.assertTrue(decision.shouldPreserveScan());
+    }
+
+    @Test
+    public void shouldSkipCleanupIfProvidedFalse() {
+        PropertyConfiguration configuration = ConfigTestUtils.configOf(Pair.of(DetectProperties.DETECT_CLEANUP.getProperty().getKey(), "false"));
+        CleanupDecision decision = new ShutdownDecider().decideCleanup(configuration, null, null);
+        Assert.assertFalse(decision.shouldCleanup());
+    }
+}
