8073b19ed8ba67422100ad896074ad9f128ce93c
Steve Billings <sbillings@blackducksoftware.com>
2020-12-18 14:16:04 -0500

    style(help): make it clear that code.location.name overrides prefix and suffix

--- src/main/java/com/synopsys/integration/detect/configuration/DetectProperties.java
+++ src/main/java/com/synopsys/integration/detect/configuration/DetectProperties.java
@@ -317,7 +317,8 @@ public class DetectProperties {
     public static final DetectProperty<NullableStringProperty> DETECT_CODE_LOCATION_NAME =
         new DetectProperty<>(new NullableStringProperty("detect.code.location.name"))
             .setInfo("Scan Name", DetectPropertyFromVersion.VERSION_4_0_0)
-            .setHelp("An override for the name Detect will use for the scan file it creates. If supplied and multiple scans are found, Detect will append an index to each scan name.")
+            .setHelp(
+                "An override for the name Detect will use for the scan file it creates. If supplied and multiple scans are found, Detect will append an index to each scan name. When this property is set, detect.project.codelocation.prefix and detect.project.codelocation.suffix are ignored.")
             .setGroups(DetectGroup.PROJECT, DetectGroup.PROJECT_SETTING)
             .setCategory(DetectCategory.Advanced);
 
