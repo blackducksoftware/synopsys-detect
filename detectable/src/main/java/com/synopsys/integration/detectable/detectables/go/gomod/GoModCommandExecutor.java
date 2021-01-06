0e64c4c966f0bed5a8b0ee97c9a84348a68f2322
Eric Kerwin <ekerwin@synopsys.com>
2021-01-06 15:33:17 -0500

    move variable to variable block

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/go/gomod/GoModCommandExecutor.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/go/gomod/GoModCommandExecutor.java
@@ -37,6 +37,8 @@ public class GoModCommandExecutor {
     private static final String FAILURE_MSG_QUERYING_FOR_THE_GO_MOD_GRAPH = "Querying for the go mod graph failed:";
     private static final String FAILURE_MSG_QUERYING_GO_FOR_THE_LIST_OF_MODULES = "Querying go for the list of modules failed: ";
     private static final String FAILURE_MSG_QUERYING_FOR_THE_VERSION = "Querying for the version failed: ";
+    private static final Pattern GENERATE_GO_LIST_U_JSON_OUTPUT_PATTERN = Pattern.compile("\\d+\\.[\\d.]+");
+
     private final DetectableExecutableRunner executableRunner;
 
     public GoModCommandExecutor(DetectableExecutableRunner executableRunner) {
@@ -46,8 +48,6 @@ public class GoModCommandExecutor {
     List<String> generateGoListOutput(File directory, File goExe) throws ExecutableRunnerException, DetectableException {
         return execute(directory, goExe, FAILURE_MSG_QUERYING_GO_FOR_THE_LIST_OF_MODULES, "list", "-m");
     }
-
-    private static final Pattern GENERATE_GO_LIST_U_JSON_OUTPUT_PATTERN = Pattern.compile("\\d+\\.[\\d.]+");
 
     List<String> generateGoListUJsonOutput(File directory, File goExe) throws ExecutableRunnerException, DetectableException {
         List<String> goVersionOutput = execute(directory, goExe, FAILURE_MSG_QUERYING_FOR_THE_VERSION, "version");
