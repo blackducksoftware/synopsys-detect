857c9cb3864a97157227ce03a73490cd612373af
XenoAmess <xenoamess@gmail.com>
2021-01-06 16:44:03 +0800

    memory: add static to some inner classes to reduce memory used.

--- buildSrc/src/main/java/com/synopsys/integration/detect/docs/model/DetectorStatusCodes.java
+++ buildSrc/src/main/java/com/synopsys/integration/detect/docs/model/DetectorStatusCodes.java
@@ -45,7 +45,7 @@ public class DetectorStatusCodes {
         return sortedCodes;
     }
 
-    private class HelpJsonDetectorStatusCodeComparator implements Comparator<HelpJsonDetectorStatusCode> {
+    private static class HelpJsonDetectorStatusCodeComparator implements Comparator<HelpJsonDetectorStatusCode> {
 
         @Override
         public int compare(final HelpJsonDetectorStatusCode o1, final HelpJsonDetectorStatusCode o2) {
