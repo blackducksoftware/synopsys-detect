f0620d1fa51e286fda64ad60921a02f08cdbeb9d
Richard Otte <rotte@blackducksoftware.com>
2021-01-22 14:22:14 -0500

    test: Moving BuildGradleParserTest to go with other functional tests

--- detectable/src/test/java/com/synopsys/integration/detectable/detectables/gradle/functional/BuildGradleParserTest.java
+++ detectable/src/test/java/com/synopsys/integration/detectable/detectables/gradle/functional/BuildGradleParserTest.java
@@ -1,4 +1,4 @@
-package com.synopsys.integration.detectable.detectables.gradle.unit;
+package com.synopsys.integration.detectable.detectables.gradle.functional;
 
 import java.io.InputStream;
 
@@ -10,11 +10,10 @@ import com.synopsys.integration.bdio.model.externalid.ExternalIdFactory;
 import com.synopsys.integration.detectable.detectables.gradle.parsing.parse.BuildGradleParser;
 
 public class BuildGradleParserTest {
-
     @Test
     public void testComplexBuildGradle() {
         BuildGradleParser buildGradleParser = new BuildGradleParser(new ExternalIdFactory());
-        InputStream buildGradle = BuildGradleParserTest.class.getResourceAsStream("/detectables/unit/gradle/complexBuild.gradle");
+        InputStream buildGradle = BuildGradleParserTest.class.getResourceAsStream("/detectables/functional/gradle/complexBuild.gradle");
         DependencyGraph dependencyGraph = buildGradleParser.parse(buildGradle).get();
 
         Assertions.assertTrue(dependencyGraph.getRootDependencies().size() > 0);
@@ -24,9 +23,10 @@ public class BuildGradleParserTest {
     @Test
     public void testSimpleBuildGradle() {
         BuildGradleParser buildGradleParser = new BuildGradleParser(new ExternalIdFactory());
-        InputStream buildGradle = BuildGradleParserTest.class.getResourceAsStream("/detectables/unit/gradle/intCommonBuild.gradle");
+        InputStream buildGradle = BuildGradleParserTest.class.getResourceAsStream("/detectables/functional/gradle/intCommonBuild.gradle");
         DependencyGraph dependencyGraph = buildGradleParser.parse(buildGradle).get();
 
         Assertions.assertEquals(13, dependencyGraph.getRootDependencies().size());
     }
+
 }
