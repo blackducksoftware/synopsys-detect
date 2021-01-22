9fa7a844f2be4993f0c2b24efe41f5ef7e6a48bf
Richard Otte <rotte@blackducksoftware.com>
2021-01-22 17:00:51 -0500

    test: Removing unused resources, formatting files, adding pip README

--- detectable/src/test/java/com/synopsys/integration/detectable/detectables/gradle/functional/BuildGradleParserTest.java
+++ detectable/src/test/java/com/synopsys/integration/detectable/detectables/gradle/functional/BuildGradleParserTest.java
@@ -7,13 +7,16 @@ import org.junit.jupiter.api.Test;
 
 import com.synopsys.integration.bdio.graph.DependencyGraph;
 import com.synopsys.integration.bdio.model.externalid.ExternalIdFactory;
+import com.synopsys.integration.detectable.annotations.FunctionalTest;
 import com.synopsys.integration.detectable.detectables.gradle.parsing.parse.BuildGradleParser;
+import com.synopsys.integration.detectable.util.FunctionalTestFiles;
 
+@FunctionalTest
 public class BuildGradleParserTest {
     @Test
     public void testComplexBuildGradle() {
         BuildGradleParser buildGradleParser = new BuildGradleParser(new ExternalIdFactory());
-        InputStream buildGradle = BuildGradleParserTest.class.getResourceAsStream("/detectables/functional/gradle/complexBuild.gradle");
+        InputStream buildGradle = FunctionalTestFiles.asInputStream("/gradle/complexBuild.gradle");
         DependencyGraph dependencyGraph = buildGradleParser.parse(buildGradle).get();
 
         Assertions.assertTrue(dependencyGraph.getRootDependencies().size() > 0);
@@ -23,7 +26,7 @@ public class BuildGradleParserTest {
     @Test
     public void testSimpleBuildGradle() {
         BuildGradleParser buildGradleParser = new BuildGradleParser(new ExternalIdFactory());
-        InputStream buildGradle = BuildGradleParserTest.class.getResourceAsStream("/detectables/functional/gradle/intCommonBuild.gradle");
+        InputStream buildGradle = FunctionalTestFiles.asInputStream("/gradle/intCommonBuild.gradle");
         DependencyGraph dependencyGraph = buildGradleParser.parse(buildGradle).get();
 
         Assertions.assertEquals(13, dependencyGraph.getRootDependencies().size());
---
+++ src/test/resources/pip/README.txt
@@ -0,0 +1,3 @@
+Currently example-requirements.txt and other-requirements.txt exist only as manual testing files.
+They should eventually be used as a functional test of the pip inspector's python code.
+-rotte JAN 2021
