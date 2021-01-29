a26e43d5adcbd0b6a9a43ec3cc7a5c1d0515a3f7
Steve Billings <sbillings@blackducksoftware.com>
2021-01-29 10:53:10 -0500

    style: fix typo in log msg

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/yarn/parse/YarnTransformer.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/yarn/parse/YarnTransformer.java
@@ -63,12 +63,13 @@ public class YarnTransformer {
                     if (!productionOnly || !dependency.isOptional()) {
                         graphBuilder.addChildWithParent(stringDependencyId, id);
                     } else {
-                        logger.debug("Eluding optional dependency: {}", stringDependencyId.getValue());
+                        logger.debug("Excluding optional dependency: {}", stringDependencyId.getValue());
                     }
                 }
             }
         }
-
+        // TODO huh? externalDependencies comes in empty; how does it get populated?
+        // TODO half the detector's work is happening in this single LOC!
         return graphBuilder.build((dependencyId, lazyDependencyInfo) -> {
             Optional<NameVersion> externalDependency = externalDependencies.stream().filter(it -> it.getName().equals(lazyDependencyInfo.getName())).findFirst();
             Optional<ExternalId> externalId = externalDependency.map(it -> externalIdFactory.createNameVersionExternalId(Forge.NPMJS, it.getName(), it.getVersion()));
