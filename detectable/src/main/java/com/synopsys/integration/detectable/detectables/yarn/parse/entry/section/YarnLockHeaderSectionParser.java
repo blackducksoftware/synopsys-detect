5c734a4db3c8d715f0ab276a3ed8019e0523736a
Steve Billings <sbillings@blackducksoftware.com>
2021-02-15 16:44:20 -0500

    refactor: cleanup

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/yarn/parse/entry/section/YarnLockHeaderSectionParser.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/yarn/parse/entry/section/YarnLockHeaderSectionParser.java
@@ -48,15 +48,9 @@ public class YarnLockHeaderSectionParser implements YarnLockEntrySectionParser {
 
     @Override
     public int parseSection(YarnLockEntryBuilder entryBuilder, List<String> yarnLockLines, int lineIndexOfStartOfSection) {
-        String line = yarnLockLines.get(lineIndexOfStartOfSection);
-        line = StringUtils.removeEnd(line.trim(), ":").trim();
-        int doubleQuoteCount = StringUtils.countMatches(line, '"');
-        int singleQuoteCount = StringUtils.countMatches(line, '\'');
-        if (line.startsWith("\"") && line.endsWith("\"") && (doubleQuoteCount == 2)) {
-            line = yarnLockLineAnalyzer.unquote(line);
-        } else if (line.startsWith("'") && line.endsWith("'") && (singleQuoteCount == 2)) {
-            line = yarnLockLineAnalyzer.unquote(line);
-        }
+        String line = yarnLockLines.get(lineIndexOfStartOfSection).trim();
+        line = StringUtils.removeEnd(line, ":").trim();
+        line = yarnLockLineAnalyzer.unquote(line);
         StringTokenizer tokenizer = TokenizerFactory.createHeaderTokenizer(line);
         while (tokenizer.hasMoreTokens()) {
             String rawEntryString = tokenizer.nextToken().trim();
