c481296c1ea63a6443083537b8212a623bcee831
crowleySynopsys <alex.crowley@synopsys.com>
2021-03-09 14:45:42 -0500

    feat(test): Test wildcard matching for paths.

--- src/test/java/com/synopsys/integration/detect/tool/detector/impl/DetectExcludedDirectoryFilterTest.java
+++ src/test/java/com/synopsys/integration/detect/tool/detector/impl/DetectExcludedDirectoryFilterTest.java
@@ -22,15 +22,24 @@
  */
 package com.synopsys.integration.detect.tool.detector.impl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
 import java.io.File;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import com.synopsys.integration.detect.util.finder.DetectExcludedDirectoryFilter;
 
@@ -47,9 +56,9 @@ class DetectExcludedDirectoryFilterTest {
         final File root2 = new File(sourcePath.toFile(), "root2");
         final File doNotExcludeDir = new File(root, "doNotExclude");
 
-        Assertions.assertTrue(detectExcludedDirectoryFilter.isExcluded(root));
-        Assertions.assertTrue(detectExcludedDirectoryFilter.isExcluded(root2));
-        Assertions.assertFalse(detectExcludedDirectoryFilter.isExcluded(doNotExcludeDir));
+        assertTrue(detectExcludedDirectoryFilter.isExcluded(root));
+        assertTrue(detectExcludedDirectoryFilter.isExcluded(root2));
+        assertFalse(detectExcludedDirectoryFilter.isExcluded(doNotExcludeDir));
     }
 
     @Test
@@ -65,10 +74,41 @@ class DetectExcludedDirectoryFilterTest {
         final File subDir2 = new File(root, "subDir2");
         final File deepSubDir2 = new File(subDir1, "subDir2");
 
-        Assertions.assertFalse(detectExcludedDirectoryFilter.isExcluded(root));
-        Assertions.assertFalse(detectExcludedDirectoryFilter.isExcluded(subDir1));
-        Assertions.assertFalse(detectExcludedDirectoryFilter.isExcluded(subDir2));
-        Assertions.assertTrue(detectExcludedDirectoryFilter.isExcluded(deepSubDir2));
+        assertFalse(detectExcludedDirectoryFilter.isExcluded(root));
+        assertFalse(detectExcludedDirectoryFilter.isExcluded(subDir1));
+        assertFalse(detectExcludedDirectoryFilter.isExcluded(subDir2));
+        assertTrue(detectExcludedDirectoryFilter.isExcluded(deepSubDir2));
+    }
+
+    @ParameterizedTest
+    @MethodSource("inputPatternsToExclusionsProvider")
+    void testIsExcludedDirectoryPathPatterns(String exclusionPattern, boolean sub1Excluded, boolean sub2Excluded, boolean sub1Sub1Excluded, boolean sub1Sub2Excluded, boolean sub2Sub1Excluded) {
+        final Path sourcePath = new File("").toPath();
+        final List<String> excludedDirectories = new ArrayList<>();
+
+        final DetectExcludedDirectoryFilter filter = new DetectExcludedDirectoryFilter(sourcePath, excludedDirectories, Collections.singletonList(exclusionPattern), new LinkedList<>());
+        final File root = new File("root");
+        final File sub1 = new File(root, "sub1");
+        final File sub2 = new File(root, "sub2");
+        final File sub1Sub1 = new File(sub1, "sub1Sub1");
+        final File sub1Sub2 = new File(sub1, "sub1Sub2");
+        final File sub2Sub1 = new File(sub2, "sub2Sub1");
+
+        assertEquals(sub1Excluded, filter.isExcluded(sub1));
+        assertEquals(sub2Excluded, filter.isExcluded(sub2));
+        assertEquals(sub1Sub1Excluded, filter.isExcluded(sub1Sub1));
+        assertEquals(sub1Sub2Excluded, filter.isExcluded(sub1Sub2));
+        assertEquals(sub2Sub1Excluded, filter.isExcluded(sub2Sub1));
+
+    }
+
+    static Stream<Arguments> inputPatternsToExclusionsProvider() {
+        // Stream of single input patterns and booleans corresponding to the files created in testIsExcludedDirectoryPathPatterns (with the exception of root), whether or not they should be excluded according to the pattern
+        return Stream.of(
+            arguments("**ub2", false, true, false, true, false),
+            arguments("**root/sub1/sub1*", false, false, true, true, false),
+            arguments("**root/*/*", false, false, true, true, true)
+        );
     }
 
     @Test
@@ -85,10 +125,10 @@ class DetectExcludedDirectoryFilterTest {
         final File deepSubDir2 = new File(subDir1, "subDir2");
         final File namePatternsDir = new File(root, "namePatternsDir51134");
 
-        Assertions.assertFalse(detectExcludedDirectoryFilter.isExcluded(root));
-        Assertions.assertTrue(detectExcludedDirectoryFilter.isExcluded(subDir1));
-        Assertions.assertFalse(detectExcludedDirectoryFilter.isExcluded(subDir2));
-        Assertions.assertFalse(detectExcludedDirectoryFilter.isExcluded(deepSubDir2));
-        Assertions.assertTrue(detectExcludedDirectoryFilter.isExcluded(namePatternsDir));
+        assertFalse(detectExcludedDirectoryFilter.isExcluded(root));
+        assertTrue(detectExcludedDirectoryFilter.isExcluded(subDir1));
+        assertFalse(detectExcludedDirectoryFilter.isExcluded(subDir2));
+        assertFalse(detectExcludedDirectoryFilter.isExcluded(deepSubDir2));
+        assertTrue(detectExcludedDirectoryFilter.isExcluded(namePatternsDir));
     }
 }
--- src/test/java/com/synopsys/integration/detect/workflow/blackduck/ExclusionPatternCreatorTest.java
+++ src/test/java/com/synopsys/integration/detect/workflow/blackduck/ExclusionPatternCreatorTest.java
@@ -1,15 +1,69 @@
 package com.synopsys.integration.detect.workflow.blackduck;
 
-import java.util.Arrays;
-import java.util.List;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import com.synopsys.integration.common.util.finder.WildcardFileFinder;
+import com.synopsys.integration.detect.util.finder.DetectExcludedDirectoryFilter;
 
 public class ExclusionPatternCreatorTest {
-    @Test
-    public void testProducesCorrectExclusions() {
-        List<String> patterns = Arrays.asList(
+    @ParameterizedTest
+    @MethodSource("inputPatternsToExclusionsProvider")
+    public void testProducesCorrectExclusions(List<String> providedPatterns, List<String> resultingExclusions) throws IOException {
+        final File root = new File("root");
+        root.mkdir();
+        final File sub1 = new File(root, "sub1");
+        sub1.mkdir();
+        final File sub2 = new File(root, "sub2");
+        sub2.mkdir();
+        final File sub1Sub1 = new File(sub1, "sub1Sub1");
+        sub1Sub1.mkdir();
+        final File sub1Sub2 = new File(sub1, "sub1Sub2");
+        sub1Sub2.mkdir();
+        final File sub2Sub1 = new File(sub2, "sub2Sub1");
+        sub2Sub1.mkdir();
 
+        DetectExcludedDirectoryFilter filter = new DetectExcludedDirectoryFilter(root.toPath(), providedPatterns, providedPatterns, providedPatterns);
+        ExclusionPatternCreator exclusionPatternCreator = new ExclusionPatternCreator(new WildcardFileFinder(), file -> filter.isExcluded(file), root);
+        assertEqualCollections(resultingExclusions, exclusionPatternCreator.determineExclusionPatterns(3, providedPatterns));
+    }
+
+    static Stream<Arguments> inputPatternsToExclusionsProvider() {
+        // Stream of single input patterns and the list of patterns that they should resolve to
+        return Stream.of(
+            arguments(Collections.singletonList("**ub2"), Arrays.asList("/sub1/sub1Sub2/", "/sub2/")),
+            arguments(Collections.singletonList("**root/sub1/sub1*"), Arrays.asList("/sub1/sub1Sub1/", "/sub1/sub1Sub2/")),
+            arguments(Collections.singletonList("**root/*/*"), Arrays.asList("/sub1/sub1Sub1/", "/sub1/sub1Sub2/", "/sub2/sub2Sub1/")),
+            arguments(Collections.singletonList("*1Sub2"), Collections.singletonList("/sub1/sub1Sub2/")),
+            arguments(Collections.singletonList("sub?"), Arrays.asList("/sub1/", "/sub2/")),
+            arguments(Collections.singletonList("/blackduck-*/"), Collections.emptyList())
         );
+    }
+
+    private void assertEqualCollections(Collection<String> collection1, Collection<String> collection2) {
+        for (String item1 : collection1) {
+            if (!collection2.contains(item1)) {
+                Assertions.fail();
+            }
+        }
+        for (String item2 : collection2) {
+            if (!collection1.contains(item2)) {
+                Assertions.fail();
+            }
+        }
     }
 }
