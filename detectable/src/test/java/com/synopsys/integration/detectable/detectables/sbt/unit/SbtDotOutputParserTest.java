692b0175dbb10ca9f7e82b8008b90ad2f7488cc4
Jordan Piscitelli <jordanp@synopsys.com>
2021-03-29 16:42:34 -0400

    chore: Updating headers and fixing tests.

--- detectable/src/test/java/com/synopsys/integration/detectable/detectables/sbt/unit/SbtDotOutputParserTest.java
+++ detectable/src/test/java/com/synopsys/integration/detectable/detectables/sbt/unit/SbtDotOutputParserTest.java
@@ -49,18 +49,18 @@ public class SbtDotOutputParserTest {
     @Test
     public void parsesMultipleGraphsNIXPaths() {
         List<String> input = Arrays.asList(
-                "[warn] There may be incompatibilities among your library dependencies; run 'evicted' to see detailed eviction warnings.",
-                "[info] Wrote dependency graph to '/Users/jordanp/scalafmt-master/scalafmt/scalafmt-interfaces/target/dependencies-compile.dot'",
-                "[warn] There may be incompatibilities among your library dependencies; run 'evicted' to see detailed eviction warnings.",
-                "[warn] There may be incompatibilities among your library dependencies; run 'evicted' to see detailed eviction warnings.",
-                "[info] Wrote dependency graph to '/Users/jordanp/scalafmt-master/scalafmt/target/dependencies-compile.dot'",
-                "[info] Wrote dependency graph to '/Users/jordanp/scalafmt-master/scalafmt/scalafmt-dynamic/target/dependencies-compile.dot'");
+            "[warn] There may be incompatibilities among your library dependencies; run 'evicted' to see detailed eviction warnings.",
+            "[info] Wrote dependency graph to '/Users/jordanp/scalafmt-master/scalafmt/scalafmt-interfaces/target/dependencies-compile.dot'",
+            "[warn] There may be incompatibilities among your library dependencies; run 'evicted' to see detailed eviction warnings.",
+            "[warn] There may be incompatibilities among your library dependencies; run 'evicted' to see detailed eviction warnings.",
+            "[info] Wrote dependency graph to '/Users/jordanp/scalafmt-master/scalafmt/target/dependencies-compile.dot'",
+            "[info] Wrote dependency graph to '/Users/jordanp/scalafmt-master/scalafmt/scalafmt-dynamic/target/dependencies-compile.dot'");
         SbtDotOutputParser parser = new SbtDotOutputParser();
         List<File> results = parser.parseGeneratedGraphFiles(input);
         Assertions.assertEquals(3, results.size());
-        Assertions.assertEquals("/Users/jordanp/scalafmt-master/scalafmt/scalafmt-interfaces/target/dependencies-compile.dot", results.get(0).toString());
-        Assertions.assertEquals("/Users/jordanp/scalafmt-master/scalafmt/target/dependencies-compile.dot", results.get(1).toString());
-        Assertions.assertEquals("/Users/jordanp/scalafmt-master/scalafmt/scalafmt-dynamic/target/dependencies-compile.dot", results.get(2).toString());
+        Assertions.assertEquals("\\Users\\jordanp\\scalafmt-master\\scalafmt\\scalafmt-interfaces\\target\\dependencies-compile.dot", results.get(0).toString());
+        Assertions.assertEquals("\\Users\\jordanp\\scalafmt-master\\scalafmt\\target\\dependencies-compile.dot", results.get(1).toString());
+        Assertions.assertEquals("\\Users\\jordanp\\scalafmt-master\\scalafmt\\scalafmt-dynamic\\target\\dependencies-compile.dot", results.get(2).toString());
     }
 
 }
