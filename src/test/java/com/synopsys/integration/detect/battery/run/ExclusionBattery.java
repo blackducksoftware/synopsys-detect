88a3af878f7dda9d099c3f8641d0d64cafe07cce
Jordan Piscitelli <jordanp@synopsys.com>
2021-06-03 13:30:50 -0400

    test: Actually disabling not yet working test.

--- src/test/java/com/synopsys/integration/detect/battery/run/ExclusionBattery.java
+++ src/test/java/com/synopsys/integration/detect/battery/run/ExclusionBattery.java
@@ -1,16 +1,14 @@
 package com.synopsys.integration.detect.battery.run;
 
-import org.junit.Ignore;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 
 import com.synopsys.integration.detect.battery.util.DetectOutput;
 import com.synopsys.integration.detect.battery.util.DetectorBatteryTest;
 
-@Ignore //Does not pass yet.
 @Tag("battery")
 class ExclusionBattery {
-    @Test
+    @Disabled
     void excludesTwo() { /* Requires Signature Scanner */
         DetectorBatteryTest test = new DetectorBatteryTest("exclusions");
         test.withToolsValue("SIGNATURE_SCAN");
