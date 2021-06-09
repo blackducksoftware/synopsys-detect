cde5d35298a117990d5341efb630fa69adb0d0b4
Steve Billings <billings@synopsys.com>
2021-06-09 15:46:31 -0400

    test: refactor new test

--- detector/src/test/java/com/synopsys/integration/detector/evaluation/DetectorAggregateEvaluationResultTest.java
+++ detector/src/test/java/com/synopsys/integration/detector/evaluation/DetectorAggregateEvaluationResultTest.java
@@ -4,6 +4,7 @@ import com.synopsys.integration.detector.base.DetectorEvaluation;
 import com.synopsys.integration.detector.base.DetectorEvaluationTree;
 import com.synopsys.integration.detector.base.DetectorType;
 import com.synopsys.integration.detector.rule.DetectorRule;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -19,17 +20,9 @@ public class DetectorAggregateEvaluationResultTest {
 
     @Test
     public void testSingleLevel() {
-        DetectorEvaluationTree evaluationTree = Mockito.mock(DetectorEvaluationTree.class);
-        List<DetectorEvaluation> evaluations = new ArrayList<>();
 
-        DetectorEvaluation topLevelEvaluation = Mockito.mock(DetectorEvaluation.class);
-        Mockito.when(topLevelEvaluation.isApplicable()).thenReturn(true);
-        DetectorRule rule = Mockito.mock(DetectorRule.class);
         DetectorType detectorType = DetectorType.GRADLE;
-        Mockito.when(rule.getDetectorType()).thenReturn(detectorType);
-        Mockito.when(topLevelEvaluation.getDetectorRule()).thenReturn(rule);
-        evaluations.add(topLevelEvaluation);
-        Mockito.when(evaluationTree.getOrderedEvaluations()).thenReturn(evaluations);
+        DetectorEvaluationTree evaluationTree = generateDetectorEvaluationTreeMock(detectorType);
 
         DetectorAggregateEvaluationResult result = new DetectorAggregateEvaluationResult(evaluationTree);
 
@@ -39,34 +32,15 @@ public class DetectorAggregateEvaluationResultTest {
 
     @Test
     public void testMultiLevel() {
-        DetectorEvaluationTree topLevelEvaluationTree = Mockito.mock(DetectorEvaluationTree.class);
-        DetectorEvaluationTree secondLevelEvaluationTree = Mockito.mock(DetectorEvaluationTree.class);
 
-        List<DetectorEvaluation> topLevelEvaluations = new ArrayList<>();
-        List<DetectorEvaluation> secondLevelEvaluations = new ArrayList<>();
-
-        DetectorEvaluation topLevelEvaluation = Mockito.mock(DetectorEvaluation.class);
-        Mockito.when(topLevelEvaluation.isApplicable()).thenReturn(true);
-        DetectorRule topLevelRule = Mockito.mock(DetectorRule.class);
         DetectorType topLevelDetectorType = DetectorType.GRADLE;
-        Mockito.when(topLevelRule.getDetectorType()).thenReturn(topLevelDetectorType);
-        Mockito.when(topLevelEvaluation.getDetectorRule()).thenReturn(topLevelRule);
-        topLevelEvaluations.add(topLevelEvaluation);
-
-        DetectorEvaluation secondLevelEvaluation = Mockito.mock(DetectorEvaluation.class);
-        Mockito.when(secondLevelEvaluation.isApplicable()).thenReturn(true);
-        DetectorRule secondLevelRule = Mockito.mock(DetectorRule.class);
         DetectorType secondLevelDetectorType = DetectorType.MAVEN;
-        Mockito.when(secondLevelRule.getDetectorType()).thenReturn(secondLevelDetectorType);
-        Mockito.when(secondLevelEvaluation.getDetectorRule()).thenReturn(secondLevelRule);
-        secondLevelEvaluations.add(secondLevelEvaluation);
 
+        DetectorEvaluationTree topLevelEvaluationTree = generateDetectorEvaluationTreeMock(topLevelDetectorType);
+        DetectorEvaluationTree secondLevelEvaluationTree = generateDetectorEvaluationTreeMock(secondLevelDetectorType);
         Set<DetectorEvaluationTree> secondLevelEvaluationTrees = new HashSet<>();
         secondLevelEvaluationTrees.add(secondLevelEvaluationTree);
-
-        Mockito.when(topLevelEvaluationTree.getOrderedEvaluations()).thenReturn(topLevelEvaluations);
         Mockito.when(topLevelEvaluationTree.getChildren()).thenReturn(secondLevelEvaluationTrees);
-        Mockito.when(secondLevelEvaluationTree.getOrderedEvaluations()).thenReturn(secondLevelEvaluations);
 
         DetectorAggregateEvaluationResult result = new DetectorAggregateEvaluationResult(topLevelEvaluationTree);
 
@@ -74,5 +48,19 @@ public class DetectorAggregateEvaluationResultTest {
         assertEquals(2, result.getApplicableDetectorTypesRecursively().size());
         assertTrue(result.getApplicableDetectorTypesRecursively().contains(topLevelDetectorType));
         assertTrue(result.getApplicableDetectorTypesRecursively().contains(secondLevelDetectorType));
+    }
+
+    @NotNull
+    private DetectorEvaluationTree generateDetectorEvaluationTreeMock(DetectorType detectorType) {
+        DetectorEvaluationTree evaluationTree = Mockito.mock(DetectorEvaluationTree.class);
+        List<DetectorEvaluation> evaluations = new ArrayList<>();
+        DetectorEvaluation topLevelEvaluation = Mockito.mock(DetectorEvaluation.class);
+        Mockito.when(topLevelEvaluation.isApplicable()).thenReturn(true);
+        DetectorRule rule = Mockito.mock(DetectorRule.class);
+        Mockito.when(rule.getDetectorType()).thenReturn(detectorType);
+        Mockito.when(topLevelEvaluation.getDetectorRule()).thenReturn(rule);
+        evaluations.add(topLevelEvaluation);
+        Mockito.when(evaluationTree.getOrderedEvaluations()).thenReturn(evaluations);
+        return evaluationTree;
     }
 }
