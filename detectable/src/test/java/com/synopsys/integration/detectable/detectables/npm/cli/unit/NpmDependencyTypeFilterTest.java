719894be4de725d60ab46f7f44778242658eae7d
Jake Mathews <jake.mathews.email@gmail.com>
2021-06-21 17:24:29 -0400

    test-refactor(npm-cli): Removed parameterized tests.

--- detectable/src/test/java/com/synopsys/integration/detectable/detectables/npm/cli/unit/NpmDependencyTypeFilterTest.java
+++ detectable/src/test/java/com/synopsys/integration/detectable/detectables/npm/cli/unit/NpmDependencyTypeFilterTest.java
@@ -12,117 +12,63 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Collections;
 import java.util.Set;
-import java.util.stream.Stream;
 
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 
 import com.synopsys.integration.detectable.detectables.npm.cli.parse.NpmDependencyTypeFilter;
-import com.synopsys.integration.util.Stringable;
 
 class NpmDependencyTypeFilterTest {
+    private final static String NEITHER = "test";
+    private final static String DEV = "test-dev";
+    private final static String PEER = "test-dev";
 
-    @ParameterizedTest()
-    @MethodSource("shouldIncludeSource")
-    void shouldIncludeTest(TestParameter testParameter) {
-        boolean shouldInclude = testWithParameter(testParameter);
-        assertTrue(shouldInclude, "Test Parameters: " + testParameter);
+    @Test
+    void includeNeitherTest() {
+        NpmDependencyTypeFilter filter = createFilter(false, false);
+        assertTrue(filter.shouldInclude(NEITHER, true));
+        assertTrue(filter.shouldInclude(NEITHER, false));
+        assertTrue(filter.shouldInclude(DEV, false));
+        assertFalse(filter.shouldInclude(DEV, true));
+        assertTrue(filter.shouldInclude(PEER, false));
+        assertFalse(filter.shouldInclude(PEER, true));
     }
 
-    @ParameterizedTest()
-    @MethodSource("shouldNotIncludeSource")
-    void shouldNotIncludeTest(TestParameter testParameter) {
-        boolean shouldInclude = testWithParameter(testParameter);
-        assertFalse(shouldInclude, "Test Parameters: " + testParameter);
+    @Test
+    void includeDevTest() {
+        NpmDependencyTypeFilter filter = createFilter(true, false);
+        assertTrue(filter.shouldInclude(NEITHER, true));
+        assertTrue(filter.shouldInclude(NEITHER, false));
+        assertTrue(filter.shouldInclude(DEV, true));
+        assertTrue(filter.shouldInclude(DEV, false));
+        assertTrue(filter.shouldInclude(PEER, false));
+
     }
 
-    boolean testWithParameter(TestParameter testParameter) {
+    @Test
+    void includePeerTest() {
+        NpmDependencyTypeFilter filter = createFilter(false, true);
+        assertTrue(filter.shouldInclude(NEITHER, true));
+        assertTrue(filter.shouldInclude(NEITHER, false));
+        assertTrue(filter.shouldInclude(DEV, false));
+        assertTrue(filter.shouldInclude(PEER, true));
+        assertTrue(filter.shouldInclude(PEER, false));
+    }
+
+    @Test
+    void includeBothTest() {
+        NpmDependencyTypeFilter filter = createFilter(true, true);
+        assertTrue(filter.shouldInclude(NEITHER, true));
+        assertTrue(filter.shouldInclude(NEITHER, false));
+        assertTrue(filter.shouldInclude(DEV, true));
+        assertTrue(filter.shouldInclude(DEV, false));
+        assertTrue(filter.shouldInclude(PEER, true));
+        assertTrue(filter.shouldInclude(PEER, false));
+    }
+
+    private NpmDependencyTypeFilter createFilter(boolean includeDevDependencies, boolean includePeerDependencies) {
         Set<String> devDependencies = Collections.singleton("test-dev");
         Set<String> peerDependencies = Collections.singleton("test-peer");
-
-        NpmDependencyTypeFilter npmDependencyTypeFilter = new NpmDependencyTypeFilter(devDependencies, peerDependencies, testParameter.includeDevDependencies, testParameter.includePeerDependencies);
-        String dependencyName = "test";
-        if (testParameter.isDevDependency) {
-            dependencyName = "test-dev";
-        } else if (testParameter.isPeerDependency) {
-            dependencyName = "test-peer";
-        }
-
-        return npmDependencyTypeFilter.shouldInclude(dependencyName, testParameter.isRootDependency);
+        return new NpmDependencyTypeFilter(devDependencies, peerDependencies, includeDevDependencies, includePeerDependencies);
     }
 
-    static Stream<TestParameter> shouldIncludeSource() {
-        return Stream.<TestParameter>builder()
-                   // Include Dev Dependencies
-                   .add(TestParameter.neither(true, false, true))
-                   .add(TestParameter.neither(true, false, false))
-                   .add(TestParameter.dev(true, false, true))
-                   .add(TestParameter.dev(true, false, false))
-                   .add(TestParameter.peer(true, false, false))
-
-                   // Include Peer Dependencies
-                   .add(TestParameter.neither(false, true, true))
-                   .add(TestParameter.neither(false, true, false))
-                   .add(TestParameter.dev(false, true, true))
-                   .add(TestParameter.dev(false, true, false))
-                   .add(TestParameter.peer(false, true, true))
-                   .add(TestParameter.peer(false, true, false))
-
-                   // Exclude both dependency types
-                   .add(TestParameter.neither(false, false, false))
-                   .add(TestParameter.neither(false, false, true))
-                   .add(TestParameter.dev(false, false, false))
-                   .add(TestParameter.peer(false, false, false))
-
-                   // Include both dependency types
-                   .add(TestParameter.dev(true, true, true))
-                   .add(TestParameter.dev(true, true, false))
-                   .add(TestParameter.peer(true, true, true))
-                   .add(TestParameter.peer(true, true, false))
-                   .add(TestParameter.neither(true, true, true))
-                   .add(TestParameter.neither(true, true, false))
-
-                   .build();
-    }
-
-    static Stream<TestParameter> shouldNotIncludeSource() {
-        return Stream.<TestParameter>builder()
-                   // Dev Dependencies
-                   .add(TestParameter.dev(false, false, true))
-                   .add(TestParameter.dev(false, true, true))
-
-                   // Peer Dependencies
-                   .add(TestParameter.peer(false, false, true))
-                   .add(TestParameter.peer(true, false, true))
-
-                   .build();
-    }
-
-    private static class TestParameter extends Stringable {
-        public final boolean includeDevDependencies;
-        public final boolean includePeerDependencies;
-        public final boolean isDevDependency;
-        public final boolean isPeerDependency;
-        public final boolean isRootDependency;
-
-        public static TestParameter neither(boolean includeDevDependencies, boolean includePeerDependencies, boolean isRootDependency) {
-            return new TestParameter(includeDevDependencies, includePeerDependencies, false, false, isRootDependency);
-        }
-
-        public static TestParameter dev(boolean includeDevDependencies, boolean includePeerDependencies, boolean isRootDependency) {
-            return new TestParameter(includeDevDependencies, includePeerDependencies, true, false, isRootDependency);
-        }
-
-        public static TestParameter peer(boolean includeDevDependencies, boolean includePeerDependencies, boolean isRootDependency) {
-            return new TestParameter(includeDevDependencies, includePeerDependencies, false, true, isRootDependency);
-        }
-
-        private TestParameter(boolean includeDevDependencies, boolean includePeerDependencies, boolean isDevDependency, boolean isPeerDependency, boolean isRootDependency) {
-            this.includeDevDependencies = includeDevDependencies;
-            this.includePeerDependencies = includePeerDependencies;
-            this.isDevDependency = isDevDependency;
-            this.isPeerDependency = isPeerDependency;
-            this.isRootDependency = isRootDependency;
-        }
-    }
 }
