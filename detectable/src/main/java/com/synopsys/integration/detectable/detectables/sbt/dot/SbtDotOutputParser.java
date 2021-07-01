77c34a04249e249e5ab58350ed18ee7889c11648
Jordan Piscitelli <jordanp@synopsys.com>
2021-07-01 12:29:02 -0400

    fix: Added warning when sbt parser fails to find any graphs.

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/sbt/dot/SbtDotOutputParser.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/sbt/dot/SbtDotOutputParser.java
@@ -13,18 +13,25 @@ import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SbtDotOutputParser {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     public List<File> parseGeneratedGraphFiles(List<String> dotOutput) {
         List<File> graphs = new ArrayList<>();
 
         for (String line : dotOutput) {
             String potentialFile = parseDotGraphFromLine(line);
             if (potentialFile != null) {
+                logger.debug("Found graph: " + potentialFile);
                 graphs.add(new File(potentialFile));
             }
         }
-
+        if (graphs.size() == 0) {
+            logger.warn("Sbt found no graphs! This may be an issue with your project.");
+        }
         return graphs;
     }
 
