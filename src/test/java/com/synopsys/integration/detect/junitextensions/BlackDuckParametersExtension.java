81b589743c7d1838eb930ec57ce29fe3be37e85e
Eric Kerwin <ekerwin@synopsys.com>
2021-07-16 14:23:42 -0400

    clean up

--- src/test/java/com/synopsys/integration/detect/junitextensions/BlackDuckParametersExtension.java
+++ src/test/java/com/synopsys/integration/detect/junitextensions/BlackDuckParametersExtension.java
@@ -1,18 +1,9 @@
 package com.synopsys.integration.detect.junitextensions;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-import java.lang.reflect.Field;
-import java.util.Optional;
-import java.util.function.Function;
-
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
-import org.springframework.util.ReflectionUtils;
 
 import com.synopsys.integration.blackduck.configuration.BlackDuckServerConfig;
 import com.synopsys.integration.blackduck.configuration.BlackDuckServerConfigBuilder;
@@ -24,7 +15,7 @@ import com.synopsys.integration.log.SilentIntLogger;
 public class BlackDuckParametersExtension implements ParameterResolver {
     @Override
     public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
-        return parameterContext.isAnnotated(BlackDuck.class);
+        return parameterContext.isAnnotated(BlackDuck.class) && parameterContext.getParameter().getType().isAssignableFrom(BlackDuckServicesFactory.class);
     }
 
     @Override
@@ -42,9 +33,8 @@ public class BlackDuckParametersExtension implements ParameterResolver {
             throw new ParameterResolutionException(String.format("Unable to connect to %s.", blackDuckServerConfig.getBlackDuckUrl().string()));
         }
 
-        Object testInstance = extensionContext.getTestInstance().get();
-        Function<IntLogger, BlackDuckServicesFactory> createFactory = blackDuckServerConfig::createBlackDuckServicesFactory;
-        return FieldSupport.useField(testInstance, "blackDuckLogger", createFactory, new SilentIntLogger());
+        IntLogger logger = FieldSupport.getValueOrDefault(extensionContext, "blackDuckLogger", new SilentIntLogger());
+        return blackDuckServerConfig.createBlackDuckServicesFactory(logger);
     }
 
 }
--- src/test/java/com/synopsys/integration/detect/junitextensions/FieldSupport.java
+++ src/test/java/com/synopsys/integration/detect/junitextensions/FieldSupport.java
@@ -2,7 +2,8 @@ package com.synopsys.integration.detect.junitextensions;
 
 import java.lang.reflect.Field;
 import java.util.Optional;
-import java.util.function.Function;
+
+import org.junit.jupiter.api.extension.ExtensionContext;
 
 public class FieldSupport {
     public static Optional<Field> find(Class<?> clazz, String fieldName) {
@@ -14,7 +15,7 @@ public class FieldSupport {
         return Optional.empty();
     }
 
-    public static Optional<Object> getFieldValue(Object o, String fieldName) {
+    public static Optional<Object> getValue(Object o, String fieldName) {
         Optional<Field> optionalField = find(o.getClass(), fieldName);
         if (optionalField.isPresent()) {
             Field field = optionalField.get();
@@ -28,11 +29,12 @@ public class FieldSupport {
         return Optional.empty();
     }
 
-    public static <R, F> R useField(Object o, String fieldName, Function<F, R> fieldFunction, F defaultValue) {
-            return getFieldValue(o, fieldName)
-                .map(fieldValue -> (F)fieldValue)
-                .map(fieldFunction)
-                .orElse(fieldFunction.apply(defaultValue));
+    public static <T> T getValueOrDefault(ExtensionContext extensionContext, String fieldName, T defaultValue) {
+        return extensionContext
+                   .getTestInstance()
+                   .flatMap(obj -> getValue(obj, fieldName))
+                   .map(fieldValue -> (T) fieldValue)
+                   .orElse(defaultValue);
     }
 
 }
