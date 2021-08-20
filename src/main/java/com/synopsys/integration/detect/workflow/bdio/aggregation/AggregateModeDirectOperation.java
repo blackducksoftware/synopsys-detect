949d621d37448cd4afe1ca7bb1a4df9609ee0106
Eric Kerwin <ekerwin@synopsys.com>
2021-08-20 12:47:24 -0400

    dropping the requirement on TriFunction

--- src/main/java/com/synopsys/integration/detect/workflow/bdio/aggregation/AggregateModeDirectOperation.java
+++ src/main/java/com/synopsys/integration/detect/workflow/bdio/aggregation/AggregateModeDirectOperation.java
@@ -7,11 +7,7 @@
  */
 package com.synopsys.integration.detect.workflow.bdio.aggregation;
 
-import java.io.File;
 import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.synopsys.integration.bdio.SimpleBdioFactory;
 import com.synopsys.integration.bdio.graph.DependencyGraph;
@@ -20,15 +16,13 @@ import com.synopsys.integration.detect.configuration.DetectUserFriendlyException
 import com.synopsys.integration.detect.workflow.codelocation.DetectCodeLocation;
 
 public class AggregateModeDirectOperation {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
     private final SimpleBdioFactory simpleBdioFactory;
 
     public AggregateModeDirectOperation(final SimpleBdioFactory simpleBdioFactory) {
         this.simpleBdioFactory = simpleBdioFactory;
     }
 
-    public DependencyGraph aggregateCodeLocations(final File sourcePath, final List<DetectCodeLocation> codeLocations) throws DetectUserFriendlyException {
+    public DependencyGraph aggregateCodeLocations(final List<DetectCodeLocation> codeLocations) throws DetectUserFriendlyException {
         final MutableDependencyGraph aggregateDependencyGraph = simpleBdioFactory.createMutableDependencyGraph();
 
         for (final DetectCodeLocation detectCodeLocation : codeLocations) {
@@ -37,4 +31,5 @@ public class AggregateModeDirectOperation {
 
         return aggregateDependencyGraph;
     }
+
 }
