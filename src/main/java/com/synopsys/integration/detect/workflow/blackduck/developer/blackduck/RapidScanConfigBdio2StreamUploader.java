08dc1c4f062c61968bcf660d0f100d3cb3d31cf8
Used to build SIG applications <serv-builder@int-ubuntu-jenkins02.dc1.lan>
2021-10-18 07:40:24 -0400

    Using the next snapshot post release 7.7.0-SIGQA3-SNAPSHOT

--- src/main/java/com/synopsys/integration/detect/workflow/blackduck/developer/blackduck/RapidScanConfigBdio2StreamUploader.java
+++ src/main/java/com/synopsys/integration/detect/workflow/blackduck/developer/blackduck/RapidScanConfigBdio2StreamUploader.java
@@ -1,3 +1,10 @@
+/*
+ * synopsys-detect
+ *
+ * Copyright (c) 2021 Synopsys, Inc.
+ *
+ * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
+ */
 package com.synopsys.integration.detect.workflow.blackduck.developer.blackduck;
 
 /*
