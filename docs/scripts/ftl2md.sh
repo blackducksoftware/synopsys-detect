38d02ba79c495640d1b8bd69dfe9c9095065ac87
Steve Billings <billings@synopsys.com>
2021-11-01 11:15:09 -0400

    chore: in progress script to convert .ftl to .md

---
+++ docs/scripts/ftl2md.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+templatesDir="docs/templates/content"
+markdownDir="docs/markdown"
+if [ -d "$templatesDir" ]
+then
+  echo "$templatesDir is a directory."
+else
+  echo "$templatesDir is not a directory. You need to run from the root of the Detect project (e.g. src/synopsys-detect)"
+  exit 1
+fi
+if [ -d "$markdownDir" ]
+then
+  echo "$markdownDir already exists."
+else
+  mkdir $markdownDir
+fi
+
+convertOneScript=/tmp/convertOneDocFile.sh
+echo "#!/bin/bash" > ${convertOneScript}
+echo "# Arg: path of .ftl file relative to docs/templates/content" >> ${convertOneScript}
+echo "f=\$1" >> ${convertOneScript}
+echo "basename=\"\${f%.*}\"" >> ${convertOneScript}
+echo "echo \">>> \$f -> ../../markdown/\${basename}.md\"" >> ${convertOneScript}
+echo "sed 's/\\\${\\([a-z_]*\\)}/[\\1]/g'  \${basename}.ftl > ../../markdown/\${basename}.md" >> ${convertOneScript}
+echo "echo \"CONCERNS:\"" >> ${convertOneScript}
+echo "grep '\\[\\[' ../../markdown/\${basename}.md" >> ${convertOneScript}
+
+chmod +x ${convertOneScript}
+#echo "SCRIPT:"
+#cat ${convertOneScript}
+#echo "==="
+
+cd $templatesDir
+pwd
+find . -name "*.ftl" -exec $convertOneScript {} \;
+
