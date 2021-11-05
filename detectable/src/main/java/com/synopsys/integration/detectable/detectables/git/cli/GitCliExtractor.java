4a9433ae4af575c07fc594c0a38be630697e64a1
Steve Billings <billings@synopsys.com>
2021-11-05 11:57:50 -0400

    refactor: removed unused import

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/git/cli/GitCliExtractor.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/git/cli/GitCliExtractor.java
@@ -14,7 +14,6 @@ import java.util.Optional;
 
 import com.synopsys.integration.detectable.util.ToolVersionLogger;
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.synopsys.integration.detectable.ExecutableTarget;
