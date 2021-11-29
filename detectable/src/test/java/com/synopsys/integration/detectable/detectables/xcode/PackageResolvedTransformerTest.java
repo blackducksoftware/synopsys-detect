249c392ab7c2ea0e3bad5808dfcaa431d2a69e3b
Jake Mathews <jake.mathews.email@gmail.com>
2021-11-29 15:00:23 -0500

    test(xcode): Added test cases.

--- detectable/src/test/java/com/synopsys/integration/detectable/detectables/xcode/PackageResolvedTransformerTest.java
+++ detectable/src/test/java/com/synopsys/integration/detectable/detectables/xcode/PackageResolvedTransformerTest.java
@@ -18,31 +18,26 @@ import com.synopsys.integration.detectable.detectables.xcode.model.ResolvedPacka
 import com.synopsys.integration.detectable.util.graph.GraphAssert;
 
 class PackageResolvedTransformerTest {
-    ExternalIdFactory externalIdFactory = new ExternalIdFactory();
-    ExternalId swiftCollections = createId(externalIdFactory, "apple/swift-collections", "1.0.1");
-    ExternalId auth0 = createId(externalIdFactory, "auth0/Auth0.swift", "1.35.0");
-    ExternalId rSwiftLibrary = createId(externalIdFactory, "mac-cain13/R.swift.Library", "5.4.0");
-    ExternalId swiftLog = createId(externalIdFactory, "apple/swift-log", "1.4.2");
+    private final ExternalIdFactory externalIdFactory = new ExternalIdFactory();
+    private final ExternalId swiftCollections = createId(externalIdFactory, "apple/swift-collections", "1.0.1");
+    private final ExternalId auth0 = createId(externalIdFactory, "auth0/Auth0.swift", "1.35.0");
+    private final ExternalId rSwiftLibrary = createId(externalIdFactory, "mac-cain13/R.swift.Library", "5.4.0");
+    private final ExternalId swiftLog = createId(externalIdFactory, "apple/swift-log", "1.4.2");
+    private final String defaultFileFormatVersion = PackageResolvedTransformer.KNOWN_FILE_FORMAT_VERSIONS[0];
 
     @Test
     void simpleTest() {
         PackageResolvedTransformer transformer = new PackageResolvedTransformer(externalIdFactory);
-        PackageResolved packageResolved = createPackageResolved(null);
+        PackageResolved packageResolved = createPackageResolved(null, defaultFileFormatVersion);
 
         DependencyGraph dependencyGraph = transformer.transform(packageResolved);
-        GraphAssert graphAssert = new GraphAssert(Forge.GITHUB, dependencyGraph);
-
-        graphAssert.hasRootDependency(swiftCollections);
-        graphAssert.hasRootDependency(auth0);
-        graphAssert.hasRootDependency(rSwiftLibrary);
-        graphAssert.hasRootDependency(swiftLog);
-        graphAssert.hasRootSize(4);
+        assertDefaultPackagesExist(dependencyGraph);
     }
 
     @Test
     void malformedUrlTest() {
         PackageResolvedTransformer transformer = new PackageResolvedTransformer(externalIdFactory);
-        PackageResolved packageResolved = createPackageResolved("some non-url gibberish");
+        PackageResolved packageResolved = createPackageResolved("some non-url gibberish", defaultFileFormatVersion);
 
         DependencyGraph dependencyGraph = transformer.transform(packageResolved);
         GraphAssert graphAssert = new GraphAssert(Forge.GITHUB, dependencyGraph);
@@ -57,12 +52,35 @@ class PackageResolvedTransformerTest {
         graphAssert.hasRootSize(1);
     }
 
+    @Test
+    void fileFormatVersionTest() {
+        PackageResolvedTransformer transformer = new PackageResolvedTransformer(externalIdFactory);
+
+        PackageResolved knownVersionFile = createPackageResolved(null, defaultFileFormatVersion);
+        DependencyGraph knownDependencyGraph = transformer.transform(knownVersionFile);
+        assertDefaultPackagesExist(knownDependencyGraph);
+
+        // Testing that parsing continues despite unknown version. Also helps with coverage.
+        PackageResolved unknownVersionFile = createPackageResolved(null, "11/2021-jm");
+        DependencyGraph unknownDependencyGraph = transformer.transform(unknownVersionFile);
+        assertDefaultPackagesExist(unknownDependencyGraph);
+    }
+
+    private void assertDefaultPackagesExist(DependencyGraph dependencyGraph) {
+        GraphAssert graphAssert = new GraphAssert(Forge.GITHUB, dependencyGraph);
+        graphAssert.hasRootDependency(swiftCollections);
+        graphAssert.hasRootDependency(auth0);
+        graphAssert.hasRootDependency(rSwiftLibrary);
+        graphAssert.hasRootDependency(swiftLog);
+        graphAssert.hasRootSize(4);
+    }
+
     private ExternalId createId(ExternalIdFactory externalIdFactory, String name, String version) {
         return externalIdFactory.createNameVersionExternalId(Forge.GITHUB, name, version);
     }
 
     // The urlPrefix is so the url can be quickly malformed by the tests
-    private PackageResolved createPackageResolved(@Nullable String urlPrefix) {
+    private PackageResolved createPackageResolved(@Nullable String urlPrefix, String fileFormatVersion) {
         urlPrefix = StringUtils.trimToEmpty(urlPrefix);
         ResolvedPackage swiftCollections = new ResolvedPackage(
             "swift-collections",
@@ -87,6 +105,6 @@ class PackageResolvedTransformerTest {
 
         List<ResolvedPackage> resolvedPackages = Arrays.asList(swiftCollections, auth0, rSwiftLibrary, swiftLog);
         ResolvedObject resolvedObject = new ResolvedObject(resolvedPackages);
-        return new PackageResolved(resolvedObject, "1");
+        return new PackageResolved(resolvedObject, fileFormatVersion);
     }
 }
