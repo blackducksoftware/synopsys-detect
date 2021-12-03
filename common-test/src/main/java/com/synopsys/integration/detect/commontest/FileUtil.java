7266adb72643293a7e52ee5c384bfcda8eeb801d
Jake Mathews <jake.mathews.email@gmail.com>
2021-12-03 14:25:47 -0500

    refactor(common-test): Removed license headers and applied formatter to all files in the /common-test module.

--- common-test/src/main/java/com/synopsys/integration/detect/commontest/FileUtil.java
+++ common-test/src/main/java/com/synopsys/integration/detect/commontest/FileUtil.java
@@ -1,10 +1,3 @@
-/*
- * common-test
- *
- * Copyright (c) 2021 Synopsys, Inc.
- *
- * Use subject to the terms and conditions of the Synopsys End User Software License and Maintenance Agreement. All rights reserved worldwide.
- */
 package com.synopsys.integration.detect.commontest;
 
 import java.io.File;
@@ -14,10 +7,10 @@ import java.net.URL;
 import org.junit.jupiter.api.Assertions;
 
 public class FileUtil {
-    public static File asFile(Class<?> resourceLoadingClass, final String relativeResourcePath, final String prefix) {
-        final URL resource = resourceLoadingClass.getResource(prefix + relativeResourcePath);
+    public static File asFile(Class<?> resourceLoadingClass, String relativeResourcePath, String prefix) {
+        URL resource = resourceLoadingClass.getResource(prefix + relativeResourcePath);
         Assertions.assertNotNull(resource, "Could not find resource path: " + prefix + relativeResourcePath);
-        final File file;
+        File file;
         try {
             file = new File(resource.toURI().getPath());
         } catch (URISyntaxException e) {
