ba558807931cc64271f015fd9cf7568fd6ec7db1
Steve Billings <billings@synopsys.com>
2022-01-05 17:51:45 -0500

    refactor: cleanup

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/bitbake/parse/BitbakeGraphTransformer.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/bitbake/parse/BitbakeGraphTransformer.java
@@ -38,7 +38,7 @@ public class BitbakeGraphTransformer {
             if (bitbakeNode.getVersion().isPresent()) {
                 String version = bitbakeNode.getVersion().get();
                 if (qualifies(includeDevDependencies, imageRecipes, name, version)) {
-                    Optional<Dependency> dependency = generateExternalId(name, version, recipeLayerMap).map(Dependency::new);
+                    Optional<Dependency> dependency = generateExternalId(name, version, recipeLayerMap, includeDevDependencies).map(Dependency::new);
                     dependency.ifPresent(value -> namesToExternalIds.put(bitbakeNode.getName(), value));
             }
             } else if (name.startsWith("virtual/")) {
@@ -71,28 +71,38 @@ public class BitbakeGraphTransformer {
         if (includeDevDependencies) {
             return true;
         }
-        if (imageRecipes.containsKey(recipeName)) {
-            // TODO refactor
-            String imageRecipeVersion = imageRecipes.get(recipeName);
-            String epochlessRecipeVersion = recipeVersion;
-            if (recipeVersion.matches("^[0-9]+:.*")) {
-                logger.info(String.format("%s has an epoch", recipeVersion));
-                int colonPos = recipeVersion.indexOf(':');
-                epochlessRecipeVersion = recipeVersion.substring(colonPos+1);
-                logger.info(String.format("epochlessVersion: %s", epochlessRecipeVersion));
-            }
-            if (epochlessRecipeVersion.startsWith(imageRecipeVersion)) {
-                return true;
-            } else {
-                // TODO I think this will exclude legit recipes due to version string simplification in license.manifest
-                logger.info(String.format("*** Version %s of recipe %s is in the image, but not this version (%s); excluding it.", imageRecipeVersion, recipeName, recipeVersion));
-            }
+        if (foundInImageRecipes(imageRecipes, recipeName, recipeVersion)) {
+            return true;
         }
         logger.debug(String.format("Excluding dev dependency %s/%s", recipeName, recipeName));
         return false;
     }
 
-    private Optional<ExternalId> generateExternalId(String dependencyName, String dependencyVersion, Map<String, String> recipeLayerMap) {
+    private boolean foundInImageRecipes(Map<String, String> imageRecipes, String recipeName, String recipeVersion) {
+        if (imageRecipes.containsKey(recipeName)) {
+            String imageRecipeVersion = imageRecipes.get(recipeName);
+            String epochlessRecipeVersion = removeEpochPrefix(recipeVersion);
+            if (epochlessRecipeVersion.startsWith(imageRecipeVersion)) {
+                return true;
+            } else {
+                logger.debug(String.format("Recipe %s/%s is included in the image, but version %s is not.", recipeName, imageRecipeVersion, recipeVersion));
+            }
+        }
+        return false;
+    }
+
+    private String removeEpochPrefix(final String recipeVersion) {
+        String epochlessRecipeVersion = recipeVersion;
+        if (recipeVersion.matches("^[0-9]+:.*")) {
+            logger.info(String.format("%s has an epoch", recipeVersion));
+            int colonPos = recipeVersion.indexOf(':');
+            epochlessRecipeVersion = recipeVersion.substring(colonPos+1);
+            logger.info(String.format("epochlessVersion: %s", epochlessRecipeVersion));
+        }
+        return epochlessRecipeVersion;
+    }
+
+    private Optional<ExternalId> generateExternalId(String dependencyName, String dependencyVersion, Map<String, String> recipeLayerMap, boolean includeDevDependencies) {
         String priorityLayerName = recipeLayerMap.get(dependencyName);
         ExternalId externalId = null;
 
@@ -102,9 +112,13 @@ public class BitbakeGraphTransformer {
             logger.debug(String.format("Failed to find component '%s' in component layer map.", dependencyName));
 
             if (dependencyName.endsWith(NATIVE_SUFFIX)) {
-                String alternativeName = dependencyName.replace(NATIVE_SUFFIX, "");
-                logger.debug(String.format("Generating alternative component name '%s' for '%s==%s'", alternativeName, dependencyName, dependencyVersion));
-                externalId = generateExternalId(alternativeName, dependencyVersion, recipeLayerMap).orElse(null);
+                if (includeDevDependencies) {
+                    String alternativeName = dependencyName.replace(NATIVE_SUFFIX, "");
+                    logger.debug(String.format("Generating alternative component name '%s' for '%s==%s'", alternativeName, dependencyName, dependencyVersion));
+                    externalId = generateExternalId(alternativeName, dependencyVersion, recipeLayerMap, includeDevDependencies).orElse(null);
+                } else {
+                    logger.debug(String.format("Recipe %s excluded because dev dependencies are being excluded"));
+                }
             } else {
                 logger.debug(String.format("'%s==%s' is not an actual component. Excluding from graph.", dependencyName, dependencyVersion));
             }
