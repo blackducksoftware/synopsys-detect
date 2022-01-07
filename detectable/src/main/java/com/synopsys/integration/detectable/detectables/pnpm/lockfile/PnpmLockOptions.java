d0b852e7f792fbc08841bc72f660f3961c5441ec
Jake Mathews <jake.mathews.email@gmail.com>
2022-01-07 11:24:22 -0500

    refactor(pnpm): Removed completed TODO.

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/pnpm/lockfile/PnpmLockOptions.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/pnpm/lockfile/PnpmLockOptions.java
@@ -4,7 +4,7 @@ import com.synopsys.integration.detectable.detectable.util.DependencyTypeFilter;
 import com.synopsys.integration.detectable.detectables.pnpm.lockfile.model.PnpmDependencyType;
 
 public class PnpmLockOptions {
-    private final DependencyTypeFilter<PnpmDependencyType> dependencyTypeFilter; // TODO: DependencyType should be Pnpm specific
+    private final DependencyTypeFilter<PnpmDependencyType> dependencyTypeFilter;
 
     public PnpmLockOptions(DependencyTypeFilter<PnpmDependencyType> dependencyTypeFilter) {
         this.dependencyTypeFilter = dependencyTypeFilter;
