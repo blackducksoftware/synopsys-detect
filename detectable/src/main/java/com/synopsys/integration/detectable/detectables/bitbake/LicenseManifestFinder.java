e52358dff0f432ae41400acb76f7f5b70fee5c90
Steve Billings <billings@synopsys.com>
2022-01-07 10:55:32 -0500

    refactor: cleanup

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/bitbake/LicenseManifestFinder.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/bitbake/LicenseManifestFinder.java
@@ -23,19 +23,16 @@ public class LicenseManifestFinder {
     public LicenseManifestFinder(final FileFinder fileFinder) {
         this.fileFinder = fileFinder;
     }
-    
+
     public File find(File buildDir, String targetImageName, boolean followSymLinks, int searchDepth) throws IntegrationException {
         try {
             File licensesDir = findLicensesDir(buildDir, followSymLinks, searchDepth);
             logger.debug("Checking licenses dir {} for license.manifest for {}", licensesDir.getAbsolutePath(), targetImageName);
             List<File> licensesDirContents = Arrays.asList(licensesDir.listFiles());
-            Optional<File> latestLicenseManifestFile = findMostRecentLicenseManifestFileForTarget(targetImageName, licensesDirContents);
-            if (latestLicenseManifestFile.isPresent()) {
-                logger.trace("Found target image license.manifest file: {}", latestLicenseManifestFile.get().getAbsolutePath());
-                if (latestLicenseManifestFile.get().canRead()) {
-                    logger.debug("Found readable license.manifest file: {}", latestLicenseManifestFile.get().getAbsolutePath());
-                    return latestLicenseManifestFile.get();
-                }
+            Optional<File> latestLicenseManifestFile = findMostRecentLicenseManifestFileForTarget(targetImageName, licensesDirContents, followSymLinks);
+            if (latestLicenseManifestFile.isPresent() && latestLicenseManifestFile.get().canRead()) {
+                logger.debug("Found readable license.manifest file: {}", latestLicenseManifestFile.get().getAbsolutePath());
+                return latestLicenseManifestFile.get();
             }
         } catch (Exception e) {
             logger.debug(String.format("Error finding license.manifest file for target image %s", targetImageName), e);
@@ -66,22 +63,19 @@ public class LicenseManifestFinder {
         return deployLicensesDirs.get(0);
     }
 
-    private Optional<File> findMostRecentLicenseManifestFileForTarget(final String targetImageName, final List<File> licensesDirContents) {
+    private Optional<File> findMostRecentLicenseManifestFileForTarget(final String targetImageName, final List<File> licensesDirContents, boolean followSymLinks) {
         File latestLicenseManifestFile = null;
         long latestLicenseManifestFileTime = 0;
         for (File licensesDirSubDir : licensesDirContents) {
-            // TODO combine some if's
-            if (licensesDirSubDir.getName().startsWith(targetImageName)) {
-                if (!FileUtils.isSymlink(licensesDirSubDir)) {
-                    File thisLicenseManifestFile = new File(licensesDirSubDir, "license.manifest");
-                    if (thisLicenseManifestFile.exists()) {
-                        if ((latestLicenseManifestFileTime == 0) || (thisLicenseManifestFile.lastModified() > latestLicenseManifestFileTime)) {
-                            latestLicenseManifestFileTime = thisLicenseManifestFile.lastModified();
-                            latestLicenseManifestFile = thisLicenseManifestFile;
-                            logger.trace("Latest so far: {}", latestLicenseManifestFile.getAbsolutePath());
-                        } else {
-                            logger.trace("Not the latest: {}", thisLicenseManifestFile.getAbsolutePath());
-                        }
+            if (licensesDirSubDir.getName().startsWith(targetImageName) && (followSymLinks || !FileUtils.isSymlink(licensesDirSubDir))) {
+                File thisLicenseManifestFile = new File(licensesDirSubDir, "license.manifest");
+                if (thisLicenseManifestFile.exists()) {
+                    if ((latestLicenseManifestFileTime == 0) || (thisLicenseManifestFile.lastModified() > latestLicenseManifestFileTime)) {
+                        latestLicenseManifestFileTime = thisLicenseManifestFile.lastModified();
+                        latestLicenseManifestFile = thisLicenseManifestFile;
+                        logger.trace("Latest so far: {}", latestLicenseManifestFile.getAbsolutePath());
+                    } else {
+                        logger.trace("Not the latest: {}", thisLicenseManifestFile.getAbsolutePath());
                     }
                 }
             }
