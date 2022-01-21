d2a6ce51f845746c85efb6d0173cae77d8b0b9eb
Jake Mathews <jake.mathews.email@gmail.com>
2022-01-21 16:22:15 -0500

    refactor(composer/packagist): Refactored Composer/Packagist options to use the new EnumListFilter.

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/packagist/ComposerLockDetectableOptions.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/packagist/ComposerLockDetectableOptions.java
@@ -3,7 +3,6 @@ package com.synopsys.integration.detectable.detectables.packagist;
 import com.synopsys.integration.detectable.detectable.util.EnumListFilter;
 
 public class ComposerLockDetectableOptions {
-
     private final EnumListFilter<PackagistDependencyType> dependencyTypeFilter;
 
     public ComposerLockDetectableOptions(EnumListFilter<PackagistDependencyType> dependencyTypeFilter) {
