4881e8aa33f6082f708b23c34788adadfac67526
Jake Mathews <jake.mathews.email@gmail.com>
2022-01-27 12:26:32 -0500

    refactor(imports): Removed unused imports identified by sonar.

--- configuration/src/main/java/com/synopsys/integration/configuration/property/types/enumextended/ExtendedEnumListProperty.java
+++ configuration/src/main/java/com/synopsys/integration/configuration/property/types/enumextended/ExtendedEnumListProperty.java
@@ -1,15 +1,8 @@
 package com.synopsys.integration.configuration.property.types.enumextended;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-
-import com.synopsys.integration.configuration.parse.ListValueParser;
-import com.synopsys.integration.configuration.property.base.ValuedListProperty;
-import com.synopsys.integration.configuration.util.EnumPropertyUtils;
-import com.synopsys.integration.configuration.util.PropertyUtils;
 
 public class ExtendedEnumListProperty<E extends Enum<E>, B extends Enum<B>> extends ExtendedEnumListPropertyBase<E, B, List<ExtendedEnumValue<E, B>>> {
     public ExtendedEnumListProperty(@NotNull String key, @NotNull List<ExtendedEnumValue<E, B>> defaultValue, @NotNull Class<E> eClass, @NotNull Class<B> bClass) {
