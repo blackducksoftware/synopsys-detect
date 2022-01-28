47310bb77ef2bb542cfd9b2606276d842ccdcf23
Jake Mathews <jake.mathews.email@gmail.com>
2022-01-28 10:43:45 -0500

    refactor(cargo): Tagged Data class fields with @Nullable

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/cargo/data/CargoLock.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/cargo/data/CargoLock.java
@@ -3,13 +3,16 @@ package com.synopsys.integration.detectable.detectables.cargo.data;
 import java.util.List;
 import java.util.Optional;
 
+import org.jetbrains.annotations.Nullable;
+
 import com.google.gson.annotations.SerializedName;
 
 public class CargoLock {
+    @Nullable
     @SerializedName("package")
     private final List<CargoLockPackageData> packages;
 
-    public CargoLock(List<CargoLockPackageData> packages) {this.packages = packages;}
+    public CargoLock(@Nullable List<CargoLockPackageData> packages) {this.packages = packages;}
 
     public Optional<List<CargoLockPackageData>> getPackages() {
         return Optional.ofNullable(packages);
--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/cargo/data/CargoLockPackageData.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/cargo/data/CargoLockPackageData.java
@@ -3,25 +3,32 @@ package com.synopsys.integration.detectable.detectables.cargo.data;
 import java.util.List;
 import java.util.Optional;
 
+import org.jetbrains.annotations.Nullable;
+
 import com.google.gson.annotations.SerializedName;
 
 public class CargoLockPackageData {
+    @Nullable
     @SerializedName("name")
     private final String name;
 
+    @Nullable
     @SerializedName("version")
     private final String version;
 
+    @Nullable
     @SerializedName("source")
     private final String source;
 
+    @Nullable
     @SerializedName("checksum")
     private final String checksum;
 
+    @Nullable
     @SerializedName("dependencies")
     private final List<String> dependencies;
 
-    public CargoLockPackageData(String name, String version, String source, String checksum, List<String> dependencies) {
+    public CargoLockPackageData(@Nullable String name, @Nullable String version, @Nullable String source, @Nullable String checksum, @Nullable List<String> dependencies) {
         this.name = name;
         this.version = version;
         this.source = source;
