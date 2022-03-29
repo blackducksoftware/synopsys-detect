bbdfcac96633726aa4256cb8474b279e401158cb
Steve Billings <billings@synopsys.com>
2022-03-29 17:51:04 -0400

    refactor: tweaked formatting

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/docker/ImageIdentifierGenerator.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/docker/ImageIdentifierGenerator.java
@@ -8,8 +8,11 @@ import com.synopsys.integration.detectable.detectables.docker.model.DockerInspec
 public class ImageIdentifierGenerator {
 
     public String generate(ImageIdentifierType imageIdentifierType, String suppliedImagePiece, @Nullable DockerInspectorResults dockerResults) {
-        if (imageIdentifierType.equals(ImageIdentifierType.IMAGE_ID) && (dockerResults != null) &&
-            StringUtils.isNotBlank(dockerResults.getImageRepo()) && StringUtils.isNotBlank(dockerResults.getImageTag())) {
+        if (imageIdentifierType.equals(ImageIdentifierType.IMAGE_ID)
+            && (dockerResults != null)
+            && StringUtils.isNotBlank(dockerResults.getImageRepo())
+            && StringUtils.isNotBlank(dockerResults.getImageTag())
+        ) {
             return dockerResults.getImageRepo() + ":" + dockerResults.getImageTag();
         }
         return suppliedImagePiece;
