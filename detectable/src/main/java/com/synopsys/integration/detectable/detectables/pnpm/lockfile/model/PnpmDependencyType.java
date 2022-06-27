9a98dce390e8932ba456ecad3045de765034d5ca
crowleySynopsys <51929980+crowleySynopsys@users.noreply.github.com>
2022-06-27 16:35:26 -0400

    Update detectable/src/main/java/com/synopsys/integration/detectable/detectables/pnpm/lockfile/model/PnpmDependencyType.java
    Co-authored-by: Jake Mathews <jake.mathews.email@gmail.com>

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/pnpm/lockfile/model/PnpmDependencyType.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/pnpm/lockfile/model/PnpmDependencyType.java
@@ -1,6 +1,5 @@
 package com.synopsys.integration.detectable.detectables.pnpm.lockfile.model;
 
-// TODO: Drop 'V2' from the name in Detect 8.0.0 JM-01/2022
 public enum PnpmDependencyType {
     DEV,
     OPTIONAL
