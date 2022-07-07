71f3e1d5d84eecf78416437dbf247ad24b6924fb
Jake Mathews <jake.mathews.email@gmail.com>
2022-07-07 16:38:02 -0400

    Revert "fix(coverity): Fixed resource leak identified by Coverity."
    This reverts commit f1ca6349b393c93ea8b1506f4fe56463c8cf8397.
    
    (cherry picked from commit 56894ad7adb9ec16a87a9ee3c2fa13381f2dbf96)

--- src/main/java/com/synopsys/integration/detect/util/finder/DetectDirectoryFileFilter.java
+++ src/main/java/com/synopsys/integration/detect/util/finder/DetectDirectoryFileFilter.java
@@ -3,10 +3,9 @@ package com.synopsys.integration.detect.util.finder;
 import java.io.File;
 import java.util.List;
 import java.util.function.Predicate;
-
 import org.apache.commons.io.filefilter.WildcardFileFilter;
 
-public class DetectDirectoryFileFilter implements Predicate<File> {
+public class DetectDirectoryFileFilter implements Predicate<File>  {
     private final List<String> directoryExclusionPatterns;
     private final DirectoryMatcher directoryMatcher = new DirectoryMatcher();
     private final WildcardFileFilter wildcardFilter;
@@ -19,10 +18,7 @@ public class DetectDirectoryFileFilter implements Predicate<File> {
     @Override
     public boolean test(File file) {
         if (file.isDirectory()) {
-            return directoryMatcher.nameMatchesExcludedDirectory(directoryExclusionPatterns, file) || directoryMatcher.pathMatchesExcludedDirectory(
-                directoryExclusionPatterns,
-                file
-            );
+            return directoryMatcher.nameMatchesExludedDirectory(directoryExclusionPatterns, file) || directoryMatcher.pathMatchesExcludedDirectory(directoryExclusionPatterns, file);
         }
         if (file.isFile()) {
             return wildcardFilter.accept(file);
--- src/main/java/com/synopsys/integration/detect/util/finder/DirectoryMatcher.java
+++ src/main/java/com/synopsys/integration/detect/util/finder/DirectoryMatcher.java
@@ -1,8 +1,6 @@
 package com.synopsys.integration.detect.util.finder;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
@@ -10,7 +8,6 @@ import java.nio.file.PathMatcher;
 import java.util.List;
 
 import org.apache.commons.io.FilenameUtils;
-import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,7 +15,7 @@ public class DirectoryMatcher {
     private static final String PATH_MATCHER_SYNTAX = "glob:%s";
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public boolean nameMatchesExcludedDirectory(List<String> directoryExclusionPatterns, File file) {
+    public boolean nameMatchesExludedDirectory(List<String> directoryExclusionPatterns, File file) {
         for (String excludedDirectory : directoryExclusionPatterns) {
             if (FilenameUtils.wildcardMatchOnSystem(file.getName(), excludedDirectory)) {
                 return true;
@@ -28,32 +25,25 @@ public class DirectoryMatcher {
     }
 
     public boolean pathMatchesExcludedDirectory(List<String> directoryExclusionPatterns, File file) {
-        try (FileSystem fileSystemDefaults = FileSystems.getDefault()) {
-            for (String excludedDirectory : directoryExclusionPatterns) {
-                // On Windows, patterns starting with a * raise InvalidPathException, so perform pattern check first
-                PathMatcher pathMatcher = fileSystemDefaults.getPathMatcher(String.format(PATH_MATCHER_SYNTAX, excludedDirectory));
-                if (pathMatcher.matches(file.toPath())) {
-                    return true;
-                }
-
-                Path excludedDirectoryPath = createExcludedDirectoryPath(excludedDirectory);
-                if (excludedDirectoryPath == null || file.toPath().endsWith(excludedDirectoryPath)) {
-                    return true;
-                }
+        for (String excludedDirectory : directoryExclusionPatterns) {
+            // On Windows, patterns starting with a * raise InvalidPathException, so perform pattern check first
+            PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher(String.format(PATH_MATCHER_SYNTAX, excludedDirectory));
+            if (pathMatcher.matches(file.toPath())) {
+                return true;
             }
-        } catch (IOException e) {
-            logger.debug("Failure using FileSystem for Excluded Directory pattern matching.", e);
+
+            Path excludedDirectoryPath;
+            try {
+                excludedDirectoryPath = new File(excludedDirectory).toPath();
+            } catch (InvalidPathException e) {
+                logger.debug(String.format("%s could not be resolved to a path.", excludedDirectory));
+                continue;
+            }
+
+            if (file.toPath().endsWith(excludedDirectoryPath)) {
+                return true;
+            }
         }
         return false;
-    }
-
-    @Nullable
-    private Path createExcludedDirectoryPath(String excludedDirectory) {
-        try {
-            return new File(excludedDirectory).toPath();
-        } catch (InvalidPathException e) {
-            logger.debug(String.format("%s could not be resolved to a path.", excludedDirectory));
-            return null;
-        }
     }
 }
