8e67397fa683201daa3dc4aa27f1bef8780e4df7
Steve Billings <billings@synopsys.com>
2022-07-11 16:43:49 -0400

    test: added test for zip-file-exists scenario

--- src/test/java/com/synopsys/integration/detect/tool/detector/inspector/projectinspector/installer/LocalProjectInspectorInstallerTest.java
+++ src/test/java/com/synopsys/integration/detect/tool/detector/inspector/projectinspector/installer/LocalProjectInspectorInstallerTest.java
@@ -8,17 +8,31 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import org.mockito.Mockito;
 
+import com.synopsys.integration.detect.configuration.DetectInfo;
 import com.synopsys.integration.detect.tool.detector.inspector.projectinspector.ProjectInspectorExecutableLocator;
 import com.synopsys.integration.detectable.detectable.exception.DetectableException;
+import com.synopsys.integration.util.OperatingSystemType;
 
 public class LocalProjectInspectorInstallerTest {
 
     @Test
-    void testNonExistent() {
+    void testValidZipFile(@TempDir Path tempDir) throws DetectableException {
+        DetectInfo detectInfo = Mockito.mock(DetectInfo.class);
+        Mockito.when(detectInfo.getCurrentOs()).thenReturn(OperatingSystemType.LINUX);
+        ProjectInspectorExecutableLocator projectInspectorExecutableLocator = new ProjectInspectorExecutableLocator(detectInfo);
+        Path projectInspectorZipPath = Paths.get("src/test/resources/tool/detector/inspector/projectinspector/installer/project-inspector-fake.zip");
+        ProjectInspectorInstaller localProjectInspectorInstaller = new LocalProjectInspectorInstaller(projectInspectorExecutableLocator, projectInspectorZipPath);
+        File exeFile = localProjectInspectorInstaller.install(tempDir.toFile());
+        assertTrue(exeFile.exists());
+    }
+
+    @Test
+    void testNonExistentZipFile() {
         ProjectInspectorExecutableLocator projectInspectorExecutableLocator = Mockito.mock(ProjectInspectorExecutableLocator.class);
-        Path projectInspectorZipPath = Paths.get("/thisdoesnotexist");
+        Path projectInspectorZipPath = Paths.get("/thisfiledoesnotexistonanysystem");
         ProjectInspectorInstaller localProjectInspectorInstaller = new LocalProjectInspectorInstaller(projectInspectorExecutableLocator, projectInspectorZipPath);
 
         try {
