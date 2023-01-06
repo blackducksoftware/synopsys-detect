1400240ef62b91527e685fada8922a762f46015d
Madhu Gopanna <gopanna@synopsys.com>
2023-01-06 17:17:00 -0500

    Force BDIO generation for Offline execution of Detect controlled by property --blackduck.offline.mode.force.bdio=true. Fix: Corrected Detect version for the new property.

--- src/main/java/com/synopsys/integration/detect/configuration/DetectProperties.java
+++ src/main/java/com/synopsys/integration/detect/configuration/DetectProperties.java
@@ -1733,7 +1733,7 @@ public class DetectProperties {
     
     public static final BooleanProperty BLACKDUCK_OFFLINE_MODE_FORCE_BDIO =
         BooleanProperty.newBuilder("blackduck.offline.mode.force.bdio", false)
-            .setInfo("Offline Mode", DetectPropertyFromVersion.VERSION_4_2_0)
+            .setInfo("Offline Mode", DetectPropertyFromVersion.VERSION_8_5_0)
             .setHelp(
                 "This property will force Detect in offline mode to generate a BDIO even if no code locations were identified.")
             .setGroups(DetectGroup.BLACKDUCK_SERVER, DetectGroup.BLACKDUCK, DetectGroup.OFFLINE, DetectGroup.DEFAULT)
