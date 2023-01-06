346e6976e19f9342a8aa113f410e7024b363d339
sig-cpotts <36172712+sig-cpotts@users.noreply.github.com>
2023-01-06 14:50:22 -0500

    Resolve IDETECT-3609
    Added download details for IaC scanner binary (Sigma) to resolve https://jira-sig.internal.synopsys.com/browse/IDETECT-3609

--- src/main/java/com/synopsys/integration/detect/configuration/DetectProperties.java
+++ src/main/java/com/synopsys/integration/detect/configuration/DetectProperties.java
@@ -1534,7 +1534,7 @@ public class DetectProperties {
             .setInfo("IaC Scan Target Paths", DetectPropertyFromVersion.VERSION_7_14_0)
             .setHelp(
                 "A comma-separated list of paths to perform IaC scans on.",
-                "If this property is set, an IaC scan will be performed on each of the paths provided. If this property is not set, but Iac Scanning is enabled via detect.tools, the IaC scan target path is the source path (see property detect.source.path)."
+                "If this property is set, an IaC scan will be performed on each of the paths provided. If this property is not set, but IaC Scanning is enabled via detect.tools, the IaC scan target path is the source path (see property detect.source.path)."
             )
             .setGroups(DetectGroup.IAC_SCAN, DetectGroup.GLOBAL)
             .setExample("/user/source/target1,/user/source/target2")
@@ -1552,17 +1552,18 @@ public class DetectProperties {
     public static final NullablePathProperty DETECT_IAC_SCANNER_LOCAL_PATH =
         NullablePathProperty.newBuilder("detect.iac.scanner.local.path")
             .setInfo("IaC Scanner Local Path", DetectPropertyFromVersion.VERSION_7_14_0)
-            .setHelp(
-                "Use this property to specify the path to a local IaC Scanner.")
+            .setHelp("Use this property to specify the path to a local IaC Scanner.",
+					"If you are running in an Air Gap environment you may need to download the IaC Scanner(Sigma) binary from Artifactory. See the Downloading and Running Synopsys Detect page for download location."
+			)
             .setGroups(DetectGroup.IAC_SCAN, DetectGroup.GLOBAL)
             .build();
-
+			
     public static final NullablePathProperty DETECT_SOURCE_PATH =
         NullablePathProperty.newBuilder("detect.source.path")
             .setInfo("Source Path", DetectPropertyFromVersion.VERSION_3_0_0)
             .setHelp(
                 "The source path is the path to the project directory to inspect. If no value is provided, the source path defaults to the current working directory.",
-                "Detect will search the source directory for hints that indicate which package manager(s) the project uses, and will attempt to run the corresponding detector(s). "
+                "Detect will search the source directory for hints that indicate which package manager(s) the project uses, and will attempt to run the corresponding detector(s)."
                     +
                     "The source path is also the default target for signature scanning. (This can be overridden with the detect.blackduck.signature.scanner.paths property.)"
             )
