401420aec9f5df13e1c806779c92f7c115ee90e6
David Terry <dterry@synopsys.com>
2023-03-22 13:09:30 -0400

    consider flattening lock file

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/npm/lockfile/model/PackageLockPackage.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/npm/lockfile/model/PackageLockPackage.java
@@ -11,10 +11,12 @@ public class PackageLockPackage {
     @SerializedName("dependencies")
     public Map<String, String> dependencies;
     
+    // TODO version 3 no longer has a concept of nested dependencies, just prints them weirdly 
+    // node_modules/x/node_modules/y
+    public Map<String, PackageLockPackage> packages;
+    
     // These fields are not in the JSON but are used internally by detect calculations
     public Boolean dev;
 
     public Boolean peer;
-
-    public Map<String, PackageLockPackage> detectDependencies;
 }
