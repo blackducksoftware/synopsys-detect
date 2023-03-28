56556e6fc61cd9a499ff61facc837c49b75d5fd2
sig-cpotts <36172712+sig-cpotts@users.noreply.github.com>
2023-03-28 10:59:51 -0400

    Cp doc edits 8.8.0 (#765)
    * Update detectorcascade.md
    
    Fix detector table reference link.
    
    * Update directories.md
    
    * New doc links.
    
    Update documentation links to point to the new Doc Portal.
    
    * Additional documentation URL updates
    
    Updating docs to point to the new Detect and BD documentation Portal.

--- src/main/java/com/synopsys/integration/detect/configuration/DetectProperties.java
+++ src/main/java/com/synopsys/integration/detect/configuration/DetectProperties.java
@@ -467,7 +467,7 @@ public class DetectProperties {
                 )
                 .setInfo("Reduced Persistence", DetectPropertyFromVersion.VERSION_8_3_0)
                 .setHelp(
-                    "Use this value to control how unmatched files from signature scans are stored. For a full explanation, please refer to <xref href=\"https://community.synopsys.com/s/document-item?bundleId=bd-hub&topicId=ComponentDiscovery%2Fabout_reduced_persistence_signature_scanning.html&_LANG=enus\" scope=\"external\" outputclass=\"external\" format=\"html\" target=\"_blank\">about reduced persistence signature scanning.</xref>")
+                    "Use this value to control how unmatched files from signature scans are stored. For a full explanation, please refer to <xref href=\"https://sig-product-docs.synopsys.com/bundle/bd-hub/page/ComponentDiscovery/about_reduced_persistence_signature_scanning.html\" scope=\"external\" outputclass=\"external\" format=\"html\" target=\"_blank\">about reduced persistence signature scanning.</xref>")
                 .setGroups(DetectGroup.SIGNATURE_SCANNER, DetectGroup.GLOBAL)
                 .build();
 
