89a60b8f1a16f924a7f30db781feeee40631809f
gopanna <gopanna@snps-x70wnyooeahd.internal.synopsys.com>
2023-05-04 00:54:19 -0400

    Improved code to prevent NPE.

--- src/main/java/com/synopsys/integration/detect/ApplicationUpdater.java
+++ src/main/java/com/synopsys/integration/detect/ApplicationUpdater.java
@@ -150,7 +150,7 @@ public class ApplicationUpdater extends URLClassLoader {
         while (it.hasNext()) {
             String argument = it.next();
             if (argument.contains(BLACKDUCK_OFFLINE_MODE_ARG)) {
-                offlineMode = findArgument(it, argument).toLowerCase();
+                offlineMode = findArgument(it, argument);
             } else if (argument.contains(BLACKDUCK_URL_ARG)) {
                 blackduckHost = findArgument(it, argument);
             }
@@ -175,7 +175,7 @@ public class ApplicationUpdater extends URLClassLoader {
         return detectSource == null 
                 && detectLatestReleaseVersion == null
                 && detectVersionKey == null
-                && offlineMode == null || offlineMode.equals("false")
+                && offlineMode == null || offlineMode.toLowerCase().equals("false")
                 && blackduckHost != null;
     }
     
