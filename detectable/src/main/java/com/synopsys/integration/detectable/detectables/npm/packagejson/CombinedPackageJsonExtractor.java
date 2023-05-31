8d70b39e83ff726403c3ece52fe2a868b43ba42a
David Terry <dterry@synopsys.com>
2023-05-31 13:59:27 -0400

    workspace wildcards for lock detectors

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/npm/packagejson/CombinedPackageJsonExtractor.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/npm/packagejson/CombinedPackageJsonExtractor.java
@@ -3,9 +3,11 @@ package com.synopsys.integration.detectable.detectables.npm.packagejson;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -55,7 +57,13 @@ public class CombinedPackageJsonExtractor {
             
             for(String workspace : convertedWorkspaces) {
                 Path workspaceJsonPath =
-                        Path.of(projectRoot + workspace + "/package.json").normalize();
+                        Path.of(workspace + "/package.json").normalize();
+                
+                // We are looking for a package.json but they aren't always where we expect them.
+                // Don't try to read a file that doesn't exist.
+                if (!Files.exists(workspaceJsonPath)) {
+                    continue;
+                }
                 
                 String workspaceJsonString 
                     = FileUtils.readFileToString(new File(workspaceJsonPath.toString()), StandardCharsets.UTF_8);
@@ -73,26 +81,44 @@ public class CombinedPackageJsonExtractor {
         return combinedPackageJson;
     }
 
-    // TODO confirm works with ./directory/* and directory/*
+    // TODO seems to allow directory/*/* but will only read dirs that follow that pattern
+    // so c in my example and root, no a or b, only reads leaf in other words
     private List<String> convertWorkspaceWildcards(String projectRoot, List<String> workspaces) {
         List<String> convertedWorkspaces = new ArrayList<>();
         
         for (String workspace : workspaces) {
-            if (!workspace.contains("*")) {
-                convertedWorkspaces.add(workspace);
-            } else {
-                // TODO currently only handling one wildcard
-                int wildcardIndex = workspace.lastIndexOf(File.separator);
-                String mainWorkspace = workspace.substring(0, wildcardIndex + 1);
-                
-                File mainWorkspaceDirectory = new File(projectRoot + File.separator + mainWorkspace);
-                File[] childWorkspaces = mainWorkspaceDirectory.listFiles(File::isDirectory);
-                Arrays.stream(childWorkspaces)
-                    .map(File::getPath)
-                    .forEach(convertedWorkspaces::add);   
+            replaceWildcards(projectRoot + workspace, convertedWorkspaces);
+        }                
+        
+        return convertedWorkspaces;
+    }
+    
+    private String replaceWildcards(String path, List<String> convertedWorkspaces) {        
+        if (!path.contains("*")) {
+            convertedWorkspaces.add(path);
+            return path;
+        }
+        
+        int wildcardIndex = path.indexOf("*");
+        String prefix = path.substring(0, wildcardIndex);
+        String suffix = path.substring(wildcardIndex + 1);
+        
+        File[] files = new File(prefix).listFiles(File::isDirectory);
+        String newPath = "";
+        
+        if (files != null) {
+            for (File file: files) {
+                // Handle any weird relative path pieces, such as ./ before turning
+                // into an absolute path.
+                newPath = file.toPath().normalize().toFile().getAbsolutePath() + suffix;
+                if (newPath.contains("*")) {
+                    newPath = replaceWildcards(newPath, convertedWorkspaces);
+                } else {
+                    convertedWorkspaces.add(newPath);
+                }
             }
         }
         
-        return convertedWorkspaces;
+        return newPath;
     }
 }
