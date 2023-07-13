89a3476ace097f2cf155aef37aefeda8dd545370
Shanty Kamal <shanty@snps-lyfuywmnj01f.internal.synopsys.com>
2023-07-13 13:20:23 -0600

    Disable ITs for now. See IDETECT-3952

--- src/test/java/com/synopsys/integration/detect/workflow/componentlocationanalysis/GenerateComponentLocationAnalysisOperationIT.java
+++ src/test/java/com/synopsys/integration/detect/workflow/componentlocationanalysis/GenerateComponentLocationAnalysisOperationIT.java
@@ -5,11 +5,13 @@ import com.synopsys.integration.detect.battery.docker.util.DetectCommandBuilder;
 import com.synopsys.integration.detect.battery.docker.util.DetectDockerTestRunner;
 import com.synopsys.integration.detect.battery.docker.util.DockerAssertions;
 import com.synopsys.integration.detect.configuration.DetectProperties;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
+@Disabled
 @Tag("integration")
 public class GenerateComponentLocationAnalysisOperationIT {
     @Test
