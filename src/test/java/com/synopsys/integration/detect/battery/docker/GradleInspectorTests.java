6d40029df46f017d2b2588f7584f4a2ea8979c2e
Atiq Zaman <atiq.zaman@dsinnovators.com>
2023-07-17 13:02:43 +0600

    Gradle native inspecor integration test with latest gradle versions (7.6 & 8.2)

---
+++ src/test/java/com/synopsys/integration/detect/battery/docker/GradleInspectorTests.java
@@ -0,0 +1,51 @@
+package com.synopsys.integration.detect.battery.docker;
+
+import com.synopsys.integration.detect.battery.docker.provider.BuildDockerImageProvider;
+import com.synopsys.integration.detect.battery.docker.util.DetectCommandBuilder;
+import com.synopsys.integration.detect.battery.docker.util.DetectDockerTestRunner;
+import com.synopsys.integration.detect.battery.docker.util.DockerAssertions;
+import com.synopsys.integration.detect.configuration.DetectProperties;
+import com.synopsys.integration.detector.base.DetectorType;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+@Tag("integration")
+public class GradleInspectorTests {
+    @Test
+    void gradleInspector_7_6() throws IOException, InterruptedException {
+        try (DetectDockerTestRunner test = new DetectDockerTestRunner("detect-gradle-project-inspector", "gradle-simple-7-6:1.0.0")) {
+            test.withImageProvider(BuildDockerImageProvider.forDockerfilResourceNamed("SimpleGradle_7_6.dockerfile"));
+
+            DetectCommandBuilder commandBuilder = DetectCommandBuilder.withOfflineDefaults().defaultDirectories(test);
+            commandBuilder.property(DetectProperties.DETECT_TOOLS, "DETECTOR");
+            commandBuilder.property(DetectProperties.BLACKDUCK_OFFLINE_MODE, "true");
+            commandBuilder.property(DetectProperties.DETECT_ACCURACY_REQUIRED, "NONE");
+            commandBuilder.property(DetectProperties.DETECT_INCLUDED_DETECTOR_TYPES, DetectorType.GRADLE.toString());
+            DockerAssertions dockerAssertions = test.run(commandBuilder);
+
+            dockerAssertions.logContains("Gradle Native Inspector: SUCCESS");
+            dockerAssertions.logContains("GRADLE: SUCCESS");
+            dockerAssertions.atLeastOneBdioFile();
+        }
+    }
+
+    @Test
+    void gradleInspector_8_2() throws IOException, InterruptedException {
+        try (DetectDockerTestRunner test = new DetectDockerTestRunner("detect-gradle-project-inspector", "gradle-simple-8-2:1.0.0")) {
+            test.withImageProvider(BuildDockerImageProvider.forDockerfilResourceNamed("SimpleGradle_8_2.dockerfile"));
+
+            DetectCommandBuilder commandBuilder = DetectCommandBuilder.withOfflineDefaults().defaultDirectories(test);
+            commandBuilder.property(DetectProperties.DETECT_TOOLS, "DETECTOR");
+            commandBuilder.property(DetectProperties.BLACKDUCK_OFFLINE_MODE, "true");
+            commandBuilder.property(DetectProperties.DETECT_ACCURACY_REQUIRED, "NONE");
+            commandBuilder.property(DetectProperties.DETECT_INCLUDED_DETECTOR_TYPES, DetectorType.GRADLE.toString());
+            DockerAssertions dockerAssertions = test.run(commandBuilder);
+
+            dockerAssertions.logContains("Gradle Native Inspector: SUCCESS");
+            dockerAssertions.logContains("GRADLE: SUCCESS");
+            dockerAssertions.atLeastOneBdioFile();
+        }
+    }
+}
