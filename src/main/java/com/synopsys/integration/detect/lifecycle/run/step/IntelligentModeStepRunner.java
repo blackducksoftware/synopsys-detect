597429049d562e3c7750afeecf7ab7dd427f72b4
Shanty Kamal <shanty@snps-lyfuywmnj01f.internal.synopsys.com>
2023-07-26 06:46:28 -0600

    Throw exception and fail Component Location Analysis in all cases where the output file cannot be generated (unsupported detectors, unsupported scan mode, empty rapid results, etc)

--- src/main/java/com/synopsys/integration/detect/lifecycle/run/step/IntelligentModeStepRunner.java
+++ src/main/java/com/synopsys/integration/detect/lifecycle/run/step/IntelligentModeStepRunner.java
@@ -8,6 +8,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import com.synopsys.integration.detect.workflow.componentlocationanalysis.ComponentLocatorException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
