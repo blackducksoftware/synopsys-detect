b0265f8e33eee5f1a69925ef69ea99755dd30123
Nirav Raje <niravr@synopsys.com>
2023-08-17 00:06:39 -0400

    Parameterized test setup for go versions; support dockerfile arg for goversion

--- src/test/java/com/synopsys/integration/detect/battery/docker/GoModTest.java
+++ src/test/java/com/synopsys/integration/detect/battery/docker/GoModTest.java
@@ -1,9 +1,15 @@
 package com.synopsys.integration.detect.battery.docker;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import com.synopsys.integration.detect.battery.docker.integration.BlackDuckAssertions;
 import com.synopsys.integration.detect.battery.docker.integration.BlackDuckTestConnection;
@@ -12,25 +18,76 @@ import com.synopsys.integration.detect.battery.docker.util.DetectCommandBuilder;
 import com.synopsys.integration.detect.battery.docker.util.DetectDockerTestRunner;
 import com.synopsys.integration.detect.battery.docker.util.DockerAssertions;
 import com.synopsys.integration.detect.configuration.DetectProperties;
+import com.synopsys.integration.detect.configuration.enumeration.DetectTool;
+import com.synopsys.integration.detector.base.DetectorType;
 import com.synopsys.integration.exception.IntegrationException;
 
 //@Tag("integration")
 public class GoModTest {
 
-    void goModOfflineTest() throws IOException {
-        try (DetectDockerTestRunner test = new DetectDockerTestRunner("go-mod-executables", "go-mod-executables:1.0.0")) {
-            test.withImageProvider(BuildDockerImageProvider.forDockerfilResourceNamed("GoModExecutables.dockerfile"));
+    private static final String[] GO_VERSIONS_TO_TEST = new String[] {
+        "1.16.5",
+        "1.17.6",
+        "1.18.8",
+        "1.19.6",
+        "1.20.4"
+    };
 
-            DetectCommandBuilder commandBuilder = DetectCommandBuilder.withOfflineDefaults().defaultDirectories(test);
-            commandBuilder.property(DetectProperties.DETECT_TOOLS, "DETECTOR");
+    private static final String PROJECT_NAME = "go-mod-docker";
+
+//    @Test
+//    void goModExecutablesTest() throws IOException, IntegrationException {
+//        for (String goVersion : GO_VERSIONS_TO_TEST) {
+//            goModSpecificExecutableTest(goVersion);
+//        }
+//    }
+
+    private static Stream<String> provideGoVersionsToTest() {
+        return Arrays.stream(GO_VERSIONS_TO_TEST);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideGoVersionsToTest")
+    public void goModSpecificExecutableTest(String goVersion) throws IntegrationException, IOException {
+        try (DetectDockerTestRunner test = new DetectDockerTestRunner("go-mod-executables", "go-mod-executables:1.0.0")) {
+
+            Map<String, String> goModDockerfileArgs = new HashMap<>();
+            goModDockerfileArgs.put("goVersion", goVersion);
+
+            BuildDockerImageProvider buildDockerImageProvider = BuildDockerImageProvider.forDockerfilResourceNamed("GoModExecutables.dockerfile");
+            buildDockerImageProvider.setBuildArgs(goModDockerfileArgs);
+            test.withImageProvider(buildDockerImageProvider);
+
+            // Set up blackduck connection and environment
+            String projectVersion = PROJECT_NAME + "-" + goVersion;
+            BlackDuckTestConnection blackDuckTestConnection = BlackDuckTestConnection.fromEnvironment();
+            BlackDuckAssertions blackduckAssertions = blackDuckTestConnection.projectVersionAssertions(PROJECT_NAME, projectVersion);
+            blackduckAssertions.emptyOnBlackDuck();
+
+            // Build command with BlackDuck config
+            DetectCommandBuilder commandBuilder = new DetectCommandBuilder().defaults().defaultDirectories(test);
+            commandBuilder.connectToBlackDuck(blackDuckTestConnection);
+            commandBuilder.projectNameVersion(blackduckAssertions);
+            commandBuilder.waitForResults();
+
+            // Set up Detect properties
+            commandBuilder.property(DetectProperties.DETECT_TOOLS, DetectTool.DETECTOR.toString());
             commandBuilder.property(DetectProperties.DETECT_GO_PATH, "/usr/local/go1.16.6/go/bin/go");
             DockerAssertions dockerAssertions = test.run(commandBuilder);
 
-            dockerAssertions.successfulDetectorType("GO_MOD");
+            // Detect specific assertions
+            dockerAssertions.successfulDetectorType(DetectorType.GO_MOD.toString());
             dockerAssertions.atLeastOneBdioFile();
+
+            // Blackduck specific assertions
+            String codeLocationName = PROJECT_NAME + "/" + projectVersion + " bdio";
+            blackduckAssertions.hasCodeLocations(codeLocationName);
+            validateComponentsForSampleGoProject(blackduckAssertions);
         }
     }
 
+    // These are all components in the go.mod file of the test project "https://github.com/Masterminds/squirrel.git (v1.5.4)" that should appear on the BOM on BlackDuck
+    // If ever updating the above test project, ensure to update the component list below accordingly
     private void validateComponentsForSampleGoProject(BlackDuckAssertions blackduckAssertions) throws IntegrationException {
         blackduckAssertions.hasComponents("Go Testify");
         blackduckAssertions.hasComponents("go-spew");
@@ -39,36 +96,4 @@ public class GoModTest {
         blackduckAssertions.hasComponents("pmezard-go-difflib");
     }
 
-    @Test
-    void goModExecutablesTest() throws IOException, IntegrationException {
-        try (DetectDockerTestRunner test = new DetectDockerTestRunner("go-mod-executables", "go-mod-executables:1.0.0")) {
-            test.withImageProvider(BuildDockerImageProvider.forDockerfilResourceNamed("GoModExecutables.dockerfile"));
-
-            // Set up blackduck connection and environment
-            BlackDuckTestConnection blackDuckTestConnection = BlackDuckTestConnection.fromEnvironment();
-            BlackDuckAssertions blackduckAssertions = blackDuckTestConnection.projectVersionAssertions("go-mod-docker", "go-mod-docker-version");
-            blackduckAssertions.emptyOnBlackDuck();
-
-            DetectCommandBuilder commandBuilder = new DetectCommandBuilder().defaults().defaultDirectories(test);
-            commandBuilder.connectToBlackDuck(blackDuckTestConnection);
-            commandBuilder.projectNameVersion(blackduckAssertions);
-            commandBuilder.waitForResults();
-
-            // Set up Detect properties
-            commandBuilder.property(DetectProperties.DETECT_TOOLS, "DETECTOR");
-            commandBuilder.property(DetectProperties.DETECT_GO_PATH, "/usr/local/go1.16.6/go/bin/go");
-            DockerAssertions dockerAssertions = test.run(commandBuilder);
-
-            // Detect specific assertions
-            dockerAssertions.successfulDetectorType("GO_MOD");
-            dockerAssertions.atLeastOneBdioFile();
-
-            // Blackduck specific assertions
-            blackduckAssertions.hasCodeLocations(
-                "go-mod-docker/go-mod-docker-version bdio"
-            );
-            validateComponentsForSampleGoProject(blackduckAssertions);
-
-        }
-    }
 }
--- src/test/resources/docker/GoModExecutables.dockerfile
+++ src/test/resources/docker/GoModExecutables.dockerfile
@@ -1,5 +1,6 @@
 FROM --platform=linux/amd64 openjdk:8-jdk
 
+# Default value of "goVersion" below is overridden by value passed as argument when building the image
 ARG goVersion=1.16.6
 
 # Do not change SRC_DIR, value is expected by tests
@@ -9,7 +10,7 @@ ENV SRC_DIR=/opt/project/src
 RUN apt-get update
 RUN apt-get install -y git bash
 
-# Install Go@1.16.6
+# Install Go
 WORKDIR /usr/local
 RUN wget -q https://golang.org/dl/go${goVersion}.linux-amd64.tar.gz
 RUN mkdir go${goVersion}
@@ -18,5 +19,4 @@ RUN rm go${goVersion}.linux-amd64.tar.gz
 
 # Set up the test project
 RUN mkdir -p ${SRC_DIR}
-
-RUN git clone --depth 1 https://github.com/Masterminds/squirrel.git ${SRC_DIR}
+RUN git clone -b v1.5.4 --depth 1 https://github.com/Masterminds/squirrel.git ${SRC_DIR}
