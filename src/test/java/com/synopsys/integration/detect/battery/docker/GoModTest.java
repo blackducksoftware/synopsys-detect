4ac75b14a5b876cc833b1794a865660a9bf9269f
Nirav Raje <niravr@synopsys.com>
2023-08-16 21:39:08 -0400

    Add BD integration tests and parameterize go version in Dockerfile

--- src/test/java/com/synopsys/integration/detect/battery/docker/GoModTest.java
+++ src/test/java/com/synopsys/integration/detect/battery/docker/GoModTest.java
@@ -5,16 +5,19 @@ import java.io.IOException;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import com.synopsys.integration.detect.battery.docker.integration.BlackDuckAssertions;
+import com.synopsys.integration.detect.battery.docker.integration.BlackDuckTestConnection;
 import com.synopsys.integration.detect.battery.docker.provider.BuildDockerImageProvider;
 import com.synopsys.integration.detect.battery.docker.util.DetectCommandBuilder;
 import com.synopsys.integration.detect.battery.docker.util.DetectDockerTestRunner;
 import com.synopsys.integration.detect.battery.docker.util.DockerAssertions;
 import com.synopsys.integration.detect.configuration.DetectProperties;
+import com.synopsys.integration.exception.IntegrationException;
 
 //@Tag("integration")
 public class GoModTest {
-    @Test
-    void goModExecutablesTest() throws IOException {
+
+    void goModOfflineTest() throws IOException {
         try (DetectDockerTestRunner test = new DetectDockerTestRunner("go-mod-executables", "go-mod-executables:1.0.0")) {
             test.withImageProvider(BuildDockerImageProvider.forDockerfilResourceNamed("GoModExecutables.dockerfile"));
 
@@ -25,6 +28,47 @@ public class GoModTest {
 
             dockerAssertions.successfulDetectorType("GO_MOD");
             dockerAssertions.atLeastOneBdioFile();
+        }
+    }
+
+    private void validateComponentsForSampleGoProject(BlackDuckAssertions blackduckAssertions) throws IntegrationException {
+        blackduckAssertions.hasComponents("Go Testify");
+        blackduckAssertions.hasComponents("go-spew");
+        blackduckAssertions.hasComponents("lann-builder");
+        blackduckAssertions.hasComponents("lann-ps");
+        blackduckAssertions.hasComponents("pmezard-go-difflib");
+    }
+
+    @Test
+    void goModExecutablesTest() throws IOException, IntegrationException {
+        try (DetectDockerTestRunner test = new DetectDockerTestRunner("go-mod-executables", "go-mod-executables:1.0.0")) {
+            test.withImageProvider(BuildDockerImageProvider.forDockerfilResourceNamed("GoModExecutables.dockerfile"));
+
+            // Set up blackduck connection and environment
+            BlackDuckTestConnection blackDuckTestConnection = BlackDuckTestConnection.fromEnvironment();
+            BlackDuckAssertions blackduckAssertions = blackDuckTestConnection.projectVersionAssertions("go-mod-docker", "go-mod-docker-version");
+            blackduckAssertions.emptyOnBlackDuck();
+
+            DetectCommandBuilder commandBuilder = new DetectCommandBuilder().defaults().defaultDirectories(test);
+            commandBuilder.connectToBlackDuck(blackDuckTestConnection);
+            commandBuilder.projectNameVersion(blackduckAssertions);
+            commandBuilder.waitForResults();
+
+            // Set up Detect properties
+            commandBuilder.property(DetectProperties.DETECT_TOOLS, "DETECTOR");
+            commandBuilder.property(DetectProperties.DETECT_GO_PATH, "/usr/local/go1.16.6/go/bin/go");
+            DockerAssertions dockerAssertions = test.run(commandBuilder);
+
+            // Detect specific assertions
+            dockerAssertions.successfulDetectorType("GO_MOD");
+            dockerAssertions.atLeastOneBdioFile();
+
+            // Blackduck specific assertions
+            blackduckAssertions.hasCodeLocations(
+                "go-mod-docker/go-mod-docker-version bdio"
+            );
+            validateComponentsForSampleGoProject(blackduckAssertions);
+
         }
     }
 }
--- src/test/resources/docker/GoModExecutables.dockerfile
+++ src/test/resources/docker/GoModExecutables.dockerfile
@@ -1,5 +1,7 @@
 FROM --platform=linux/amd64 openjdk:8-jdk
 
+ARG goVersion=1.16.6
+
 # Do not change SRC_DIR, value is expected by tests
 ENV SRC_DIR=/opt/project/src
 
@@ -9,10 +11,10 @@ RUN apt-get install -y git bash
 
 # Install Go@1.16.6
 WORKDIR /usr/local
-RUN wget -q "https://golang.org/dl/go1.16.6.linux-amd64.tar.gz"
-RUN mkdir go1.16.6
-RUN tar -C go1.16.6 -xzf go1.16.6.linux-amd64.tar.gz
-RUN rm go1.16.6.linux-amd64.tar.gz
+RUN wget -q https://golang.org/dl/go${goVersion}.linux-amd64.tar.gz
+RUN mkdir go${goVersion}
+RUN tar -C go${goVersion} -xzf go${goVersion}.linux-amd64.tar.gz
+RUN rm go${goVersion}.linux-amd64.tar.gz
 
 # Set up the test project
 RUN mkdir -p ${SRC_DIR}
