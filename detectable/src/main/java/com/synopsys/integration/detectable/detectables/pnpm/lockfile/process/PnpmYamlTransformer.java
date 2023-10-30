07e4d59bdd44b454ca4c96dc7fb0856b2cfdf03a
David Terry <dterry@synopsys.com>
2023-10-30 13:57:51 -0400

    large refactor of comparison code for v6

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/pnpm/lockfile/process/PnpmYamlTransformer.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/pnpm/lockfile/process/PnpmYamlTransformer.java
@@ -28,6 +28,7 @@ import com.synopsys.integration.detectable.detectables.pnpm.lockfile.model.PnpmP
 import com.synopsys.integration.exception.IntegrationException;
 import com.synopsys.integration.util.NameVersion;
 
+// TODO have to keep both code paths so potentially need two transformers
 public class PnpmYamlTransformer {
     private static final String LINKED_PACKAGE_PREFIX = "link:";
 
@@ -77,22 +78,13 @@ public class PnpmYamlTransformer {
             throw new DetectableException("Could not parse 'packages' section of the pnpm-lock.yaml file.");
         }
         for (Map.Entry<String, PnpmPackageInfo> packageEntry : packageMap.entrySet()) {
-//            String packageEntryKey = packageEntry.getKey();
-//            int indexOfLastAt = packageEntryKey.lastIndexOf("@");
-//            String name = packageEntryKey.substring(0, indexOfLastAt);
-//            String version = packageEntryKey.substring(indexOfLastAt + 1);
-//            String packageId = name + "/" + version;  
-
-            //String packageId = packageEntry.getKey();
             String packageId = packageEntry.getKey();
             Optional<Dependency> pnpmPackage = buildDependencyFromPackageEntry(packageEntry);
             if (!pnpmPackage.isPresent()) {
                 logger.debug(String.format("Could not add package %s to the graph.", packageId));
                 continue;
             }
-
-            // TODO there seems to be parsing, potentially here, as well as in parseNameVersionFromIdForRoot
-            // where I need to align the two different formats from the v6 lockfile.
+            
             if (isRootPackage(packageId, rootPackageIds)) {
                 graphBuilder.addChildToRoot(pnpmPackage.get());
             }
@@ -100,8 +92,8 @@ public class PnpmYamlTransformer {
             PnpmPackageInfo packageInfo = packageEntry.getValue();
             if (!packageInfo.getDependencyType().isPresent() || dependencyTypeFilter.shouldInclude(packageInfo.getDependencyType().get())) {
                 for (Map.Entry<String, String> packageDependency : packageInfo.getDependencies().entrySet()) {
-                    String dependencyPackageId = convertRawEntryToPackageId(packageDependency, linkedPackageResolver, reportingProjectPackagePath);
-                    Optional<Dependency> child = buildDependencyFromPackageIdWithSlash(dependencyPackageId);
+                    String dependencyPackageId = convertRawEntryToPackageId(packageDependency.getKey(), packageDependency.getValue(), linkedPackageResolver, reportingProjectPackagePath);
+                    Optional<Dependency> child = buildDependencyFromPackageId(dependencyPackageId);
                     child.ifPresent(c -> graphBuilder.addChildWithParent(child.get(), pnpmPackage.get()));
                 }
             }
@@ -130,74 +122,33 @@ public class PnpmYamlTransformer {
         dependencyTypeFilter.ifShouldInclude(PnpmDependencyType.DEV, pnpmProjectPackage.devDependencies, rawPackageInfo::putAll);
         dependencyTypeFilter.ifShouldInclude(PnpmDependencyType.OPTIONAL, pnpmProjectPackage.optionalDependencies, rawPackageInfo::putAll);
 
-        // TODO major key area, make sure this parses correctly compared to v5
         return rawPackageInfo.entrySet().stream()
-            .map(entry -> convertPnpmDependencyEntryToPackageId(entry, linkedPackageResolver, reportingProjectPackagePath))
+            .map(entry -> convertRawEntryToPackageId(entry.getKey(), entry.getValue().version, linkedPackageResolver, reportingProjectPackagePath))
             .collect(Collectors.toList());
     }
 
-    private String convertRawEntryToPackageId(Map.Entry<String, String> entry, PnpmLinkedPackageResolver linkedPackageResolver, @Nullable String reportingProjectPackagePath) {
-        String name = StringUtils.strip(entry.getKey(), "'");
-        String version = entry.getValue();
+    private String convertRawEntryToPackageId(String name, String version, PnpmLinkedPackageResolver linkedPackageResolver, @Nullable String reportingProjectPackagePath) {
+        name = StringUtils.strip(name, "'");
         if (version.startsWith(LINKED_PACKAGE_PREFIX)) {
             // a linked project package's version will be referenced in the format: <linkPrefix><pathToLinkedPackageRelativeToReportingProjectPackage>
             version = linkedPackageResolver.resolveVersionOfLinkedPackage(reportingProjectPackagePath, version.replace(LINKED_PACKAGE_PREFIX, ""));
         }
-        return String.format("/%s/%s", name, version);
-    }
-    
-    // TODO can combine this code with the above if just always pass in name and version instead of entry
-    private String convertPnpmDependencyEntryToPackageId(Map.Entry<String, PnpmDependencyInfo> entry, PnpmLinkedPackageResolver linkedPackageResolver, @Nullable String reportingProjectPackagePath) {
-        String name = StringUtils.strip(entry.getKey(), "'");
-        String version = entry.getValue().version;
-        if (version.startsWith(LINKED_PACKAGE_PREFIX)) {
-            // a linked project package's version will be referenced in the format: <linkPrefix><pathToLinkedPackageRelativeToReportingProjectPackage>
-            version = linkedPackageResolver.resolveVersionOfLinkedPackage(reportingProjectPackagePath, version.replace(LINKED_PACKAGE_PREFIX, ""));
-        }
-        // TODO could I just put @ between these values and have an easier time?
-        // seems like yes but maybe not fo duel code path
-        return String.format("/%s/%s", name, version);
+        return String.format("/%s@%s", name, version);
     }
 
     private Optional<NameVersion> parseNameVersionFromId(String id) {
-        // ids follow format: /name/version, where name often contains slashes
+        // ids follow format: /name/version for v5 and /name@version for v6
         try {
-         // TODO have to keep both code paths, it seems critical not to send this extra _ in v5 or () in v6
-            // stuff or the kb has a tough time matching it.
-         if (id.contains("(")) {
-             id = id.split("\\(")[0];
-         }
-          
-//             Pattern pattern = Pattern.compile("\\@(.*?)\\(");
-//             Matcher matcher = pattern.matcher(id);
-//             matcher.find();
-//             String version = matcher.group(1);
-//             return Optional.of(new NameVersion("name", version));
-//         } else {
-             int indexOfLastSlash = id.lastIndexOf("@");
-             String name = id.substring(1, indexOfLastSlash);
-             String version = id.substring(indexOfLastSlash + 1);
-             return Optional.of(new NameVersion(name, version));
-//         }
-        } catch (Exception e) {
-            logger.debug(String.format("There was an issue parsing package id: %s.  This is likely an unsupported format.", id));
-            return Optional.empty();
-        }
-    }
-    
-    private Optional<NameVersion> parseNameVersionFromIdWithSlash(String id) {
-        // ids follow format: /name/version, where name often contains slashes
-        // TODO This seems to be used when we are dealing with root ids and when dealing with 
-        // transitive child IDs        
-        if (id.contains("(")) {
-            // TODO we seemt get extra data inside ()'s in v6, it appears we don't need it as it 
-            // seems to confuse the KB
-            id = id.split("\\(")[0];
-        }
-        
-        try {
-            // TODO if this works could send in the separator I want
-            int indexOfLastSlash = id.lastIndexOf("/");
+            // It seems critical not to send this extra information in () for v6
+            // or the kb will fail matching it. This is a problem for _ in v5 but we don't
+            // want to cause regressions in current behavior there.
+            if (id.contains("(")) {
+                id = id.split("\\(")[0];
+            }
+
+            // TODO for v6 need @ but could potentially send in separator (/ for v5) as an
+            // argument
+            int indexOfLastSlash = id.lastIndexOf("@");
             String name = id.substring(1, indexOfLastSlash);
             String version = id.substring(indexOfLastSlash + 1);
             return Optional.of(new NameVersion(name, version));
@@ -219,31 +170,15 @@ public class PnpmYamlTransformer {
         return parseNameVersionFromId(packageId)
             .map(nameVersion -> Dependency.FACTORY.createNameVersionDependency(Forge.NPMJS, nameVersion.getName(), nameVersion.getVersion()));
     }
-    
-    private Optional<Dependency> buildDependencyFromPackageIdWithSlash(String packageId) {
-        return parseNameVersionFromIdWithSlash(packageId)
-            .map(nameVersion -> Dependency.FACTORY.createNameVersionDependency(Forge.NPMJS, nameVersion.getName(), nameVersion.getVersion()));
-    }
 
     private boolean isRootPackage(String id, List<String> rootIds) {
-        return compareIgnoringSeparator(id, rootIds) ||
+        return rootIds.contains(id) ||
             rootIds.stream()
-                .map(this::parseNameVersionFromIdWithSlash)
+                .map(this::parseNameVersionFromId)
                 .filter(Optional::isPresent)
                 .map(Optional::get)
                 .map(NameVersion::getVersion)
                 .anyMatch(id::equals); // for file dependencies, they are declared as <name> : <fileIdAsReportedInPackagesSection>
     }
-
-    private boolean compareIgnoringSeparator(String s1, List<String> stringsToCheck) {
-        // TODO if I build with @ instead of / do I need this?
-        for (String s2 : stringsToCheck) {
-            if (s1.replace("@", "/").equals(s2.replace("@", "/"))) {
-                return true;
-            }
-        }
-        
-        return false;
-    }
-    
+ 
 }
