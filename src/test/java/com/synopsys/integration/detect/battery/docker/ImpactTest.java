df70dd7c5ae888b11de0a7b8243feb402247f3a5
devmehtasynopsys <devmehta@synopsys.com>
2024-02-05 16:22:02 -0500

    Add ASM error tracker test case

--- src/test/java/com/synopsys/integration/detect/battery/docker/ImpactTest.java
+++ src/test/java/com/synopsys/integration/detect/battery/docker/ImpactTest.java
@@ -11,7 +11,7 @@ import com.synopsys.integration.detect.battery.docker.util.DetectDockerTestRunne
 import com.synopsys.integration.detect.battery.docker.util.DockerAssertions;
 import com.synopsys.integration.detect.configuration.DetectProperties;
 
-@Tag("integration")
+//@Tag("integration")
 public class ImpactTest {
     @Test
     void offlineImpact() throws IOException {
@@ -41,6 +41,22 @@ public class ImpactTest {
             DockerAssertions dockerAssertions = test.run(commandBuilder);
 
             dockerAssertions.logContains("Vulnerability Impact Analysis generated report at /tmp/external-method-uses.bdmu");
+        }
+    }
+
+    @Test
+    void impactASMErrorMaven() throws IOException {
+        try (DetectDockerTestRunner test = new DetectDockerTestRunner("detect-impact-asm-error-test", "detect-impact-test:1.1.0")) {
+            test.withImageProvider(BuildDockerImageProvider.forDockerfilResourceNamed("ImpactAnalysis_ASMErrorMaven.dockerfile"));
+
+            DetectCommandBuilder commandBuilder = DetectCommandBuilder.withOfflineDefaults().defaultDirectories(test);
+            commandBuilder.property(DetectProperties.DETECT_TOOLS, "IMPACT_ANALYSIS");
+            commandBuilder.property(DetectProperties.DETECT_IMPACT_ANALYSIS_ENABLED, "true");
+            DockerAssertions dockerAssertions = test.run(commandBuilder);
+
+            dockerAssertions.successfulTool("IMPACT_ANALYSIS");
+            dockerAssertions.logContainsPattern("Vulnerability Impact Analysis generated report at /opt/results/output/runs/.*/impact-analysis/external-method-uses.bdmu");
+            dockerAssertions.successfulOperation("Generate Impact Analysis File");
         }
     }
 }
---
+++ src/test/resources/docker/ImpactAnalysis_ASMErrorMaven.dockerfile
@@ -0,0 +1,16 @@
+FROM adoptopenjdk/maven-openjdk11
+
+# Do not change SRC_DIR, value is expected by tests
+ENV SRC_DIR=/opt/project/src
+
+# Install git
+RUN apt-get update
+RUN apt-get install -y git
+
+# Set up the test project
+RUN mkdir -p ${SRC_DIR}
+
+RUN git clone --depth 1 https://github.com/webgoat/webgoat.git ${SRC_DIR}
+
+RUN cd ${SRC_DIR} \
+   && mvn clean compile -DskipTests
