daea2c2d263604815c564323ac8b2fbfdca20647
devmehtasynopsys <devmehta@synopsys.com>
2024-02-06 17:04:21 -0500

    Uncomment integration tag

--- src/test/java/com/synopsys/integration/detect/battery/docker/ImpactTest.java
+++ src/test/java/com/synopsys/integration/detect/battery/docker/ImpactTest.java
@@ -11,7 +11,7 @@ import com.synopsys.integration.detect.battery.docker.util.DetectDockerTestRunne
 import com.synopsys.integration.detect.battery.docker.util.DockerAssertions;
 import com.synopsys.integration.detect.configuration.DetectProperties;
 
-//@Tag("integration")
+@Tag("integration")
 public class ImpactTest {
     @Test
     void offlineImpact() throws IOException {
