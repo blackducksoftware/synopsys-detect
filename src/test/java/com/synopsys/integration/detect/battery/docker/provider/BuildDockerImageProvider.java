a1f6e33ea51f5f538764cfca7a9ac0d7c941e35a
devmehtasynopsys <devmehta@synopsys.com>
2024-03-12 15:46:56 -0400

    add correct comment for dockerfile

--- src/test/java/com/synopsys/integration/detect/battery/docker/provider/BuildDockerImageProvider.java
+++ src/test/java/com/synopsys/integration/detect/battery/docker/provider/BuildDockerImageProvider.java
@@ -39,6 +39,7 @@ public class BuildDockerImageProvider implements DockerImageProvider {
 
         try (BuildImageCmd buildImageCmd = dockerClient.buildImageCmd(imageDockerFile)) {
 
+            // If the parent test has provided Dockerfile args, pass them to the image build command
             for (Map.Entry<String, String> entry : buildArgs.entrySet()) {
                 buildImageCmd.withBuildArg(entry.getKey(), entry.getValue());
             }
