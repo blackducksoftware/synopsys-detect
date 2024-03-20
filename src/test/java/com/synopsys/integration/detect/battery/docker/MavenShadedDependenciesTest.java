38fba9b854eb427a91b26584d8333eb3bb0791b3
devmehtasynopsys <devmehta@synopsys.com>
2024-03-20 12:09:08 -0400

    Final Artifactory property updates

--- src/test/java/com/synopsys/integration/detect/battery/docker/MavenShadedDependenciesTest.java
+++ src/test/java/com/synopsys/integration/detect/battery/docker/MavenShadedDependenciesTest.java
@@ -15,39 +15,38 @@ import com.synopsys.integration.detect.battery.docker.util.DockerAssertions;
 import com.synopsys.integration.detect.configuration.DetectProperties;
 import com.synopsys.integration.detector.base.DetectorType;
 
-//@Tag("integration")
+@Tag("integration")
 public class MavenShadedDependenciesTest {
 
     private static final String PROJECT_NAME = "maven-shaded-dependency";
 
-//    @Test
-//    void mavenShadedDependencyTest() throws IOException,IntegrationException {
-//        try(DetectDockerTestRunner test = new DetectDockerTestRunner("detect-maven-shaded-dependency", "detect-maven:1.0.0")) {
-//            test.withImageProvider(BuildDockerImageProvider.forDockerfilResourceNamed("MavenShadedDependencies.dockerfile"));
-//
-//            String projectVersion = PROJECT_NAME + "-PI";
-//            BlackDuckTestConnection blackDuckTestConnection = BlackDuckTestConnection.fromEnvironment();
-//            BlackDuckAssertions blackduckAssertions = blackDuckTestConnection.projectVersionAssertions(PROJECT_NAME, projectVersion);
-//            blackduckAssertions.emptyOnBlackDuck();
-//
-//            DetectCommandBuilder commandBuilder = new DetectCommandBuilder().defaults().defaultDirectories(test);
-//            commandBuilder.connectToBlackDuck(blackDuckTestConnection);
-//            commandBuilder.projectNameVersion(blackduckAssertions);
-//            commandBuilder.waitForResults();
-//
-//            commandBuilder.property(DetectProperties.DETECT_TOOLS, "DETECTOR");
-//            commandBuilder.property(DetectProperties.DETECT_INCLUDED_DETECTOR_TYPES, DetectorType.MAVEN.toString());
-//            commandBuilder.property(DetectProperties.DETECT_MAVEN_INCLUDE_SHADED_DEPENDENCIES, String.valueOf(true));
-//            DockerAssertions dockerAssertions = test.run(commandBuilder);
-//
-//            dockerAssertions.logContains("Maven CLI: SUCCESS");
-//            dockerAssertions.atLeastOneBdioFile();
-//
-//            blackduckAssertions.hasComponents("ch.randelshofer:fastdoubleparser");
-//            blackduckAssertions.hasComponents("ASM");
-//            blackduckAssertions.hasComponents("Java Concurrency Tools Core Library");
-//            blackduckAssertions.hasComponents("Byte Buddy (with dependencies)");
-//        }
-//    }
+    @Test
+    void mavenShadedDependencyTest() throws IOException,IntegrationException {
+        try(DetectDockerTestRunner test = new DetectDockerTestRunner("detect-maven-shaded-dependency", "detect-maven:1.0.0")) {
+            test.withImageProvider(BuildDockerImageProvider.forDockerfilResourceNamed("MavenShadedDependencies.dockerfile"));
+
+            String projectVersion = PROJECT_NAME + "-PI";
+            BlackDuckTestConnection blackDuckTestConnection = BlackDuckTestConnection.fromEnvironment();
+            BlackDuckAssertions blackduckAssertions = blackDuckTestConnection.projectVersionAssertions(PROJECT_NAME, projectVersion);
+            blackduckAssertions.emptyOnBlackDuck();
+
+            DetectCommandBuilder commandBuilder = new DetectCommandBuilder().defaults().defaultDirectories(test);
+            commandBuilder.connectToBlackDuck(blackDuckTestConnection);
+            commandBuilder.projectNameVersion(blackduckAssertions);
+            commandBuilder.waitForResults();
+
+            commandBuilder.property(DetectProperties.DETECT_TOOLS, "DETECTOR");
+            commandBuilder.property(DetectProperties.DETECT_INCLUDED_DETECTOR_TYPES, DetectorType.MAVEN.toString());
+            commandBuilder.property(DetectProperties.DETECT_MAVEN_INCLUDE_SHADED_DEPENDENCIES, String.valueOf(true));
+            DockerAssertions dockerAssertions = test.run(commandBuilder);
+
+            dockerAssertions.logContains("Maven CLI: SUCCESS");
+            dockerAssertions.atLeastOneBdioFile();
+
+            blackduckAssertions.hasComponents("ch.randelshofer:fastdoubleparser");
+            blackduckAssertions.hasComponents("Java Concurrency Tools Core Library");
+            blackduckAssertions.hasComponents("Byte Buddy (with dependencies)");
+        }
+    }
 
 }
