8c9bdf80163c34d1913ce69db53e69b29f25651a
Andrian Sevastyanov <andrians@synopsys.com>
2024-04-17 12:18:06 -0600

    Change exluded groups type from list to set for faster lookups

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/poetry/PoetryOptions.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/poetry/PoetryOptions.java
@@ -1,15 +1,17 @@
 package com.synopsys.integration.detectable.detectables.poetry;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class PoetryOptions {
-    private final List<String> excludedGroups;
+    private final Set<String> excludedGroups;
 
     public PoetryOptions(List<String> excludedGroups) {
-        this.excludedGroups = excludedGroups;
+        this.excludedGroups = new HashSet<>(excludedGroups);
     }
 
-    public List<String> getExcludedGroups() {
+    public Set<String> getExcludedGroups() {
         return excludedGroups;
     }
 }
