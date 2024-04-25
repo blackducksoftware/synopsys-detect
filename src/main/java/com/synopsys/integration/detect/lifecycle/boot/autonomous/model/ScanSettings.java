4411a67b3dc8a950a74bffe038c9aae33297dabf
Nirav Raje <niravr@synopsys.com>
2024-04-24 22:46:24 -0400

    Add javadoc and update to using sorted sets and maps

--- src/main/java/com/synopsys/integration/detect/lifecycle/boot/autonomous/model/ScanSettings.java
+++ src/main/java/com/synopsys/integration/detect/lifecycle/boot/autonomous/model/ScanSettings.java
@@ -4,25 +4,45 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import org.apache.commons.collections.set.AbstractSortedSetDecorator;
 
 public class ScanSettings {
-    Map<String, String> globalDetectProperties = new HashMap<>();
-    Map<String, String> detectorSharedProperties = new HashMap<>();
-    Set<ScanType> scanTypes = new HashSet<>();
+    /**
+     * Sorted property-value hash map for Detect's globally applicable properties.
+     * Accepts property name and value as a <code>String</code>.
+     * <br>Example: <code>globalDetectProperties.put("detect.tools", "BINARY_SCAN,DETECTOR")</code>
+     */
+    private SortedMap<String, String> globalDetectProperties = new TreeMap<>();
+    /**
+     * Sorted property-value hash map for properties that are shared across all detectors.
+     * Accepts property name and value as a <code>String</code>.
+     * <br>Example: <code>globalDetectProperties.put("detect.detector.search.depth", "3")</code>
+     */
+    private Map<String, String> detectorSharedProperties = new HashMap<>();
+    /**
+     * Sorted set of {@link ScanType} objects.
+     * Each <code>ScanType</code> object stores properties that are only applicable to that scan type.
+     */
+    private SortedSet<ScanType> scanTypes = new TreeSet<>();
 
-    public Map<String, String> getGlobalDetectProperties() {
+    public SortedMap<String, String> getGlobalDetectProperties() {
         return globalDetectProperties;
     }
 
-    public void setGlobalDetectProperties(final Map<String, String> globalDetectProperties) {
+    public void setGlobalDetectProperties(final SortedMap<String, String> globalDetectProperties) {
         this.globalDetectProperties = globalDetectProperties;
     }
 
-    public Set<ScanType> getScanTypes() {
+    public SortedSet<ScanType> getScanTypes() {
         return scanTypes;
     }
 
-    public void setScanTypes(final Set<ScanType> scanTypes) {
+    public void setScanTypes(final SortedSet<ScanType> scanTypes) {
         this.scanTypes = scanTypes;
     }
 }
--- src/main/java/com/synopsys/integration/detect/lifecycle/boot/autonomous/model/ScanType.java
+++ src/main/java/com/synopsys/integration/detect/lifecycle/boot/autonomous/model/ScanType.java
@@ -4,33 +4,54 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class ScanType {
-    private String scanType;
-    private Map<String, String> scanProperties = new HashMap<>();
-    private Set<String> scanTargets = new HashSet<>();
+    /**
+     * Name of the scan tool or detector that is applicable.
+     * Example: "SIGNATURE_SCAN" or "MAVEN".
+     */
+    private String scanTypeName;
+    /**
+     * Sorted property-value hash map for properties that are applicable only to the current scan type.
+     * Accepts property name and value as a <code>String</code>.
+     * Example: <code>scanProperties.put("detect.maven.path", "/path/to/maven/executable")</code>
+     */
+    private SortedMap<String, String> scanProperties = new TreeMap<>();
+    /**
+     * Sorted set of the absolute paths to the target that is being scanned.
+     * Accepts a <code>String</code> value for the target's path.
+     * For detectors, this is the set of path(s) to the package manager file(s).
+     * For binary/container scan, this is the set of path(s) to the binary/image to be scanned.
+     * <br>Example 1: <code>scanTargets.add("/path/to/build.gradle")</code>
+     * <br>Example 2: <code>scanTargets.add("/path/to/binary_1.exe")</code>
+     */
+    private SortedSet<String> scanTargets = new TreeSet<>();
 
-    public String getScanType() {
-        return scanType;
+    public String getScanTypeName() {
+        return scanTypeName;
     }
 
-    public void setScanType(final String scanType) {
-        this.scanType = scanType;
+    public void setScanTypeName(final String scanTypeName) {
+        this.scanTypeName = scanTypeName;
     }
 
     public Map<String, String> getScanProperties() {
         return scanProperties;
     }
 
-    public void setScanProperties(final Map<String, String> scanProperties) {
+    public void setScanProperties(final SortedMap<String, String> scanProperties) {
         this.scanProperties = scanProperties;
     }
 
-    public Set<String> getScanTargets() {
+    public SortedSet<String> getScanTargets() {
         return scanTargets;
     }
 
-    public void setScanTargets(final Set<String> scanTargets) {
+    public void setScanTargets(final SortedSet<String> scanTargets) {
         this.scanTargets = scanTargets;
     }
 }
