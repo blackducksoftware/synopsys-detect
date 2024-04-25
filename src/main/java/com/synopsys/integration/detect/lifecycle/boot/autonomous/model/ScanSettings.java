8f278e439b111942dfd5dda8c001b6a3037e0791
Nirav Raje <niravr@synopsys.com>
2024-04-25 00:20:03 -0400

    Update detectorSharedProperties to sorted maps, add javadoc for ScanSettings data model

--- src/main/java/com/synopsys/integration/detect/lifecycle/boot/autonomous/model/ScanSettings.java
+++ src/main/java/com/synopsys/integration/detect/lifecycle/boot/autonomous/model/ScanSettings.java
@@ -11,6 +11,16 @@ import java.util.TreeSet;
 
 import org.apache.commons.collections.set.AbstractSortedSetDecorator;
 
+/**
+ * ScanSettings is a data model used to store the properties and scan targets used by Detect for the given or
+ * derived <code>detect.source.path</code> value, when Detect's autonomous scanning mode is enabled.
+ * <br>If Detect has access to a scan settings JSON file from a previous run on the <code>detect.source.path</code>,
+ * the ScanSettings model will be initialized by deserializing this scan settings JSON file.
+ * <br>The ScanSettings model will also be updated based on the explicit properties provided
+ * by the user, and the autonomous decisions taken by Detect during the boot stage.
+ * <br>Once Detect's autonomous decisions and configurations are ready,
+ * these scan settings will be saved in an updated scan settings JSON file in the <code>blackduck</code> directory.
+ */
 public class ScanSettings {
     /**
      * Sorted property-value hash map for Detect's globally applicable properties.
@@ -23,7 +33,7 @@ public class ScanSettings {
      * Accepts property name and value as a <code>String</code>.
      * <br>Example: <code>globalDetectProperties.put("detect.detector.search.depth", "3")</code>
      */
-    private Map<String, String> detectorSharedProperties = new HashMap<>();
+    private SortedMap<String, String> detectorSharedProperties = new TreeMap<>();
     /**
      * Sorted set of {@link ScanType} objects.
      * Each <code>ScanType</code> object stores properties that are only applicable to that scan type.
@@ -36,6 +46,14 @@ public class ScanSettings {
 
     public void setGlobalDetectProperties(final SortedMap<String, String> globalDetectProperties) {
         this.globalDetectProperties = globalDetectProperties;
+    }
+
+    public SortedMap<String, String> getDetectorSharedProperties() {
+        return detectorSharedProperties;
+    }
+
+    public void setDetectorSharedProperties(final SortedMap<String, String> detectorSharedProperties) {
+        this.detectorSharedProperties = detectorSharedProperties;
     }
 
     public SortedSet<ScanType> getScanTypes() {
