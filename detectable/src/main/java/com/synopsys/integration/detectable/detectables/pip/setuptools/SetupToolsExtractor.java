e1327235e24c6642033e05f2c6f0fd8f3becf40b
David Terry <dterry@synopsys.com>
2024-05-02 15:24:23 -0400

    updated approach using toml and pip show

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/pip/setuptools/SetupToolsExtractor.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/pip/setuptools/SetupToolsExtractor.java
@@ -4,14 +4,13 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.commons.lang3.StringUtils;
 import org.tomlj.TomlArray;
 import org.tomlj.TomlParseResult;
 
@@ -24,6 +23,7 @@ import com.synopsys.integration.bdio.model.externalid.ExternalIdFactory;
 import com.synopsys.integration.detectable.detectable.codelocation.CodeLocation;
 import com.synopsys.integration.detectable.detectable.util.TomlFileUtils;
 import com.synopsys.integration.detectable.extraction.Extraction;
+import com.synopsys.integration.detectable.extraction.Extraction.Builder;
 
 public class SetupToolsExtractor {
     
@@ -35,67 +35,88 @@ public class SetupToolsExtractor {
 
     public Extraction extract(File projectToml) {
         // TODO eventually have to account, perhaps in a new set of files entirely, for a pip-less approach
-        // Get dependencies by running pip list
+        // Get dependencies by running pip show on each direct dependency
         try {
-            Map<String, String> pipDependencies = listDependencies();
-        
+            TomlParseResult tomlParseResult = TomlFileUtils.parseFile(projectToml);
+            
             // TODO get direct dependencies from Toml file. Eventually we'll need to get these from cfg and py files
             // instead if those exist or have dependencies. Also, the npm detector uses a more complex packager
             // and transformer approach but this seems pretty straightforward so not sure we need to follow that model
-            Set<String> tomlDirectDependencies = parseDirectDependencies(projectToml);
-            
-            List<Dependency> directDependencies = createDirectDependencies(pipDependencies, tomlDirectDependencies);
-            
-            // Build graph from dependencies
+            Set<String> tomlDirectDependencies = parseDirectDependencies(tomlParseResult);
+
             DependencyGraph dependencyGraph = new BasicDependencyGraph();
-            dependencyGraph.addChildrenToRoot(directDependencies);
+            
+            for (String directDependency : tomlDirectDependencies) {
+                parseShowDependency(dependencyGraph, directDependency, null);
+            }
 
             CodeLocation codeLocation = new CodeLocation(dependencyGraph);
 
-//            String projectName = StringUtils.stripToNull(combinedPackageJson.getName());
-//            String projectVersion = StringUtils.stripToNull(combinedPackageJson.getVersion());
+            String projectName = tomlParseResult.getString("project.name");
+            String projectVersion = tomlParseResult.getString("project.version");
 
-            return new Extraction.Builder()
-                .success(codeLocation)
-//                .projectName(projectName)
-//                .projectVersion(projectVersion)
-                .build();
+            Builder builder = new Extraction.Builder();
+            builder.success(codeLocation);
+            
+            if (!StringUtils.isEmpty(projectName)) {
+                builder.projectName(projectName);
+            }
+            
+            if (!StringUtils.isEmpty(projectVersion)) {
+                builder.projectVersion(projectVersion);
+            }
+            
+            return builder.build();
         } catch (Exception e) {
             return new Extraction.Builder().exception(e).build();
         }
     }
 
-    private List<Dependency> createDirectDependencies(Map<String, String> pipDependencies, Set<String> tomlDirectDependencies) {
-        List<Dependency> directDependencies = new ArrayList<>();
-        
-        for (String dependency : tomlDirectDependencies) {
-            String normalizedDirectDependency = normalize(dependency);
-            
-            for (Map.Entry<String, String> pipDependency : pipDependencies.entrySet()) {
-                String normalizedPipDependency = normalize(pipDependency.getKey());
-                if (normalizedDirectDependency.equals(normalizedPipDependency)) {
-                    directDependencies.add(entryToDependency(pipDependency.getKey(), pipDependency.getValue()));
-                    break;
+    // TODO poor name as this both runs and parses a dependency
+    private void parseShowDependency(DependencyGraph dependencyGraph, String dependencyToSearch, Dependency parentDependency) throws IOException, InterruptedException {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command(Arrays.asList("pip", "show", dependencyToSearch));
+        Process process = processBuilder.start();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
+        Map<String, String> showOutput = new HashMap<>();
+        while ((line = reader.readLine()) != null) {
+            String[] parts = line.split(": ");
+            if (parts.length >= 2) {
+                String key = parts[0].trim();
+                String value = parts[1].trim();
+                if (key.equals("Version") || key.equals("Requires")) {
+                    showOutput.put(key, value);
                 }
             }
         }
+
+        int exitCode = process.waitFor();
+        if (exitCode != 0) {
+            throw new RuntimeException("Error running pip show, exit code: " + exitCode);
+        }
         
-        return directDependencies;
-    }
-    
-    /**
-     * Normalize Python package names as a.b, a-b, and a_b all refer to the same package
-     * @param name Python package name
-     * @return 
-     */
-    private String normalize(String name) {
-        return name.replace('-', '_').replace('.', '_');
+        Dependency currentDependency = entryToDependency(dependencyToSearch, showOutput.get("Version"));
+        
+        if (parentDependency == null) {
+            // No parent, add to root of graph
+            dependencyGraph.addChildrenToRoot(currentDependency);
+        } else {
+            dependencyGraph.addChildWithParent(currentDependency, parentDependency);
+        }
+        
+        // See if we need to continue exploring this chain of dependencies
+        if (showOutput.containsKey("Requires")) {
+            String[] requiredPackages = showOutput.get("Requires").split(", ");
+            for (String requiredPackage : requiredPackages) {
+                parseShowDependency(dependencyGraph, requiredPackage, currentDependency);
+            }
+        }
     }
 
-    public Set<String> parseDirectDependencies(File projectToml) throws IOException {
+    public Set<String> parseDirectDependencies(TomlParseResult tomlParseResult) throws IOException {
         Set<String> results = new HashSet<>();
-        
-        TomlParseResult tomlParseResult = TomlFileUtils.parseFile(projectToml);
         
         TomlArray dependencies = tomlParseResult.getArray("project.dependencies");
         
@@ -119,28 +140,5 @@ public class SetupToolsExtractor {
     private Dependency entryToDependency(String key, String value) {
         ExternalId externalId = externalIdFactory.createNameVersionExternalId(Forge.PYPI, key, value);
         return new Dependency(externalId);
-    }
-
-    private Map<String, String> listDependencies() throws IOException, InterruptedException {
-        ProcessBuilder processBuilder = new ProcessBuilder();
-        processBuilder.command(Arrays.asList("pip", "list"));
-        Process process = processBuilder.start();
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-        String line;
-        Map<String, String> resultMap = new HashMap<>();
-        while ((line = reader.readLine()) != null) {
-            String[] parts = line.split("\\s+");
-            if (parts.length >= 2) {
-                resultMap.put(parts[0], parts[1]);
-            }
-        }
-
-        int exitCode = process.waitFor();
-        if (exitCode != 0) {
-            throw new RuntimeException("Error running pip list, exit code: " + exitCode);
-        }
-
-        return resultMap;
     }
 }
