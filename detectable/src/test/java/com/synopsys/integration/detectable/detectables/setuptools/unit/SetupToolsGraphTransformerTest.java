20d29826fc51fc9ea358bf466d8ca05a8f11ffee
David Terry <dterry@synopsys.com>
2024-05-20 10:44:06 -0400

    graph test

--- detectable/src/test/java/com/synopsys/integration/detectable/detectables/setuptools/unit/SetupToolsGraphTransformerTest.java
+++ detectable/src/test/java/com/synopsys/integration/detectable/detectables/setuptools/unit/SetupToolsGraphTransformerTest.java
@@ -2,19 +2,25 @@ package com.synopsys.integration.detectable.detectables.setuptools.unit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import com.synopsys.integration.bdio.graph.DependencyGraph;
 import com.synopsys.integration.bdio.model.Forge;
 import com.synopsys.integration.bdio.model.externalid.ExternalIdFactory;
+import com.synopsys.integration.detectable.ExecutableTarget;
+import com.synopsys.integration.detectable.detectable.executable.DetectableExecutableRunner;
 import com.synopsys.integration.detectable.detectables.setuptools.parse.SetupToolsParsedResult;
 import com.synopsys.integration.detectable.detectables.setuptools.transform.SetupToolsGraphTransformer;
 import com.synopsys.integration.detectable.python.util.PythonDependency;
 import com.synopsys.integration.detectable.util.graph.NameVersionGraphAssert;
+import com.synopsys.integration.executable.ExecutableOutput;
 import com.synopsys.integration.executable.ExecutableRunnerException;
 
 public class SetupToolsGraphTransformerTest {
@@ -42,5 +48,41 @@ public class SetupToolsGraphTransformerTest {
         graphAssert.hasRootDependency("numpy", "1.21.0");
         
         assertEquals(2, dependencyGraph.getRootDependencies().size());
+    }
+    
+    @Test
+    public void testPipTransform() throws ExecutableRunnerException {
+        // Create a list of PythonDependency objects
+        List<PythonDependency> dependencies = Arrays.asList(
+                new PythonDependency("certifi", "2022.6.15")
+        );
+
+        // Create a SetupToolsParsedResult object
+        SetupToolsParsedResult parsedResult = new SetupToolsParsedResult("setuptools", "1.0.0", dependencies);
+
+        // Mock the DetectableExecutableRunner
+        DetectableExecutableRunner executableRunner = Mockito.mock(DetectableExecutableRunner.class);
+        ExecutableOutput executableOutput = Mockito.mock(ExecutableOutput.class);
+        when(executableOutput.getReturnCode()).thenReturn(0);
+        when(executableOutput.getStandardOutputAsList()).thenReturn(Arrays.asList(
+                "Name: certifi",
+                "Version: 2022.6.15",
+                "Requires: "
+        ));
+        when(executableRunner.execute(Mockito.any())).thenReturn(executableOutput);
+
+        // Create an instance of SetupToolsGraphTransformer
+        SetupToolsGraphTransformer graphTransformer = new SetupToolsGraphTransformer(null, new ExternalIdFactory(), executableRunner);
+
+        // Call the transform method
+        DependencyGraph dependencyGraph = graphTransformer.transform(ExecutableTarget.forCommand("pip"), parsedResult);
+
+        // Assert that the returned DependencyGraph is not null and contains the expected number of dependencies
+        assertNotNull(dependencyGraph);
+        assertEquals(1, dependencyGraph.getRootDependencies().size());
+        
+        NameVersionGraphAssert graphAssert = new NameVersionGraphAssert(Forge.PYPI, dependencyGraph);
+
+        graphAssert.hasRootDependency("certifi", "2022.6.15");
     }
 }
