7c4ef00a1b4d521688ead0ae1784cd5fbb943f07
Andrian Sevastyanov <andrians@synopsys.com>
2024-05-20 16:50:47 -0600

    Tests for component locator status code

--- src/test/java/com/synopsys/integration/detect/workflow/componentlocationanalysis/GenerateComponentLocationAnalysisOperationIT.java
+++ src/test/java/com/synopsys/integration/detect/workflow/componentlocationanalysis/GenerateComponentLocationAnalysisOperationIT.java
@@ -1,15 +1,16 @@
 package com.synopsys.integration.detect.workflow.componentlocationanalysis;
 
+import java.io.IOException;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
 import com.synopsys.integration.detect.battery.docker.provider.BuildDockerImageProvider;
 import com.synopsys.integration.detect.battery.docker.util.DetectCommandBuilder;
 import com.synopsys.integration.detect.battery.docker.util.DetectDockerTestRunner;
 import com.synopsys.integration.detect.battery.docker.util.DockerAssertions;
 import com.synopsys.integration.detect.configuration.DetectProperties;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
 
 @Disabled
 @Tag("integration")
@@ -29,8 +30,10 @@ public class GenerateComponentLocationAnalysisOperationIT {
 
             dockerAssertions.successfulOperation("Generating Component Location Analysis File for All Components");
             dockerAssertions.logContainsPattern("Component Location Analysis File: .*components-with-locations\\.json");
+            dockerAssertions.logDoesNotContain("COMPONENT_LOCATOR: SUCCESS");
         }
     }
+
     @Test
     void onlineRapidPkgMngrScan_analysisEnabled() throws IOException {
         try (DetectDockerTestRunner test = new DetectDockerTestRunner("component-location-analysis-test", "gradle-simple:1.0.0")) {
@@ -44,6 +47,47 @@ public class GenerateComponentLocationAnalysisOperationIT {
 
             dockerAssertions.successfulOperation("Generating Component Location Analysis File for Reported Components");
             dockerAssertions.logContainsPattern("Component Location Analysis File: .*components-with-locations\\.json");
+            dockerAssertions.logDoesNotContain("COMPONENT_LOCATOR: SUCCESS");
+
+        }
+    }
+
+    @Test
+    void testOfflinePkgMngrScan_analysisEnabled_affectsStatus() throws IOException {
+        try (DetectDockerTestRunner test = new DetectDockerTestRunner("component-location-analysis-test", "gradle-simple:1.0.0")) {
+            test.withImageProvider(BuildDockerImageProvider.forDockerfilResourceNamed("SimpleGradle.dockerfile"));
+
+            DetectCommandBuilder commandBuilder = DetectCommandBuilder.withOfflineDefaults().defaultDirectories(test);
+            commandBuilder.property(DetectProperties.DETECT_COMPONENT_LOCATION_ANALYSIS_ENABLED, "true");
+            commandBuilder.property(DetectProperties.DETECT_COMPONENT_LOCATION_ANALYSIS_AFFECTS_STATUS, "true");
+            commandBuilder.property(DetectProperties.BLACKDUCK_OFFLINE_MODE, "true");
+            commandBuilder.property(DetectProperties.BLACKDUCK_OFFLINE_MODE_FORCE_BDIO, "true");
+            commandBuilder.property(DetectProperties.DETECT_INCLUDED_DETECTOR_TYPES, "DETECTOR");
+
+            DockerAssertions dockerAssertions = test.run(commandBuilder);
+
+            dockerAssertions.successfulOperation("Generating Component Location Analysis File for All Components");
+            dockerAssertions.logContainsPattern("Component Location Analysis File: .*components-with-locations\\.json");
+            dockerAssertions.logContains("COMPONENT_LOCATOR: SUCCESS");
+        }
+    }
+
+    @Test
+    void onlineRapidPkgMngrScan_analysisEnabled_affectsStatus() throws IOException {
+        try (DetectDockerTestRunner test = new DetectDockerTestRunner("component-location-analysis-test", "gradle-simple:1.0.0")) {
+            test.withImageProvider(BuildDockerImageProvider.forDockerfilResourceNamed("SimpleGradle.dockerfile"));
+
+            DetectCommandBuilder commandBuilder = DetectCommandBuilder.withOfflineDefaults().defaultDirectories(test);
+            commandBuilder.property(DetectProperties.DETECT_COMPONENT_LOCATION_ANALYSIS_ENABLED, "true");
+            commandBuilder.property(DetectProperties.DETECT_COMPONENT_LOCATION_ANALYSIS_AFFECTS_STATUS, "true");
+            commandBuilder.property(DetectProperties.DETECT_BLACKDUCK_SCAN_MODE, "RAPID");
+
+            DockerAssertions dockerAssertions = test.run(commandBuilder);
+
+            dockerAssertions.successfulOperation("Generating Component Location Analysis File for Reported Components");
+            dockerAssertions.logContainsPattern("Component Location Analysis File: .*components-with-locations\\.json");
+            dockerAssertions.logContains("COMPONENT_LOCATOR: SUCCESS");
+
         }
     }
 }
