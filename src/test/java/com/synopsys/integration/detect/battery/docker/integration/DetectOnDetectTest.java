724a8117f725bbf0395b2f9357413a873058943f
devmehtasynopsys <devmehta@synopsys.com>
2024-06-10 16:12:15 -0400

    Initial Unit Tests

--- src/test/java/com/synopsys/integration/detect/battery/docker/integration/DetectOnDetectTest.java
+++ src/test/java/com/synopsys/integration/detect/battery/docker/integration/DetectOnDetectTest.java
@@ -140,6 +140,32 @@ public class DetectOnDetectTest {
         }
     }
 
+    @Test
+    public void testRunWithAutonomousEnabled() throws Exception {
+        try (DetectDockerTestRunner test = new DetectDockerTestRunner("autonomous-scan-test", "detect-9.8.0:1.0.0")) {
+            test.withImageProvider(BuildDockerImageProvider.forDockerfilResourceNamed("Detect-9.8.0.dockerfile"));
+
+            BlackDuckTestConnection blackDuckTestConnection = BlackDuckTestConnection.fromEnvironment();
+            BlackDuckAssertions blackduckAssertions = blackDuckTestConnection.projectVersionAssertions("autonomous-scan-test", "happy-path");
+            blackduckAssertions.emptyOnBlackDuck();
+
+            DetectCommandBuilder commandBuilder = new DetectCommandBuilder().defaults().defaultDirectories(test);
+            commandBuilder.connectToBlackDuck(blackDuckTestConnection);
+            commandBuilder.projectNameVersion(blackduckAssertions);
+            commandBuilder.waitForResults();
+
+            DockerAssertions dockerAssertions = test.run(commandBuilder);
+
+            dockerAssertions.bdioFiles(1); //7 code locations, 6 bdio, 1 signature scanner
+
+            blackduckAssertions.hasCodeLocations(
+                    "src/detect-on-detect-docker/happy-path signature"
+            );
+
+            blackduckAssertions.hasComponents("jackson-core");
+        }
+    }
+
     private long assertEmptyRiskReport(File reportDirectory, ProjectVersionWrapper projectVersionWrapper, ReportService reportService) throws IntegrationException {
         List<File> pdfFiles = getPdfFiles(reportDirectory);
         assertEquals(0, pdfFiles.size());
