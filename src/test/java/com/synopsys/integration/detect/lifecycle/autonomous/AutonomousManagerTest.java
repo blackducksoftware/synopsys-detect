6271c03f269470e17b234e527af60918df6003c5
Nirav Raje <niravr@synopsys.com>
2024-06-13 11:28:11 -0400

    Add AutonomousManagerTest class

---
+++ src/test/java/com/synopsys/integration/detect/lifecycle/autonomous/AutonomousManagerTest.java
@@ -0,0 +1,13 @@
+package com.synopsys.integration.detect.lifecycle.autonomous;
+
+import java.io.File;
+
+public class AutonomousManagerTest {
+
+    public static final File PRE_RUN_SCAN_SETTINGS_TEST_DIRECTORY = new File("src/test/resources/lifecycle/autonomous/pre-run-scan-settings");
+    public static final File POST_RUN_SCAN_SETTINGS_TEST_DIRECTORY = new File("src/test/resources/lifecycle/autonomous/post-run-scan-settings");
+
+
+
+
+}
