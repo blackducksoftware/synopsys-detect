e4414a95311e57bc3d9725b40ab837bcf036a01a
David Terry <dterry@synopsys.com>
2024-06-17 13:30:13 -0400

    basic cleanup and error handling

--- src/main/java/com/synopsys/integration/detect/tool/binaryscanner/BinaryUploadOperation.java
+++ src/main/java/com/synopsys/integration/detect/tool/binaryscanner/BinaryUploadOperation.java
@@ -3,16 +3,11 @@ package com.synopsys.integration.detect.tool.binaryscanner;
 import java.io.File;
 import java.io.IOException;
 
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.synopsys.blackduck.upload.client.uploaders.BinaryUploader;
 import com.synopsys.blackduck.upload.rest.model.response.UploadFinishResponse;
-import com.synopsys.integration.blackduck.codelocation.Result;
-import com.synopsys.integration.blackduck.codelocation.binaryscanner.BinaryScanBatchOutput;
-import com.synopsys.integration.blackduck.codelocation.binaryscanner.BinaryScanOutput;
-import com.synopsys.integration.blackduck.exception.BlackDuckIntegrationException;
 import com.synopsys.integration.detect.configuration.DetectUserFriendlyException;
 import com.synopsys.integration.detect.configuration.enumeration.ExitCodeType;
 import com.synopsys.integration.detect.workflow.codelocation.CodeLocationNameManager;
@@ -26,6 +21,7 @@ public class BinaryUploadOperation {
     private static final String STATUS_KEY = "BINARY_SCAN";
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
+    // TODO might not need this depends on how I finalize sending the uploader around
     private final CodeLocationNameManager codeLocationNameManager;
     private final StatusEventPublisher statusEventPublisher;
 
@@ -42,57 +38,16 @@ public class BinaryUploadOperation {
         throws DetectUserFriendlyException {
         try {
             logger.info("Preparing to upload binary scan file: " + binaryScanFile.getAbsolutePath());       
-            // TODO this only has etag and location. So how do I tell if something fails? Exception?
-            // Just print the library logging to the logs?
-            
-            // TODO handle two of these?
+
             UploadFinishResponse response = binaryUploader.upload(binaryScanFile.toPath());
                     
             logger.info("Successfully uploaded binary scan file: " + binaryScanFile.getAbsolutePath());
             statusEventPublisher.publishStatusSummary(new Status(STATUS_KEY, StatusType.SUCCESS));
-                
-           // CodeLocationCreationData<BinaryScanBatchOutput> codeLocationCreationData = binaryScanUploadService.uploadBinaryScan(binaryScanBatch);
-
-         //   BinaryScanBatchOutput binaryScanBatchOutput = codeLocationCreationData.getOutput();
-            // The throwExceptionForError() in BinaryScanBatchOutput has a bug, so doing that work here
-          //  throwExceptionForError(binaryScanBatchOutput);
             return response;
         } catch (IntegrationException | IOException e) {
+            // TODO need to test how the errors look when there is an exception
             statusEventPublisher.publishStatusSummary(new Status(STATUS_KEY, StatusType.FAILURE));
             throw new DetectUserFriendlyException("Failed to upload binary scan file.", e, ExitCodeType.FAILURE_BLACKDUCK_FEATURE_ERROR);
         }
-    }
-
-    // BinaryScanBatchOutput used to do this, but our understanding of what needs to happen has been
-    // changing rapidly. Once we're confident we know what it should do, it should presumably move back there.
-    private void throwExceptionForError(BinaryScanBatchOutput binaryScanBatchOutput) throws BlackDuckIntegrationException {
-        for (BinaryScanOutput binaryScanOutput : binaryScanBatchOutput) {
-            if (binaryScanOutput.getResult() == Result.FAILURE) {
-                // Black Duck responses are single-line message (loggable as-is), but nginx Bad Gateway responses are
-                // multi-line html with the message embedded (that mess up the log).
-                // cleanResponse() attempts to produce something reasonable to log in either case
-                String cleanedBlackDuckResponse = cleanResponse(binaryScanOutput.getResponse());
-                String uploadErrorMessage = String.format(
-                    "Error when uploading binary scan: %s (Black Duck response: %s)",
-                    binaryScanOutput.getErrorMessage().orElse(binaryScanOutput.getStatusMessage()),
-                    cleanedBlackDuckResponse
-                );
-                logger.error(uploadErrorMessage);
-                throw new BlackDuckIntegrationException(uploadErrorMessage);
-            }
-        }
-    }
-
-    private String cleanResponse(String response) {
-        if (StringUtils.isBlank(response)) {
-            return "";
-        }
-        String lengthLimitedResponse = StringUtils.substring(response, 0, 200);
-        if (!lengthLimitedResponse.equals(response)) {
-            lengthLimitedResponse = lengthLimitedResponse + "...";
-        }
-        String[] searchList = { "\n", "\r" };
-        String[] replacementList = { " ", "" };
-        return StringUtils.replaceEachRepeatedly(lengthLimitedResponse, searchList, replacementList);
     }
 }
