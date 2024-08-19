df156738f72992fd244c29d0e5a354f1913e4e30
Nirav Raje <niravr@synopsys.com>
2024-08-19 18:11:35 -0400

    Remove unused imports

--- src/main/java/com/synopsys/integration/detect/workflow/phonehome/PhoneHomeCredentialsFactory.java
+++ src/main/java/com/synopsys/integration/detect/workflow/phonehome/PhoneHomeCredentialsFactory.java
@@ -2,7 +2,6 @@ package com.synopsys.integration.detect.workflow.phonehome;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
-import com.synopsys.integration.blackduck.http.client.DefaultBlackDuckHttpClient;
 import com.synopsys.integration.detect.configuration.DetectInfoUtility;
 import com.synopsys.integration.exception.IntegrationException;
 import com.synopsys.integration.log.SilentIntLogger;
@@ -12,20 +11,12 @@ import com.synopsys.integration.rest.proxy.ProxyInfo;
 import com.synopsys.integration.rest.response.Response;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.time.Duration;
-import java.time.temporal.ChronoUnit;
 
 public class PhoneHomeCredentialsFactory {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
