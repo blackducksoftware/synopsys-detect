708eae0ce3000a8a7dff4d3e8f5c46ed7299d542
andrian-sevastyanov <139918786+andrian-sevastyanov@users.noreply.github.com>
2024-08-23 11:36:57 -0600

    10.0.z (#1213)
    * begin alterations based on snapshots
    
    * first pass of enhancing v9
    
    * handle extra detail in snapshot
    
    * refactoring basic common code path still too many v6 not handling slash
    
    * remove v6 names fix and write tests
    
    * remove v6 need to examine tests
    
    * minor cleanup
    
    * add v9 extraction test
    
    * new v9 tests
    
    * remove extra info from version string
    
    * random dependency name
    
    * update comment
    
    * add new csv property to detect and bump blackduck-common
    
    * basic path to keep csv in own folder
    
    * fix cleanup and align airgap
    
    * don't create csv if online
    
    * add test
    
    * fix version
    
    * fix typo
    
    * initial package json character cleanup
    
    * improve edge cases add test
    
    * cleanup comments
    
    * refactor to have utility semver comparator class
    
    * update release notes
    
    * Release 10.0.0-SIGQA1
    
    * protect illegal markdown characters
    
    * protect illegal markdown characters
    
    * Release 10.0.0-SIGQA2
    
    * Using the next snapshot post release 10.0.0-SIGQA3-SNAPSHOT
    
    * Increase versions of tested package managers in integration tests and add Dart integration test (#1201)
    
    * Use latest Yocto version in the smoke test
    
    * Bump tested version of maven up to 3.9.8
    
    * Dart smoke test
    
    * Ensure java is installed on the docker image
    
    * Force image regeneration
    
    * Release 10.0.0-SIGQA3
    
    * Using the next snapshot post release 10.0.0-SIGQA4-SNAPSHOT
    
    * Revert "Add ability to generate signature scan output in csv format"
    
    * restore needed unrelated changes
    
    * Bump up maven and gradle versions in tests to latest available (#1212)
    
    ---------
    
    Co-authored-by: David Terry <dterry@synopsys.com>
    Co-authored-by: dterrysynopsys <103214400+dterrysynopsys@users.noreply.github.com>
    Co-authored-by: blackduck-serv-builder <serv-builder@blackducksoftware.com>

--- detectable/src/main/java/com/synopsys/integration/detectable/detectables/pnpm/lockfile/model/PnpmLockYaml.java
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/pnpm/lockfile/model/PnpmLockYaml.java
@@ -1,8 +1,25 @@
 package com.synopsys.integration.detectable.detectables.pnpm.lockfile.model;
 
+import java.util.Map;
+
 import org.jetbrains.annotations.Nullable;
 
-public class PnpmLockYaml {
+public class PnpmLockYaml extends PnpmLockYamlBase {
     @Nullable
-    public String lockfileVersion;
+    public Map<String, PnpmPackageInfo> packages;
+    
+    @Nullable
+    public Map<String, PnpmProjectPackage> importers;
+    
+    @Nullable
+    public Map<String, PnpmDependencyInfo> dependencies;
+
+    @Nullable
+    public Map<String, PnpmDependencyInfo> devDependencies;
+
+    @Nullable
+    public Map<String, PnpmDependencyInfo> optionalDependencies;
+    
+    @Nullable
+    public Map<String, PnpmPackageInfo> snapshots;
 }
---
+++ detectable/src/main/java/com/synopsys/integration/detectable/detectables/pnpm/lockfile/model/PnpmLockYamlBase.java
@@ -0,0 +1,8 @@
+package com.synopsys.integration.detectable.detectables.pnpm.lockfile.model;
+
+import org.jetbrains.annotations.Nullable;
+
+public class PnpmLockYamlBase {
+    @Nullable
+    public String lockfileVersion;
+}
