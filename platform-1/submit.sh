08439bd22ab702cfb162261f9e8d9dc3f4526830
gopanna <gopanna@snps-x70wnyooeahd.internal.synopsys.com>
2024-09-13 13:35:40 -0400

    Renamed synopsys-detect as detect for JAR other build/deploy methods.

--- platform-1/submit.sh
+++ platform-1/submit.sh
@@ -14,11 +14,11 @@ docker push ${INTERNAL_DOCKER_REGISTRY}/${TARGET_REPO}/${TARGET_IMAGE}:${TARGET_
 docker tag ${INTERNAL_DOCKER_REGISTRY}/${TARGET_REPO}/${TARGET_IMAGE}:${TARGET_IMAGE_TAG} ${TARGET_REPO}/${TARGET_IMAGE}:${TARGET_IMAGE_TAG}
 
 # upload to artifactory
-zip_sha=$(echo $(sha256sum synopsys-detect-${RELEASE_VERSION}-air-gap.zip) | cut -d' ' -f 1)
+zip_sha=$(echo $(sha256sum detect-${RELEASE_VERSION}-air-gap.zip) | cut -d' ' -f 1)
 
 sed -e "s/TARGET_IMAGE_TAG/${TARGET_IMAGE_TAG}/g" -e "s/RELEASE_VERSION/${RELEASE_VERSION}/g" -e "s/UBI_VERSION/${UBI_VERSION}/g" -e "s/ZIP_SHA256_VALUE/${zip_sha}/g" hardening_manifest.yaml.template > hardening_manifest.yaml
 
-curl -X PUT -u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} ${ARTIFACTORY_HOST}/blackduck-repo1.dso.mil-generic/${TARGET_IMAGE}/synopsys-detect-${RELEASE_VERSION}-air-gap.zip -T synopsys-detect-${RELEASE_VERSION}-air-gap.zip
+curl -X PUT -u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} ${ARTIFACTORY_HOST}/blackduck-repo1.dso.mil-generic/${TARGET_IMAGE}/detect-${RELEASE_VERSION}-air-gap.zip -T detect-${RELEASE_VERSION}-air-gap.zip
 
 # push to repo1
 
