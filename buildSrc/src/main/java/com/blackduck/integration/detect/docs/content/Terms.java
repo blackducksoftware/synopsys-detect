74e961934ca3f8d63583a1c7d3848c9057fa2be2
gopanna <gopanna@snps-x70wnyooeahd.internal.synopsys.com>
2024-09-25 14:21:42 -0400

    Updated synopsys references in Terms.java

--- buildSrc/src/main/java/com/blackduck/integration/detect/docs/content/Terms.java
+++ buildSrc/src/main/java/com/blackduck/integration/detect/docs/content/Terms.java
@@ -8,12 +8,12 @@ public class Terms {
 
     public Terms() {
         termMap.put("solution_name", "Black Duck Detect");
-        termMap.put("script_repo_url_bash", "https://detect.synopsys.com/detect9.sh");
-        termMap.put("script_repo_url_powershell", "https://detect.blackduck.com/detect9.ps1");
+        termMap.put("script_repo_url_bash", "https://detect.blackduck.com/detect10.sh");
+        termMap.put("script_repo_url_powershell", "https://detect.blackduck.com/detect10.ps1");
         termMap.put("binary_repo_url_project", "https://repo.blackduck.com/bds-integrations-release/com/blackduck/integration/detect");
         termMap.put("binary_repo_ui_url_project", "https://repo.blackduck.com/bds-integrations-release/com/blackduck/integration/detect");
 	termMap.put("binary_repo_jenkins_url_project", "https://repo.blackduck.com/bds-integrations-release/com/blackducksoftware/integration/blackduck-detect/");
-	termMap.put("binary_repo_url_sigma", "https://repo.blackduck.com/sigma-release-trial/2022.6.0/");
+	termMap.put("binary_repo_url_sigma", "https://repo.blackduck.com/sigma-release-trial/2024.9.0/");
     }
 
     public String put(String termKey, String replacementString) {
