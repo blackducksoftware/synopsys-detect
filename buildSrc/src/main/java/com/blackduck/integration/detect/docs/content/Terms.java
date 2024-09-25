4f328e249422c50c61d54e0f58bf1125e8d74629
Chris Potts <cpotts@synopsys.com>
2024-09-25 11:48:32 -0400

    Updates for bundle/naming
    Updates for bundle/naming

--- buildSrc/src/main/java/com/blackduck/integration/detect/docs/content/Terms.java
+++ buildSrc/src/main/java/com/blackduck/integration/detect/docs/content/Terms.java
@@ -7,11 +7,11 @@ public class Terms {
     private final Map<String, String> termMap = new HashMap<>();
 
     public Terms() {
-        termMap.put("solution_name", "Synopsys Detect");
-        termMap.put("script_repo_url_bash", "https://detect.synopsys.com/detect9.sh");
-        termMap.put("script_repo_url_powershell", "https://detect.synopsys.com/detect9.ps1");
-        termMap.put("binary_repo_url_project", "https://repo.blackduck.com/bds-integrations-release/com/blackduck/integration/synopsys-detect");
-        termMap.put("binary_repo_ui_url_project", "https://repo.synopsys.com/bds-integrations-release/com/synopsys/integration/synopsys-detect");
+        termMap.put("solution_name", "Black Duck Detect");
+        termMap.put("script_repo_url_bash", "https://detect.blackduck.com/detect10.sh");
+        termMap.put("script_repo_url_powershell", "https://detect.blackduck.com/detect10.ps1");
+        termMap.put("binary_repo_url_project", "https://repo.blackduck.com/bds-integrations-release/com/blackduck/integration/detect/");
+        termMap.put("binary_repo_ui_url_project", "https://repo.blackduck.com/bds-integrations-release/com/blackduck/integration/detect/");
 		termMap.put("binary_repo_jenkins_url_project", "https://repo.blackduck.com/bds-integrations-release/com/blackducksoftware/integration/blackduck-detect/");
 		termMap.put("binary_repo_url_sigma", "https://repo.blackduck.com/sigma-release-trial/2022.6.0/");
     }
