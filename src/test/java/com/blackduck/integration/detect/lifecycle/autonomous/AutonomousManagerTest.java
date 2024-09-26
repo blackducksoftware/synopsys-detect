21fb17c2c599490d49425e10160bd103ebfe5c1e
gopanna <gopanna@snps-x70wnyooeahd.internal.synopsys.com>
2024-09-26 10:14:48 -0400

    Updated synopsys references in *.java with fixes to tests.

--- src/test/java/com/blackduck/integration/detect/lifecycle/autonomous/AutonomousManagerTest.java
+++ src/test/java/com/blackduck/integration/detect/lifecycle/autonomous/AutonomousManagerTest.java
@@ -32,7 +32,7 @@ public class AutonomousManagerTest {
     private static DetectPropertyConfiguration detectPropertyConfigurationMock;
 
     private static String TEST_DETECT_SOURCE_PATH = "/Users/username/detect";
-    private static String TEST_HASHED_SCAN_SETTINGS_FILE_NAME = "9d6c66c9-8c9b-31ef-92e1-982c0dbef944.json";
+    private static String TEST_HASHED_SCAN_SETTINGS_FILE_NAME = "a0678131-f7df-358f-814d-9b99e36906e4.json";
     private static File postRunScanSettingsTestFile;
 
     public AutonomousManagerTest() {
