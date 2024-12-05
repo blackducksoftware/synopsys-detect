a1c026acdbe6a9def2e711ae71e4a66bad906a23
devmehtasynopsys <devmehta@synopsys.com>
2024-12-05 14:19:07 -0500

    Added comments

--- detectable/src/main/java/com/blackduck/integration/detectable/detectables/opam/buildexe/parse/OpamTreeParser.java
+++ detectable/src/main/java/com/blackduck/integration/detectable/detectables/opam/buildexe/parse/OpamTreeParser.java
@@ -14,7 +14,6 @@ import com.google.gson.stream.JsonReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
@@ -37,17 +36,19 @@ public class OpamTreeParser {
     }
 
     public List<OpamParsedResult> parseJsonTreeFile(File outputFile) throws IOException {
+        //Check if there is any issue with the file
         if(outputFile == null || !outputFile.exists() || !outputFile.isFile()) {
             logger.warn("The opamTreeOutput.json file does not exist, the file was deleted, or an issue occurred while running the opam tree command.");
             return codeLocations;
         }
 
+        //initialize json reader object
         try (JsonReader jsonReader = new JsonReader(new FileReader(outputFile))) {
+            //convert the whole JSON file into OpamJsonFileModule PoJo
             OpamJsonFileModule opamTreeModule = gson.fromJson(jsonReader, OpamJsonFileModule.class);
+            //After getting PoJo, we will loop over the projects which are found in the tree
             for(OpamTreeProjectModule opamTreeProjectModule: opamTreeModule.opamProjects) {
-                if(opamTreeProjectModule != null) {
-                    processTreeJsonObject(opamTreeProjectModule);
-                }
+                processTreeJsonObject(opamTreeProjectModule);
             }
         } catch (IOException e) {
             throw new IOException("There was an error while parsing the JSON file.",e);
@@ -58,18 +59,21 @@ public class OpamTreeParser {
 
     public void processTreeJsonObject(OpamTreeProjectModule opamTreeProjectModule){
         if(opamTreeProjectModule != null) {
+            //this PoJo is OpamTreeProjectModule where it will have name, version and dependencies of one project
             String projectName = opamTreeProjectModule.name;
             String projectVersion = opamTreeProjectModule.version;
 
+            //initialze the project
             initializeProject(projectName, projectVersion);
 
             if(!opamTreeProjectModule.dependencies.isEmpty()) {
-                collectCodeLocations(opamTreeProjectModule.dependencies, null);
+                collectCodeLocations(opamTreeProjectModule.dependencies, null); //passing as null to distinguish between direct and transitive
             }
         }
     }
 
     private void collectCodeLocations(List<OpamTreeDependencyModule> dependencyModules, Dependency parentDependency) {
+        // recursively loop over the dependencies for each project
         for(OpamTreeDependencyModule dependencyModule: dependencyModules) {
             if(dependencyModule != null) {
                 String packageName = dependencyModule.name;
@@ -77,12 +81,14 @@ public class OpamTreeParser {
 
                 Dependency dependency = createDependencyExternalId(packageName, packageVersion);
 
+                //direct dependency
                 if(parentDependency == null) {
                     currentGraph.addDirectDependency(dependency);
-                } else {
+                } else { //transitive dependency
                     currentGraph.addChildWithParent(dependency, parentDependency);
                 }
 
+                //recursively call this method again if there are any transitives found with current dependency as parent
                 if(!dependencyModule.dependencies.isEmpty()) {
                     collectCodeLocations(dependencyModule.dependencies, dependency);
                 }
